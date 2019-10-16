b0f663ad326616c0a6409e28b0b14e2151e404d1
Devin Witherspoon <dcwither@gmail.com>
2019-10-16 13:25:26 -0700

    use typescript for storybook

--- packages/czedi-kit-components/.storybook/config.ts
+++ packages/czedi-kit-components/.storybook/config.ts
@@ -1,8 +1,10 @@
 import { configure } from "@storybook/react";
 import { addDecorator } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
+import { withInfo } from "@storybook/addon-info";
 
 addDecorator(withA11y);
+addDecorator(withInfo as any);
 
 // automatically import all files ending in *.stories.js
-configure(require.context("../src", true, /\.stories\.js$/), module);
+configure(require.context("../src", true, /\.stories\.(ts|tsx|mdx)$/), module);
