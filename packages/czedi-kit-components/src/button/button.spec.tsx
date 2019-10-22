69589f1781697840b23cf2029891246d22b1475e
Devin Witherspoon <dcwither@gmail.com>
2019-10-22 15:47:47 -0700

    add jest-axe to testing

--- packages/czedi-kit-components/src/button/button.spec.tsx
+++ packages/czedi-kit-components/src/button/button.spec.tsx
@@ -1,5 +1,6 @@
 import { text } from "./button.stories";
 import { render } from "@testing-library/react";
+import { axe } from "jest-axe";
 
 describe("<Button />", () => {
   it("renders the component", () => {
@@ -9,5 +10,11 @@ describe("<Button />", () => {
         Hello Button
       </button>
     `);
+  });
+
+  it("passes accessibility checks", async () => {
+    const { container } = render(text());
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
