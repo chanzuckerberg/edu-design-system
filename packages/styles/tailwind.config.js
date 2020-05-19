3bd885660937d717989bd4cf0672ca681312edce
Devin Witherspoon <dcwither@gmail.com>
2020-05-19 15:05:04 -0700

    chore(tokens): add legacy colors to tailwind

--- packages/styles/tailwind.config.js
+++ packages/styles/tailwind.config.js
@@ -1,5 +1,8 @@
+const tokens = require("@chanzuckerberg/czedi-kit-tokens/json/variables-nested.json");
+
 module.exports = {
   theme: {
+    colors: tokens.color.legacy,
     extend: {},
   },
   variants: {},
