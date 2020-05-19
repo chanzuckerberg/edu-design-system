4a9540841d80fbd50a09e7e652ef2a205546a0c1
Devin Witherspoon <dcwither@gmail.com>
2020-05-19 13:11:43 -0700

    docs(tokens): added placeholder docs for color tokens

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,16 +1,21 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/*.stories.mdx", "../src/**/*.stories.(ts|tsx|mdx)"],
   addons: ["@storybook/preset-typescript", "@storybook/addon-docs"],
   webpackFinal: config => {
-    return {
-      ...config,
-      // source: https://stackoverflow.com/questions/41522721/how-to-watch-certain-node-modules-changes-with-webpack-dev-server
-      watchOptions: {
-        ...config.watchOptions,
-        ignored: [
-          /node_modules([\\]+|\/)+(?!@chanzuckerberg\/czedi-kit-styles)/,
-        ],
-      },
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    // source: https://stackoverflow.com/questions/41522721/how-to-watch-certain-node-modules-changes-with-webpack-dev-server
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [/node_modules([\\]+|\/)+(?!@chanzuckerberg\/czedi-kit-styles)/],
     };
+
+    return config;
   },
 };
