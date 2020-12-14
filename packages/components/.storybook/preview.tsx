109e03669013b443e479fee5a2cc4727c5e20c65
Annie Hu <annie.hu@chanzuckerberg.com>
2020-12-14 16:31:01 -0500

    chore(styles): remove styles package

--- packages/components/.storybook/preview.tsx
+++ packages/components/.storybook/preview.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
 
 import { DocsContainer, DocsPage } from "@storybook/addon-docs/blocks";
+import { GlobalStyles } from "twin.macro";
 
 import { addDecorator } from "@storybook/react";
 import { addParameters } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 
 addDecorator(withA11y);
-addDecorator((storyFn) => <div dir="ltr">{storyFn()}</div>);
+addDecorator((storyFn) => (
+  <div dir="ltr">
+    <GlobalStyles />
+    {storyFn()}
+  </div>
+));
 addParameters({
   docs: {
     container: DocsContainer,
