391da0825b1c934782ee0c2e7ddf1f2062a7e77e
Andrew Huth <ahuth@chanzuckerberg.com>
2020-12-16 07:06:07 -0800

    chore: remove unnecessary addon-docs config

--- packages/components/.storybook/preview.tsx
+++ packages/components/.storybook/preview.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
-
-import { DocsContainer, DocsPage } from "@storybook/addon-docs/blocks";
 import EDSGlobalStyles from "../src/styles/global";
-
 import { addDecorator } from "@storybook/react";
-import { addParameters } from "@storybook/react";
 
 addDecorator((storyFn) => (
   <div dir="ltr">
@@ -12,9 +8,3 @@ addDecorator((storyFn) => (
     {storyFn()}
   </div>
 ));
-addParameters({
-  docs: {
-    container: DocsContainer,
-    page: DocsPage,
-  },
-});
