776d3f1824d6c59b5553952245874fa823f66a03
Andrew Huth <ahuth@chanzuckerberg.com>
2020-12-16 07:08:59 -0800

    chore: update storybook decorators definitions for Storybook v6

--- packages/components/.storybook/preview.tsx
+++ packages/components/.storybook/preview.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import EDSGlobalStyles from "../src/styles/global";
-import { addDecorator } from "@storybook/react";
 
-addDecorator((storyFn) => (
-  <div dir="ltr">
-    <EDSGlobalStyles />
-    {storyFn()}
-  </div>
-));
+export const decorators = [
+  (Story) => (
+    <div dir="ltr">
+      <EDSGlobalStyles />
+      <Story />
+    </div>
+  ),
+];
