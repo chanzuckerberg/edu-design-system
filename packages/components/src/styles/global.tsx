66313dca2cfaaaee6666ef30884b3807867084d1
Annie Hu <annie.hu@chanzuckerberg.com>
2021-01-05 16:09:48 -0500

    refactor: import fonts from google and remove export

---
+++ packages/components/src/styles/global.tsx
@@ -0,0 +1,6 @@
+import React, { ReactElement } from "react";
+import { GlobalStyles } from "twin.macro";
+
+const EDSGlobalStyles = (): ReactElement => <GlobalStyles />;
+
+export default EDSGlobalStyles;
