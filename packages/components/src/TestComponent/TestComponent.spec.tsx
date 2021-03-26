9a9ce00540fd8816a5f7afe24469c6c86ee2d44b
Diedra <drater@contractor.chanzuckerberg.com>
2021-03-26 10:23:27 -0700

    chore: remove generated test component

--- packages/components/src/TestComponent/TestComponent.spec.tsx
+++
@@ -1,6 +0,0 @@
-import * as TestComponentStoryFile from "./TestComponent.stories";
-import { generateSnapshots } from "@chanzuckerberg/story-utils";
-
-describe("<TestComponent />", () => {
-  generateSnapshots(TestComponentStoryFile);
-});
