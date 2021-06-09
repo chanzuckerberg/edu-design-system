6ffcf8c7fb36ef996c6b03d6e2e2485db57110bc
Diedra <drater@contractor.chanzuckerberg.com>
2021-06-09 11:30:49 -0700

    chore(typography): add ref test to Heading

--- packages/components/src/Heading/Heading.spec.tsx
+++ packages/components/src/Heading/Heading.spec.tsx
@@ -1,6 +1,25 @@
 import * as HeadingStoryFile from "./Heading.stories";
+
+import { render, screen } from "@testing-library/react";
+import Heading from "./Heading";
+import React from "react";
 import { generateSnapshots } from "@chanzuckerberg/story-utils";
 
 describe("<Heading />", () => {
   generateSnapshots(HeadingStoryFile);
+
+  it("should pass the passthrough ref", () => {
+    const headingRef = React.createRef();
+    render(
+      <Heading ref={headingRef} size="h1" tabIndex={-1}>
+        Some Heading
+      </Heading>,
+    );
+
+    headingRef.current.focus();
+
+    const headingElement = screen.getByText("Some Heading");
+
+    expect(headingElement).toHaveFocus();
+  });
 });
