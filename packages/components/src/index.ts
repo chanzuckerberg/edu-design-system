efbf5a8b26bf5f788fb7b5865f9dbd77b617ed8e
Diedra <drater@contractor.chanzuckerberg.com>
2021-07-08 12:41:56 -0700

    chore(button): export Button component from components package
    So it can be imported and used in other repos.

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,4 @@
+export { default as Button } from "./Button";
 export { default as Clickable } from "./Clickable";
 export { default as Heading } from "./Heading";
 export { default as Text } from "./Text";
