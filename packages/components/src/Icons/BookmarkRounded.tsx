58266af450b264d43face3e97e6663d5b4dcfca9
Annie Hu <annie.hu@chanzuckerberg.com>
2021-07-29 18:18:43 -0400

    feat(icons): add material-ui rounded icons from ui kit-alpha

--- packages/components/src/Icons/BookmarkRounded.tsx
+++ packages/components/src/Icons/BookmarkRounded.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import createSvgIcon from "../SvgIcon/utils/createSvgIcon";
 
 export default createSvgIcon(
-  <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" />,
+  <path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z" />,
 );
