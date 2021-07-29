518833ea27a73ea3fea915c7ec34aedb5352c2cd
Annie Hu <annie.hu@chanzuckerberg.com>
2021-07-29 13:03:13 -0400

    chore: add usage note to Icons/index.ts

--- packages/components/src/Icons/index.ts
+++ packages/components/src/Icons/index.ts
@@ -1,5 +1,9 @@
 // Icons re-exported from here will show up in the "AllIcons" story in Storybook.
 
+// NOTE: until we enable tree-shaking, Icons should be imported directly from their file, i.e.
+//   import CheckCircleIcon from "Icons/CheckCircle"
+// and not from the Icons folder.
+
 export { default as CheckCircle } from "./CheckCircle";
 export { default as Close } from "./Close";
 export { default as Forum } from "./Forum";
