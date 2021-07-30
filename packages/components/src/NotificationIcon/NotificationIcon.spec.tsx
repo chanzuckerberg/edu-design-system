a08826845ca4329845515f8a512e640656a4fcc8
Diedra <drater@contractor.chanzuckerberg.com>
2021-07-29 17:08:34 -0700

    chore(banner): move NotificationDismissButton to /common

--- packages/components/src/NotificationIcon/NotificationIcon.spec.tsx
+++
@@ -1,6 +0,0 @@
-import * as NotificationIconStoryFile from "./NotificationIcon.stories";
-import { generateSnapshots } from "@chanzuckerberg/story-utils";
-
-describe("<NotificationIcon />", () => {
-  generateSnapshots(NotificationIconStoryFile);
-});
