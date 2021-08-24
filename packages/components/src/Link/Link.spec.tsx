32b587a4b4bc03a239dbcb6b9d82ac7e06d7c99a
Diedra <drater@contractor.chanzuckerberg.com>
2021-08-23 23:21:27 -0700

    feat(link): add Link component; change Button to always use button tag

---
+++ packages/components/src/Link/Link.spec.tsx
@@ -0,0 +1,19 @@
+import { generateSnapshots, prepareStory } from "@chanzuckerberg/story-utils";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import * as LinkStoryFile from "./Link.stories";
+
+describe("<Link />", () => {
+  generateSnapshots(LinkStoryFile);
+
+  it("forwards refs", () => {
+    const ref = React.createRef<HTMLAnchorElement>();
+    render(prepareStory(LinkStoryFile.Primary, { ref }));
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    ref.current!.focus();
+
+    const Link = screen.getByRole("link");
+    expect(Link).toHaveFocus();
+  });
+});
