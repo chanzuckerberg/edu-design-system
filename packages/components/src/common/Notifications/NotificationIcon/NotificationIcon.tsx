08656f6089573242c089da92f706d5038e71109e
Diedra <drater@contractor.chanzuckerberg.com>
2021-10-13 13:18:40 -0700

    Revert "chore: add unused import for testing lint warnings"
    This reverts commit f076ad7e43681a61976d3da36538a2ce747b4145.

--- packages/components/src/common/Notifications/NotificationIcon/NotificationIcon.tsx
+++ packages/components/src/common/Notifications/NotificationIcon/NotificationIcon.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import * as React from "react";
-import { useEffect } from "react";
 
 import CheckCircleRoundedIcon from "../../../Icons/CheckCircleRounded";
 import ForumRoundedIcon from "../../../Icons/ForumRounded";
