7f5eed35f808aecc44a5db4d7b1c0e074f081a97
Diedra <drater@contractor.chanzuckerberg.com>
2021-10-13 15:22:55 -0700

    chore(svg-icon): use MaterialUI versions of new icons

---
+++ packages/components/src/Icons/RemoveRounded.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import createSvgIcon from "../SvgIcon/utils/createSvgIcon";
+
+export default createSvgIcon(
+  <path d="M18 13H6c-.55 0-1-.45-1-1s.45-1 1-1h12c.55 0 1 .45 1 1s-.45 1-1 1z" />,
+);
--- packages/components/src/Icons/custom/RemoveRounded.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import createSvgIcon from "../../SvgIcon/utils/createSvgIcon";
-
-export default createSvgIcon(
-  <path d="M3.75 12.25C3.75 12.8 4.2 13.25 4.75 13.25H18.75C19.3 13.25 19.75 12.8 19.75 12.25C19.75 11.7 19.3 11.25 18.75 11.25H4.75C4.2 11.25 3.75 11.7 3.75 12.25Z" />,
-);
