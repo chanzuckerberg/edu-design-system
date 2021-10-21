8410af480de8931ca2079182a740ed0f75ddd76f
Jin Lee <jin.lee@chanzuckerberg.com>
2021-10-21 13:25:18 -0700

    feat(tailwind): move import tailwind utilities for storybook only

--- packages/components/.storybook/preview.tsx
+++ packages/components/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 import "@chanzuckerberg/eds-tokens/lib/css/variables.css";
 import "../src/styles/fonts.css";
 import "../src/styles/global.css";
+import "../src/styles/tailwindUtilities.css";
 
 import React from "react";
 
