aaba49a2acfb9894a893c1d0bb169074f43c7153
Diedra <drater@contractor.chanzuckerberg.com>
2021-11-03 11:33:17 -0700

    test(clickable-style): remove redundant test

--- packages/components/src/ClickableStyle/ClickableStyle.spec.tsx
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from "@chanzuckerberg/story-utils";
-import * as ClickableStyleStoryFile from "./ClickableStyle.stories";
-
-describe("<ClickableStyle />", () => {
-  generateSnapshots(ClickableStyleStoryFile);
-});
