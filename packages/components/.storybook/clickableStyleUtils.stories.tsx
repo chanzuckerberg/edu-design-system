928a1c5c7834a0f95b346ec67f617710664493c8
Diedra Rater <diedra.rater@gmail.com>
2021-11-03 10:28:10 -0700

    test(button + link): add 'RecommendedVariants' story (#692)
    * test(button + link): add 'RecommendedVariants' story
    
    * test(button + link): get tailwind jit working in button utils
    
    * test(button + link): use jit tailwind for OpenInNewRoundedIcon
    
    * test(button + link): simplify heading bottom margin
    
    * test(button + link): fix import path
    
    * chore(button + link): purge all styles in .storybook/ for jit tw
    
    * test(button + link): rename buttonUtils to clickableStyleUtils
    
    * test(button): rename Primary story to Default
    
    * test(button + link): break up recommended sections into separate stories
    
    * test(button + link): use arbitrary TW values for icon margins
    
    * test(button + link): add headings for different sizes
    
    * test(link): move all link-specific stories above grids
    
    * test(link): fix list structure in recommended variants

---
+++ packages/components/.storybook/clickableStyleUtils.stories.tsx
@@ -0,0 +1,246 @@
+import React from "react";
+import Button from "../src/Button";
+import { ClickableStyleProps } from "../src/ClickableStyle";
+import Heading from "../src/Heading";
+import AddRoundedIcon from "../src/Icons/AddRounded";
+import ArrowBackRoundedIcon from "../src/Icons/ArrowBackRounded";
+import ArrowForwardRoundedIcon from "../src/Icons/ArrowForwardRounded";
+import OpenInNewRoundedIcon from "../src/Icons/OpenInNewRounded";
+import Link from "../src/Link";
+import styles from "./clickableStyleUtils.stories.module.css";
+
+export const getStandardSet = (
+  Component: typeof Button | typeof Link,
+  componentName: "Button" | "Link",
+  variant: ClickableStyleProps<"button">["variant"],
+  color: ClickableStyleProps<"button">["color"] = "brand",
+) => (
+  <ul>
+    <li>
+      <Heading size="h3" spacing="2x">
+        Size Large
+      </Heading>
+      <ul className="grid gap-y-4">
+        <li>
+          <Component color={color} variant={variant}>
+            {componentName}
+          </Component>
+        </li>
+        <li>
+          <Component color={color} variant={variant}>
+            <ArrowBackRoundedIcon
+              className={styles.arrowBackIcon}
+              purpose="decorative"
+            />{" "}
+            {componentName}
+          </Component>
+        </li>
+        <li className="mb-4">
+          <Component color={color} variant={variant}>
+            {componentName}
+            <ArrowForwardRoundedIcon
+              className={styles.arrowForwardIcon}
+              purpose="decorative"
+            />
+          </Component>
+        </li>
+      </ul>
+    </li>
+
+    <li>
+      <Heading size="h3" spacing="2x">
+        Size Medium
+      </Heading>
+      <div className="mb-4">
+        <Component color={color} variant={variant} size="medium">
+          {componentName}
+        </Component>
+      </div>
+    </li>
+
+    <li>
+      <Heading size="h3" spacing="2x">
+        Size Small
+      </Heading>
+      <div className="mb-4">
+        <Component color={color} variant={variant} size="small">
+          {componentName}
+        </Component>
+      </div>
+    </li>
+  </ul>
+);
+
+export const getPlainRecommendedVariants = (
+  Component: typeof Button | typeof Link,
+  componentName: "Button" | "Link",
+) => (
+  <ul>
+    <li>
+      <Heading size="h3" spacing="2x">
+        Size Large
+      </Heading>
+      <ul className="grid gap-y-4">
+        <li>
+          <Component variant="plain">
+            <ArrowBackRoundedIcon
+              className={styles.arrowBackIcon}
+              purpose="decorative"
+            />{" "}
+            {componentName}
+          </Component>
+        </li>
+        <li>
+          <Component variant="plain">
+            {componentName}
+            <ArrowForwardRoundedIcon
+              className={styles.arrowForwardIcon}
+              purpose="decorative"
+            />
+          </Component>
+        </li>
+        <li className="mb-4">
+          <Component variant="plain">
+            <ArrowForwardRoundedIcon
+              className="mx-[-0.4em]"
+              purpose="informative"
+              title="forward"
+            />
+          </Component>
+        </li>
+      </ul>
+    </li>
+
+    <li>
+      <Heading size="h3" spacing="2x">
+        Size Medium
+      </Heading>
+      <ul className="grid gap-y-4">
+        <li>
+          <Component variant="plain" size="medium">
+            <ArrowBackRoundedIcon
+              className={styles.arrowBackIcon}
+              purpose="decorative"
+            />{" "}
+            {componentName}
+          </Component>
+        </li>
+        <li>
+          <Component variant="plain" size="medium">
+            {componentName}
+            <ArrowForwardRoundedIcon
+              className={styles.arrowForwardIcon}
+              purpose="decorative"
+            />
+          </Component>
+        </li>
+        <li>
+          <Component variant="plain" size="medium">
+            <AddRoundedIcon
+              purpose="informative"
+              title="add"
+              className="mx-[-0.55em]"
+            />
+          </Component>
+        </li>
+      </ul>
+    </li>
+  </ul>
+);
+
+export const getLinkRecommendedVariants = (
+  Component: typeof Button | typeof Link,
+  componentName: "Button" | "Link",
+) => (
+  <ul className="grid gap-y-4">
+    <li>
+      <Component variant="link">{componentName}</Component>
+    </li>
+    <li>
+      <Component variant="link">
+        {componentName}
+        <OpenInNewRoundedIcon
+          className="ml-1"
+          purpose="informative"
+          title="opens in a new tab"
+        />
+      </Component>
+    </li>
+  </ul>
+);
+
+export const getDestructiveRecommendedVariants = (
+  Component: typeof Button | typeof Link,
+  componentName: "Button" | "Link",
+) => (
+  <>
+    <Heading size="h3" spacing="2x">
+      Size Large
+    </Heading>
+    <ul className="grid gap-y-4">
+      <li>
+        <Component variant="flat" color="alert">
+          {componentName}
+        </Component>
+      </li>
+      <li>
+        <Component variant="flat" color="alert">
+          <ArrowBackRoundedIcon
+            className={styles.arrowBackIcon}
+            purpose="decorative"
+          />{" "}
+          {componentName}
+        </Component>
+      </li>
+    </ul>
+  </>
+);
+
+export const getAllRecommendedVariants = (
+  Component: typeof Button | typeof Link,
+  componentName: "Button" | "Link",
+) => (
+  <ul className="grid gap-y-8">
+    <li>
+      <Heading spacing="2x" size="h2">
+        Primary
+      </Heading>
+      {getStandardSet(Component, componentName, "flat")}
+    </li>
+
+    <li>
+      <Heading spacing="2x" size="h2">
+        Secondary
+      </Heading>
+      {getStandardSet(Component, componentName, "outline")}
+    </li>
+
+    <li>
+      <Heading spacing="2x" size="h2">
+        Tertiary
+      </Heading>
+      {getStandardSet(Component, componentName, "outline", "neutral")}
+    </li>
+
+    <li>
+      <Heading spacing="2x" size="h2">
+        Plain
+      </Heading>
+      {getPlainRecommendedVariants(Component, componentName)}
+    </li>
+
+    <li>
+      <Heading spacing="2x" size="h2">
+        Link
+      </Heading>
+      {getLinkRecommendedVariants(Component, componentName)}
+    </li>
+
+    <li>
+      <Heading spacing="2x" size="h2">
+        Destructive
+      </Heading>
+      {getDestructiveRecommendedVariants(Component, componentName)}
+    </li>
+  </ul>
+);
