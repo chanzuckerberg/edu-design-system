f6640a3eeed687eb25420752f906a0ee78a03be7
Diedra Rater <diedra.rater@gmail.com>
2021-11-04 15:38:16 -0700

    docs(button + link + clickable-style): explain differences between components in docstrings (#709)
    * docs(button + link + clickable-style): explain differences between components in docstrings
    
    * docs(link): 'link element' -> '<a> element'

--- packages/components/src/Button/button.tsx
+++ packages/components/src/Button/button.tsx
@@ -17,9 +17,15 @@ export type ButtonProps = ButtonHTMLElementProps & {
 /**
  * Component for making buttons that do not navigate the user to another page.
  *
- * This component is called Button because it should be used to making button elements;
- * however, it can be styled to look like a link. In terms of the look and feel of the
- * component in the UI, the Button and Link components are exactly the same.
+ * This component is called `Button` because it should be used to make `<button>` elements;
+ * however, it can be styled to look like a link.
+ *
+ * If you need to style an `<a>` element to look like a button, please use the `Link` component.
+ * If you need to style a different element or component (e.g. `Link` from `react-router`) to
+ * look like a button or link, you can use the `ClickableStyle` component.
+ *
+ * In terms of the look and feel of the component in the UI, the `Button`, and `Link`, and `ClickableStyle`
+ * components are exactly the same.
  */
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
--- packages/components/src/Link/Link.tsx
+++ packages/components/src/Link/Link.tsx
@@ -20,12 +20,15 @@ export type LinkProps = LinkHTMLElementProps & {
 /**
  * Component for making anchor tags.
  *
- * This component is called Link because it should be used to making link elements;
- * however, it can be styled to look like a button. In terms of the look and feel of the
- * component in the UI, the Link and Button components are exactly the same.
+ * This component is called Link because it should be used to make `<a>` elements;
+ * however, it can be styled to look like a button.
  *
- * Note: when using a routing component like react-router, you'll probably want to
- * use ClickableStyle and pass in the routing component via the `as` prop.
+ * If you need to style a `<button>` element to look like a link, please use the `Button` component.
+ * If you need to style a different element or component (e.g. `Link` from `react-router`) to
+ * look like a button or link, you can use the `ClickableStyle` component.
+ *
+ * In terms of the look and feel of the component in the UI, the `Button`, and `Link`, and `ClickableStyle`
+ * components are exactly the same.
  */
 const Link = forwardRef<HTMLAnchorElement, LinkProps>(
   ({ variant = "link", color = "brand", size = "large", ...rest }, ref) => {
