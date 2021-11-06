e4a4d60228cf23d5ebb5654f4d09f123d7ffe947
Andrew Huth <ahuth@chanzuckerberg.com>
2021-11-06 06:07:16 -0700

    test(storybook): use @storybook/testing-react for writing tests based on stories
    Instead of our own `prepareStory`, now that Storybook has released an official utility.
    
    Note that @storybook/testing-react does not currently support passing refs to components using
    `React.forwardRef`. This makes sense, because what's being rendered is the story (which does not
    accept refs), not the component (which does). In any case, we work around it here by directly
    using the actual component in the 2 tests relying on refs.

--- packages/components/src/Button/button.spec.tsx
+++ packages/components/src/Button/button.spec.tsx
@@ -1,31 +1,30 @@
-import { generateSnapshots, prepareStory } from "@chanzuckerberg/story-utils";
+import { generateSnapshots } from "@chanzuckerberg/story-utils";
+import { composeStories } from "@storybook/testing-react";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import Button from "./button";
 import * as ButtonStoryFile from "./button.stories";
 
+const { Default } = composeStories(ButtonStoryFile);
+
 describe("<Button />", () => {
   generateSnapshots(ButtonStoryFile);
 
   it("passes test ids down properly", () => {
-    render(
-      <Button data-testid="example-test-id">Button with data-testid</Button>,
-    );
+    render(<Default data-testid="example-test-id" />);
     expect(screen.getByTestId("example-test-id")).toMatchSnapshot();
   });
 
   it("passes class names down properly", () => {
     render(
-      <Button className="exampleClassName" data-testid="example-class-name">
-        Button with example className
-      </Button>,
+      <Default className="exampleClassName" data-testid="example-class-name" />,
     );
     expect(screen.getByTestId("example-class-name")).toMatchSnapshot();
   });
 
   it("forwards refs", () => {
     const ref = React.createRef<HTMLButtonElement>();
-    render(prepareStory(ButtonStoryFile.Default, { ref }));
+    render(<Button ref={ref}>Hi</Button>);
 
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ref.current!.focus();
--- packages/components/src/Link/Link.spec.tsx
+++ packages/components/src/Link/Link.spec.tsx
@@ -1,34 +1,39 @@
-import { generateSnapshots, prepareStory } from "@chanzuckerberg/story-utils";
+import { generateSnapshots } from "@chanzuckerberg/story-utils";
+import { composeStories } from "@storybook/testing-react";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import Link from "./Link";
 import * as LinkStoryFile from "./Link.stories";
 
+const { Default } = composeStories(LinkStoryFile);
+
 describe("<Link />", () => {
   generateSnapshots(LinkStoryFile);
 
   it("passes test ids down properly", () => {
-    render(<Link data-testid="example-test-id">Link with data-testid</Link>);
+    render(<Default data-testid="example-test-id" />);
     expect(screen.getByTestId("example-test-id")).toMatchSnapshot();
   });
 
   it("passes class names down properly", () => {
     render(
-      <Link className="exampleClassName" data-testid="example-class-name">
-        Link with example className
-      </Link>,
+      <Default className="exampleClassName" data-testid="example-class-name" />,
     );
     expect(screen.getByTestId("example-class-name")).toMatchSnapshot();
   });
 
   it("forwards refs", () => {
     const ref = React.createRef<HTMLAnchorElement>();
-    render(prepareStory(LinkStoryFile.Default, { ref }));
+    render(
+      <Link href="www.example.com" ref={ref}>
+        Hi
+      </Link>,
+    );
 
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     ref.current!.focus();
 
-    const Link = screen.getByRole("link");
-    expect(Link).toHaveFocus();
+    const link = screen.getByRole("link");
+    expect(link).toHaveFocus();
   });
 });
