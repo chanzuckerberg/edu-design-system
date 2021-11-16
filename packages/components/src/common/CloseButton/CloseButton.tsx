286286cf6c0fed1bfbe3ee6554a45f3531e9781d
Diedra <drater@contractor.chanzuckerberg.com>
2021-11-16 13:18:39 -0800

    chore(close-button): add hover color, aria-label prop, and default prop vals

--- packages/components/src/common/CloseButton/CloseButton.tsx
+++ packages/components/src/common/CloseButton/CloseButton.tsx
@@ -2,25 +2,27 @@ import clsx from "clsx";
 import React from "react";
 import Button from "../../Button";
 import CloseRoundedIcon from "../../Icons/CloseRounded";
-import { NotificationVariant } from "../Notifications/NotificationIcon";
 import styles from "./CloseButton.module.css";
 
+export type Variant = "brand" | "neutral" | "success" | "warning" | "alert";
+
 type CloseButtonProps = {
-  className?: string;
   /**
-   * The default color of the icon. Does not effect hover state.
+   * The color theme of the icon. Also affects the hover state.
    */
-  color: NotificationVariant;
-  onClose: () => void;
+  color: Variant;
   /**
-   * Size of the icon. Does not button size. The button is larger than the
+   * Size of the icon. Does not affect actual button size. The button is larger than the
    * icon to ensure the hit box is large enough (for accessibility).
    *
    * The string should be some number + px, rem, em, vh, etc. Ex: 2rem.
    * Recommendation: use "EdsSizeLineHeight" tokens from
    * @chanzuckerberg/eds-tokens/json/variables.json
    */
-  size: string;
+  size?: string;
+  "aria-label"?: string;
+  onClose: () => void;
+  className?: string;
 };
 
 /**
@@ -29,14 +31,31 @@ type CloseButtonProps = {
  * Consumers need to set the --close-hover-color variable in their styles
  * in order for the hover state to work correctly.
  */
-const CloseButton = ({ className, color, onClose, size }: CloseButtonProps) => (
+const CloseButton = ({
+  className,
+  color = "neutral",
+  onClose,
+  size = "2rem",
+  "aria-label": ariaLabel = "close",
+  ...rest
+}: CloseButtonProps) => (
   <Button
-    className={clsx(styles.close, className)}
+    className={clsx(
+      styles.button,
+      className,
+      // Color props
+      color === "brand" && styles.colorBrand,
+      color === "neutral" && styles.colorNeutral,
+      color === "success" && styles.colorSuccess,
+      color === "warning" && styles.colorWarning,
+      color === "alert" && styles.colorAlert,
+    )}
     color={color}
     onClick={onClose}
     variant="link"
+    {...rest}
   >
-    <CloseRoundedIcon purpose="informative" size={size} title="close" />
+    <CloseRoundedIcon purpose="informative" size={size} title={ariaLabel} />
   </Button>
 );
 
