1bbfc5551a52caa9dbb1af065d0bad99b6265caa
Jin Lee <jin.lee@chanzuckerberg.com>
2022-02-18 09:19:04 -0800

    chore: export closebutton from src

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,6 +2,7 @@ export { default as Banner } from "./Banner";
 export { default as Button } from "./Button";
 export { default as ButtonGroup } from "./ButtonGroup";
 export { default as Checkbox } from "./Checkbox";
+export { default as CloseButton } from "./CloseButton";
 export { default as ClickableStyle } from "./ClickableStyle";
 export { default as DropdownButton } from "./DropdownButton";
 export { default as FormGroup } from "./FormGroup";
