c01c1029cbacbf51bc442fefbe2438931006563d
Brad Frost <brad@bradfrost.com>
2022-03-16 09:01:16 -0400

    Feature/page shell (#891)
    * chore: remove footer
    
    * feat: add sidebar width token
    
    * chore: remove table-header variant
    
    * fix: remove c- prefix from recipe template
    
    * feat: add PageShell component
    
    * chore: lock files
    
    * feat: page layout styles
    
    * feat: wip logo
    
    * feat: page layout styles
    
    * chore: remove table-header button style
    
    * chore: remove layout section styles in parent component
    
    * fix: remove unneeded Sass formatting syntax
    
    * feat: provide further detail for component API language
    
    * fix: update component plop export and path
    
    * chore: remove unnecessary component index.ts file
    
    * feat: wip avatar component
    
    * Round value up to 13.5rem
    
    * Remove package-lock.json

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,6 +2,8 @@ import clsx from 'clsx';
 import React from 'react';
 import styles from './Avatar.module.css';
 
+import { AvatarImage } from '../AvatarImage/AvatarImage';
+
 export interface Props {
   /**
    * CSS class names that can be appended to the component.
@@ -16,7 +18,7 @@ export const Avatar: React.FC<Props> = ({ className, ...other }) => {
   const componentClassName = clsx(styles['avatar'], className, {});
   return (
     <div className={componentClassName} {...other}>
-      Hello!
+      <AvatarImage />
     </div>
   );
 };
