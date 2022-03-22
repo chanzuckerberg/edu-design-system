2795558c6986d440892a58a5c867cdfbf70f3ce8
ifrost1 <ifrost@contractor.chanzuckerberg.com>
2022-03-22 19:32:31 -0400

    feature: scaffold project overview

--- .storybook/pages/ProjectOverview/ProjectOverview.tsx
+++ .storybook/pages/ProjectOverview/ProjectOverview.tsx
@@ -1,33 +1,51 @@
 import React from 'react';
 
-import { PageHeader, Panel, Tabs, Tab } from '../../../src';
+import { PageHeader, Panel, Tabs, Tab, Breadcrumbs, BreadcrumbsItem, Button, Hr } from '../../../src';
 
 import { PageShell } from '../../recipes/PageShell/PageShell';
 
 export const ProjectOverview: React.FC = () => (
   <PageShell>
-    <PageHeader title="My Body Book" />
+    <Breadcrumbs>
+    <BreadcrumbsItem text="My Courses" href="#" />
+    <BreadcrumbsItem text="Disciplinary Science 7" />
+  </Breadcrumbs>
+    <PageHeader 
+    title="Feudal Honor Codes and Values"
+    right={
+      <Button text="View plan" variant="bare" iconPosition="after" iconName="arrow-narrow-right" />
+    }
+     />
     <Panel>
-      <Tabs>
-        <Tab title="Overview">
-          <div className="fpo">Tab 1 Content</div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
-            dolores alias, cumque quos perspiciatis voluptate itaque asperiores.
-            Id sunt voluptatem dolores, officia facere reiciendis doloremque
-            eveniet nihil soluta laboriosam beatae.
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
-            dolores alias, cumque quos perspiciatis voluptate itaque asperiores.
-            Id sunt voluptatem dolores, officia facere reiciendis doloremque
-            eveniet nihil soluta laboriosam beatae.
-          </p>
-        </Tab>
-        <Tab title="Plans">
-          <div className="fpo">Tab 2 Content</div>
-        </Tab>
-      </Tabs>
+      <div className="fpo">
+        <div>List Detail Component</div>
+        <div className="fpo">Heading</div>
+        <div className="fpo">Text Passage large</div>
+        <div className="fpo">See more component (truncated text)</div>
+        <div className="fpo">Heading</div>
+        <div className="fpo">List component (Could include numbers/Q for question)</div>
+        <Hr />
+        <div className="fpo">
+          <div>Section title with image in front (Power Focus Areas)</div>
+          <div className="fpo">Tooltip (up for discussion)</div>
+          <div className="fpo">Hoverable/Linkable Card Recipe (naming TBD)</div>
+        </div>
+        <div className="fpo">
+          <div>Section title with image in front (Additional Focus Areas)</div>
+          <div className="fpo">
+            <div>Hoverable/Linkable Card Recipe (naming TBD)</div>
+            <div className="fpo">Badge? Status badge?</div>
+          </div>
+          <div className="fpo">
+            <div>Hoverable/Linkable Card Recipe (naming TBD)</div>
+            <div className="fpo">Badge? Status badge?</div>
+          </div>
+        </div>
+        <div className="fpo">
+          <div>Cognitive skills (smaller section title than ones above)</div>
+          <div className="fpo">Text passage with links?</div>
+        </div>
+      </div>
     </Panel>
   </PageShell>
 );
