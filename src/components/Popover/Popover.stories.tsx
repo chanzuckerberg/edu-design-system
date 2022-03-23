8c997f8de5ebbcf77febcf1f76e90e0d46e0034f
csnizik <csnizik@tulane.edu>
2022-03-23 15:13:56 -0500

    chore(notifications): rm unused options from sb

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -12,9 +12,3 @@ const Template: Story<Props> = (args) => <PopoverExample {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
-
-export const Small = Template.bind({});
-Small.args = { size: 'sm' };
-
-export const Large = Template.bind({});
-Large.args = { size: 'lg' };
