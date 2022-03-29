34af1bc77125cafb4c776d9e94179ea43c51d628
David Tu <40640760+silvestry@users.noreply.github.com>
2022-03-29 15:57:12 -0700

    feat(dropdown): add compact button as option (#905)
    * feat(dropdown): add compact button as option
    
    * chore(dropdown): add variant prop
    
    * chore(dropdown): add optionsClassName prop
    
    * chore(dropdown): make optionsClassName optional for compact variant
    
    * chore(dropdown): add compactDropdownButton style
    
    * chore(dropdown): document width property requirement
    
    * chore(dropdown): simplify code

--- packages/components/src/Dropdown/Dropdown.tsx
+++ packages/components/src/Dropdown/Dropdown.tsx
@@ -1,9 +1,17 @@
 import { Listbox } from "@headlessui/react";
 import clsx from "clsx";
-import React, { ReactNode, ComponentProps, ElementType } from "react";
+import React, {
+  ReactNode,
+  ComponentProps,
+  ElementType,
+  useContext,
+} from "react";
 import DropdownButton from "../DropdownButton";
 import CheckRoundedIcon from "../Icons/CheckRounded";
 import styles from "./Dropdown.module.css";
+
+export type OptionsAlignType = "left" | "right";
+export type VariantType = "compact" | "full";
 
 type ListboxProps = ComponentProps<typeof Listbox>;
 type DropdownProps = ListboxProps & {
@@ -48,6 +56,23 @@ type DropdownProps = ListboxProps & {
    * Optional className for additional styling.
    */
   className?: string;
+  /**
+   * The style of the dropdown.
+   *
+   * Compact renders dropdown button that is only as wide as the content.
+   */
+  variant?: VariantType;
+  /**
+   * Align dropdown menu to the left (default) or right of dropdown button.
+   */
+  optionsAlign?: OptionsAlignType;
+  /**
+   * Optional className for additional options menu styling.
+   *
+   * When not using the compact variant, if optionsClassName is provided please
+   * include the width property to define the options menu width.
+   */
+  optionsClassName?: string;
 };
 
 type RenderProp<Arg> = (arg: Arg) => ReactNode;
@@ -165,6 +190,63 @@ function childrenHaveLabelComponent(children?: ReactNode): boolean {
  *   <Dropdown aria-label="Options:" buttonText="Select" options={options} />
  * );
  * ```
+ *
+ * For compact variant, add variant="compact" and optionally optionsAlign.
+ *
+ * Examples:
+ *
+ *
+ * ```
+ * return (
+ *   <Dropdown
+ *     aria-label="Options"
+ *     optionsAlign="right"
+ *     variant="compact"
+ *   >
+ *     <Dropdown.Options>
+ *       <Dropdown.Option>Option 1</Dropdown.Option>
+ *       <Dropdown.Option>Option 2</Dropdown.Option>
+ *       <Dropdown.Option>Option 3</Dropdown.Option>
+ *     </Dropdown.Options>
+ *   </Dropdown>
+ * );
+ * ```
+ *
+ * ```
+ * const options = [
+ *   ...
+ * ];
+ *
+ * return (
+ *   <Dropdown
+ *     aria-label="Options"
+ *     options={options}
+ *     optionsAlign="right"
+ *     variant="compact"
+ *   />
+ * );
+ * ```
+ *
+ * For dropdown that differs in button and options menu width, style button
+ * width with className and options menu width with optionsClassName.
+ *
+ * Example:
+ *
+ * ```
+ * const options = [
+ *   ...
+ * ];
+ *
+ * return (
+ *   <Dropdown
+ *     aria-label="Options"
+ *     className="w-60"
+ *     options={options}
+ *     optionsAlign="right"
+ *     optionsClassName="w-96"
+ *   />
+ * );
+ * ```
  */
 function Dropdown(props: DropdownProps) {
   const {
@@ -174,8 +256,13 @@ function Dropdown(props: DropdownProps) {
     options,
     children,
     "aria-label": ariaLabel,
+    variant,
+    optionsAlign,
+    optionsClassName,
     ...rest
   } = props;
+
+  const compact = variant === "compact";
 
   if (process.env.NODE_ENV !== "production") {
     if (children && [labelText, buttonText, options].some((prop) => !!prop)) {
@@ -197,7 +284,11 @@ function Dropdown(props: DropdownProps) {
   }
 
   const sharedProps = {
-    className: clsx(styles.dropdown, className),
+    className: clsx(
+      styles.dropdown,
+      className,
+      compact && styles.compactDropdown,
+    ),
     // Provide a wrapping <div> element for the dropdown. This is needed so that any props
     // passed directly to this component have a corresponding DOM element to receive them.
     // Otherwise we get an error.
@@ -205,17 +296,25 @@ function Dropdown(props: DropdownProps) {
     ...rest,
   };
 
+  const contextValue = Object.assign(
+    { compact },
+    optionsAlign ? { optionsAlign } : null,
+    optionsClassName ? { optionsClassName } : null,
+  );
+
   if (typeof children === "function") {
     return (
-      <Listbox
-        {...sharedProps}
-        // We prefer to pass the aria-label in via an invisible DropdownLabel, but we can't
-        // easily pass down function children with component children, so we'll settle for
-        // using a standard aria-label in this case.
-        aria-label={ariaLabel}
-      >
-        {children}
-      </Listbox>
+      <DropdownContext.Provider value={contextValue}>
+        <Listbox
+          {...sharedProps}
+          // We prefer to pass the aria-label in via an invisible DropdownLabel, but we can't
+          // easily pass down function children with component children, so we'll settle for
+          // using a standard aria-label in this case.
+          aria-label={ariaLabel}
+        >
+          {children}
+        </Listbox>
+      </DropdownContext.Provider>
     );
   }
 
@@ -249,8 +348,18 @@ function Dropdown(props: DropdownProps) {
     </>
   );
 
-  return <Listbox {...sharedProps}>{childrenToUse}</Listbox>;
+  return (
+    <DropdownContext.Provider value={contextValue}>
+      <Listbox {...sharedProps}>{childrenToUse}</Listbox>
+    </DropdownContext.Provider>
+  );
 }
+
+const DropdownContext = React.createContext<{
+  compact?: boolean;
+  optionsAlign?: OptionsAlignType;
+  optionsClassName?: string;
+}>({});
 
 const DropdownLabel = (props: { className?: string; children: ReactNode }) => {
   const { children, className } = props;
@@ -266,6 +375,8 @@ const DropdownTrigger = function (
   props: PropsWithRenderProp<{ disabled: boolean; open: boolean }>,
 ) {
   const { children, className, ...rest } = props;
+  const { compact } = useContext(DropdownContext);
+
   return (
     <Listbox.Button
       // Render as a fragment instead of the default element. We're rendering our own element in
@@ -277,7 +388,11 @@ const DropdownTrigger = function (
       {typeof children === "function" ? (
         children
       ) : (
-        <DropdownButton className={className}>{children}</DropdownButton>
+        <DropdownButton
+          className={clsx(className, compact && styles.compactDropdownButton)}
+        >
+          {children}
+        </DropdownButton>
       )}
     </Listbox.Button>
   );
@@ -287,8 +402,19 @@ const DropdownOptions = function (
   props: PropsWithRenderProp<{ open: boolean }>,
 ) {
   const { className, ...rest } = props;
+  const { compact, optionsAlign, optionsClassName } =
+    useContext(DropdownContext);
+
   return (
-    <Listbox.Options className={clsx(styles.options, className)} {...rest} />
+    <Listbox.Options
+      className={clsx(
+        styles.options,
+        className,
+        optionsAlign === "right" && styles.optionsAlignRight,
+        optionsClassName || (!compact && styles.optionsFullWidth),
+      )}
+      {...rest}
+    />
   );
 };
 
