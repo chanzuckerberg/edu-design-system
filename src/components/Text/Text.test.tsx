f03739a96ea03d75682b0261155bb5eef002ca2f
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2022-04-04 15:19:46 -0700

    chore: move text to next (#899)
    BREAKING CHANGE: removed and renamed props

---
+++ src/components/Text/Text.test.tsx
@@ -0,0 +1,45 @@
+import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import { render, screen } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { Text } from './Text';
+import * as TextStoryFile from './Text.stories';
+
+describe('<Text />', () => {
+  generateSnapshots(TextStoryFile);
+
+  it('should add the passthrough className', () => {
+    render(
+      <Text size="body" className="passthrough">
+        Some Text
+      </Text>,
+    );
+    expect(screen.getByText('Some Text').classList).toContain('passthrough');
+  });
+  it('should handle refs', async () => {
+    const HelperComponent = ({ as }: { as: 'p' | 'span' }) => {
+      const refContainer = React.useRef(null);
+      const onButtonClick = () => {
+        expect(refContainer.current).toBe(
+          screen.getByText(`Ref container parent test ${as}`),
+        );
+      };
+      return (
+        <>
+          <Text as={as} ref={refContainer}>
+            Ref container parent test {as}
+          </Text>
+          <button onClick={onButtonClick}>Test ref</button>
+        </>
+      );
+    };
+    const { rerender } = render(<HelperComponent as="p" />);
+    expect(screen.getByText('Ref container parent test p')).toBeTruthy();
+    userEvent.click(screen.getByRole('button'));
+
+    rerender(<HelperComponent as="span" />);
+    expect(screen.getByText('Ref container parent test span')).toBeTruthy();
+    userEvent.click(screen.getByRole('button'));
+  });
+});
