c0982a8db56c7e9d987b1ee32a3b6a183175da32
Annie Hu <annie.hu@chanzuckerberg.com>
2022-04-06 14:15:23 -0400

    test: stub svg files and functions

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,10 @@ module.exports = {
   coverageDirectory: 'coverage',
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
-    svg4everybody: 'identity-obj-proxy',
     'react-portal': 'identity-obj-proxy',
     shortid: 'identity-obj-proxy',
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      'identity-obj-proxy',
+      '<rootDir>/jest/mocks/fileMock.js',
   },
   preset: 'ts-jest',
   roots: ['<rootDir>/src'],
