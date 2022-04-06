95c1fbfc8c30893f25fd2e889512d0371478c909
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2022-04-06 09:51:34 -0700

    feat: move tag to next (#941)
    BREAKING CHANGE: props renamed, added text prop

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,31 +1,25 @@
-import clsx from "clsx";
-import React from "react";
-import Text from "../Text";
-import styles from "./Tag.module.css";
+import clsx from 'clsx';
+import React from 'react';
+import styles from './Tag.module.css';
+import Text from '../Text';
 
-export type Color =
-  | "neutral"
-  | "error"
-  | "success"
-  | "warning"
-  | "yield"
-  | "brand";
+export const VARIANTS = [
+  'default',
+  'go',
+  'yield',
+  'stop',
+  'warning',
+  'brand',
+] as const;
 
-export const stylesByColor = {
-  neutral: styles.colorNeutral,
-  warning: styles.colorWarning,
-  brand: styles.colorBrand,
-  error: styles.colorError,
-  yield: styles.colorYellow,
-  success: styles.colorSuccess,
-};
+export type Variant = typeof VARIANTS[number];
 
 type Props = {
   /**
    * The color variant of the tag. New variants should be supported in the flow type,
    * in the VARIANTS array, and by its separate style in CSS file.
    */
-  variant?: Color;
+  variant?: Variant;
   /**
    * CSS class names that can be appended to the component for styling.
    */
@@ -47,30 +41,34 @@ type Props = {
 
 /**
  * ```ts
- * import {Tag} from "@chanzuckerberg/eds";
+ * import {Tag} from "@chanzuckerberg/eds-components";
  * ```
  *
  * This component provides a tag (pill shaped badge) wrapper.
  */
-function Tag({ variant, text, className, icon, hasOutline }: Props) {
+export const Tag = ({
+  variant = 'default',
+  className,
+  icon,
+  text,
+  hasOutline = false,
+}: Props) => {
   return (
     <Text
       as="span"
       className={clsx(
         className,
-        styles.tag,
-        variant && stylesByColor[variant],
-        hasOutline && styles.outline,
+        styles['tag'],
+        styles[`tag--${variant}`],
+        hasOutline && styles['tag--outline'],
       )}
       size="sm"
       weight="bold"
     >
       {icon}
-      {/* No width space to ensure height of contents */}
-      {"\u200B"}
-      {text && <span className={styles.body}>{text}</span>}
+      {text && <span className={styles['tag__body']}>{text}</span>}
     </Text>
   );
-}
+};
 
 export default Tag;
