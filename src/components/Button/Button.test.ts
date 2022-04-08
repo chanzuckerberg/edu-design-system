6ae7ddfa051443745ac8798982335ae72c0d0003
Diedra Rater <diedra.rater@gmail.com>
2022-04-08 08:51:37 -0700

    test(button): port over tests from main

--- src/components/Button/Button.test.ts
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as ButtonStoryFile from './Button.stories';
-
-describe('<Button />', () => {
-  generateSnapshots(ButtonStoryFile);
-});
