062363126953a1dae4c68c5bc1f81e7d8737065f
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2022-04-08 09:55:30 -0700

    feat: move checkbox to next (#959)
    BREAKING CHANGE: subimports

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,23 +1,192 @@
-import { Story, Meta } from '@storybook/react';
+import type { StoryObj, Meta } from '@storybook/react';
 import React from 'react';
+import { Checkbox } from './Checkbox';
+import styles from './Checkbox.stories.module.css';
+import CheckboxInput from '../CheckboxInput';
+import CheckboxLabel from '../CheckboxLabel';
 
-import { Checkbox, Props } from './Checkbox';
+const defaultArgs = {
+  disabled: false,
+  label: 'Checkbox',
+};
 
 export default {
-  title: 'Atoms/Forms/Checkbox',
+  title: 'Molecules/Forms/Checkbox',
   component: Checkbox,
-} as Meta;
+  args: defaultArgs,
+  argTypes: {
+    // For some reason Storybook does not infer all `checked` correctly;
+    checked: {
+      control: 'radio',
+      options: [true, false, 'indeterminate'],
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          margin: '0.25rem', // Provides spacing to see focus indicator around checkbox.
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<Args>;
 
-const Template: Story<Props> = (args) => <Checkbox {...args} />;
+type Args = React.ComponentProps<typeof Checkbox>;
 
-export const Default = Template.bind({});
-Default.args = {};
+/**
+ * Controlled example to make checked stories interactive.
+ */
+function CheckedExample(args: Args) {
+  const [checked, setChecked] = React.useState(true);
+  const handleChange = () => {
+    setChecked(!checked);
+  };
 
-export const Checked = Template.bind({});
-Checked.args = { checked: true };
+  return <Checkbox checked={checked} onChange={handleChange} {...args} />;
+}
 
-export const Indeterminate = Template.bind({});
-Indeterminate.args = { checked: true, indeterminate: true };
+export const Default: StoryObj<Args> = {};
 
-export const Disabled = Template.bind({});
-Disabled.args = { disabled: true };
+export const Checked: StoryObj<Args> = {
+  render: () => <CheckedExample {...defaultArgs} />,
+};
+
+export const Small: StoryObj<Args> = {
+  ...Default,
+  args: {
+    size: 'small',
+  },
+};
+
+export const SmallChecked: StoryObj<Args> = {
+  render: () => <CheckedExample size="small" {...defaultArgs} />,
+};
+
+export const Indeterminate: StoryObj<Args> = {
+  ...Default,
+  args: {
+    checked: 'indeterminate',
+    readOnly: true,
+  },
+};
+
+export const Disabled = {
+  render: () => (
+    <table style={{ borderSpacing: '2rem' }}>
+      <tbody>
+        {[false, true, 'indeterminate' as const].map((checked, i) => (
+          <tr key={i}>
+            <td>
+              <Checkbox checked={checked} disabled label="Disabled" />
+            </td>
+            <td>
+              <Checkbox checked={checked} label="Default" readOnly />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ),
+  parameters: {
+    axe: {
+      disabledRules: ['color-contrast'],
+    },
+  },
+};
+
+export const WithoutVisibleLabel: StoryObj<Args> = {
+  ...Default,
+  args: {
+    'aria-label': 'a checkbox has no name',
+    label: undefined,
+  },
+  render: (args) => (
+    <>
+      <Checkbox {...args} />
+      <Checkbox {...args} checked readOnly />
+      <Checkbox {...args} checked="indeterminate" readOnly />
+      <Checkbox {...args} disabled />
+      <Checkbox {...args} checked disabled />
+      <Checkbox {...args} checked="indeterminate" disabled />
+    </>
+  ),
+};
+
+export const LongLabels = {
+  render: () => {
+    const label = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+
+    return (
+      <div className={styles['longlabels--grid']}>
+        <Checkbox label={label} />
+        <Checkbox label={label} size="small" />
+        <Checkbox label={label} disabled />
+        <Checkbox label={label} disabled size="small" />
+      </div>
+    );
+  },
+  parameters: {
+    axe: {
+      disabledRules: ['color-contrast'],
+    },
+  },
+};
+
+// For visual regression testing
+export const LabelsOnly = {
+  args: {
+    htmlFor: 'id',
+  },
+  render: (args) => (
+    <>
+      <CheckboxLabel {...args} text="Medium label" />
+      <br />
+      <CheckboxLabel {...args} size="small" text="Small label" />
+      <br />
+      <CheckboxLabel
+        {...args}
+        text="Long medium label lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      />
+      <br />
+      <CheckboxLabel
+        {...args}
+        size="small"
+        text="Long small label lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      />
+      <br />
+      <CheckboxLabel {...args} disabled text="Medium disabled" />
+      <br />
+      <CheckboxLabel {...args} disabled size="small" text="Small disabled" />
+      <br />
+      <CheckboxLabel
+        {...args}
+        disabled
+        text="Long medium disabled label lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      />
+      <br />
+      <CheckboxLabel
+        {...args}
+        disabled
+        size="small"
+        text="Long small disabled label lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      />
+    </>
+  ),
+  parameters: {
+    axe: {
+      disabledRules: ['color-contrast'],
+    },
+  },
+};
+
+export const WithCustomPositioning = {
+  render: () => (
+    <span>
+      <CheckboxLabel htmlFor="test" text="label on left" />
+      <CheckboxInput id="test" />
+    </span>
+  ),
+};
--- src/components/CheckboxInput/CheckboxInput.tsx
+++ src/components/CheckboxInput/CheckboxInput.tsx
@@ -1,10 +1,10 @@
-import clsx from "clsx";
-import React from "react";
-import styles from "./CheckboxInput.module.css";
+import clsx from 'clsx';
+import React from 'react';
+import styles from './CheckboxInput.module.css';
 
 type CheckboxHTMLElementProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
-  "checked" | "id" | "size"
+  'checked' | 'id' | 'size'
 >;
 
 export type CheckboxInputProps = CheckboxHTMLElementProps & {
@@ -13,7 +13,7 @@ export type CheckboxInputProps = CheckboxHTMLElementProps & {
    * "indeterminate" can be used when a checkbox visually represents
    * a list of checkboxes that is "partially" checked.
    */
-  checked?: boolean | "indeterminate";
+  checked?: boolean | 'indeterminate';
   /**
    * Additional classnames passed in for styling.
    */
@@ -25,7 +25,7 @@ export type CheckboxInputProps = CheckboxHTMLElementProps & {
 };
 
 interface SvgStyle extends React.CSSProperties {
-  "--checkbox-svg-size": string;
+  '--checkbox-svg-size': string;
 }
 
 /**
@@ -36,7 +36,7 @@ const svgViewBoxSize = 20;
 const svgSizeRem = `${svgViewBoxSize / 16}rem`;
 export const svgStyle: SvgStyle = {
   /* stylelint-disable-next-line value-keyword-case */
-  "--checkbox-svg-size": svgSizeRem,
+  '--checkbox-svg-size': svgSizeRem,
 };
 
 /**
@@ -46,8 +46,8 @@ export const svgStyle: SvgStyle = {
  */
 const CheckboxSvg = ({ indeterminate }: { indeterminate?: boolean }) => {
   const checkmarkPath = indeterminate
-    ? "M 14.00,9.00 C 14.55,9.00 15.00,9.45 15.00,10.00 15.00,10.00 15.00,10.00 15.00,10.00 15.00,10.55 14.55,11.00 14.00,11.00 14.00,11.00 6.00,11.00 6.00,11.00 5.45,11.00 5.00,10.55 5.00,10.00 5.00,10.00 5.00,10.00 5.00,10.00 5.00,9.45 5.45,9.00 6.00,9.00 6.00,9.00 14.00,9.00 14.00,9.00 Z"
-    : "M15.5605 5.2094C15.447 5.12571 15.3181 5.06522 15.1811 5.03137C15.0442 4.99752 14.902 4.99099 14.7625 5.01214C14.6231 5.0333 14.4892 5.08172 14.3684 5.15465C14.2477 5.22758 14.1425 5.32359 14.0589 5.43718L8.96703 12.3478L5.72917 9.7572C5.50592 9.58618 5.22455 9.50943 4.94539 9.54341C4.66622 9.57738 4.41148 9.71937 4.23576 9.93893C4.06004 10.1585 3.97733 10.4382 4.00536 10.718C4.03339 10.9978 4.16993 11.2555 4.38572 11.4359L8.49687 14.7244C8.61048 14.8133 8.74065 14.8786 8.87979 14.9166C9.01893 14.9546 9.16424 14.9645 9.30724 14.9457C9.45024 14.9269 9.58805 14.8798 9.71264 14.8071C9.83723 14.7344 9.94609 14.6377 10.0329 14.5225L15.7917 6.71187C15.8753 6.59806 15.9356 6.4689 15.9691 6.33178C16.0027 6.19465 16.009 6.05225 15.9875 5.91272C15.966 5.77318 15.9173 5.63925 15.844 5.51857C15.7707 5.39789 15.6744 5.29283 15.5605 5.2094Z";
+    ? 'M 14.00,9.00 C 14.55,9.00 15.00,9.45 15.00,10.00 15.00,10.00 15.00,10.00 15.00,10.00 15.00,10.55 14.55,11.00 14.00,11.00 14.00,11.00 6.00,11.00 6.00,11.00 5.45,11.00 5.00,10.55 5.00,10.00 5.00,10.00 5.00,10.00 5.00,10.00 5.00,9.45 5.45,9.00 6.00,9.00 6.00,9.00 14.00,9.00 14.00,9.00 Z'
+    : 'M15.5605 5.2094C15.447 5.12571 15.3181 5.06522 15.1811 5.03137C15.0442 4.99752 14.902 4.99099 14.7625 5.01214C14.6231 5.0333 14.4892 5.08172 14.3684 5.15465C14.2477 5.22758 14.1425 5.32359 14.0589 5.43718L8.96703 12.3478L5.72917 9.7572C5.50592 9.58618 5.22455 9.50943 4.94539 9.54341C4.66622 9.57738 4.41148 9.71937 4.23576 9.93893C4.06004 10.1585 3.97733 10.4382 4.00536 10.718C4.03339 10.9978 4.16993 11.2555 4.38572 11.4359L8.49687 14.7244C8.61048 14.8133 8.74065 14.8786 8.87979 14.9166C9.01893 14.9546 9.16424 14.9645 9.30724 14.9457C9.45024 14.9269 9.58805 14.8798 9.71264 14.8071C9.83723 14.7344 9.94609 14.6377 10.0329 14.5225L15.7917 6.71187C15.8753 6.59806 15.9356 6.4689 15.9691 6.33178C16.0027 6.19465 16.009 6.05225 15.9875 5.91272C15.966 5.77318 15.9173 5.63925 15.844 5.51857C15.7707 5.39789 15.6744 5.29283 15.5605 5.2094Z';
 
   return (
     <svg
@@ -57,7 +57,7 @@ const CheckboxSvg = ({ indeterminate }: { indeterminate?: boolean }) => {
       width={svgSizeRem}
     >
       <rect
-        className={styles["checkmark__outline"]}
+        className={styles['checkmark__outline']}
         fill="none"
         height="18"
         rx="3"
@@ -68,7 +68,7 @@ const CheckboxSvg = ({ indeterminate }: { indeterminate?: boolean }) => {
         y="1"
       />
       <path
-        className={styles["checkmark__content"]}
+        className={styles['checkmark__content']}
         d={checkmarkPath}
         fill="none"
       />
@@ -87,9 +87,9 @@ export const CheckboxInput = React.forwardRef<
   // Make indeterminate checkbox visually match the colors of a
   // checked state, but announce itself as "mixed" to screen readers
   const checkedProps =
-    checked === "indeterminate"
+    checked === 'indeterminate'
       ? {
-          "aria-checked": "mixed" as const,
+          'aria-checked': 'mixed' as const,
           checked: true,
         }
       : {
@@ -99,12 +99,12 @@ export const CheckboxInput = React.forwardRef<
   return (
     <span
       className={clsx(
-        styles["input__wrapper"],
-        disabled && styles["input__wrapper--disabled"],
+        styles['input__wrapper'],
+        disabled && styles['input__wrapper--disabled'],
       )}
     >
       <input
-        className={clsx(className, styles["checkbox__input"])}
+        className={clsx(className, styles['checkbox__input'])}
         data-bootstrap-override="checkbox"
         disabled={disabled}
         ref={ref}
@@ -113,9 +113,9 @@ export const CheckboxInput = React.forwardRef<
         {...checkedProps}
         {...other}
       />
-      <CheckboxSvg indeterminate={checked === "indeterminate"} />
+      <CheckboxSvg indeterminate={checked === 'indeterminate'} />
     </span>
   );
 });
 
-CheckboxInput.displayName = "CheckboxInput";
+CheckboxInput.displayName = 'CheckboxInput';
