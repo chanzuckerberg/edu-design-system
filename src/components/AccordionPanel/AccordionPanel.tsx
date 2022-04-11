8a03238408f6699f2b6b89ca5f1bccfa29b3e968
ifrost1 <ifrost@contractor.chanzuckerberg.com>
2022-04-11 16:08:24 -0400

    fix: accordion panel import

--- src/components/AccordionPanel/AccordionPanel.tsx
+++ src/components/AccordionPanel/AccordionPanel.tsx
@@ -8,8 +8,8 @@ import React, {
   MutableRefObject,
   ReactNode,
 } from 'react';
-import styles from './AccordionPanel.module.css';
 import { EdsThemeColorIconNeutralStrong } from '../../tokens-dist/colors';
+import styles from '../Accordion/Accordion.module.css';
 import Icon from '../Icon';
 
 export interface Props {
