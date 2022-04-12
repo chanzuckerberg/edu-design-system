a8a50d63b2329d860e1114c3179f71f62a83aed4
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2022-04-12 12:06:58 -0700

    chore(buttongroup): move buttongroup to next (#970)
    * chore: move buttongroup to next
    
    * chore: wrap buttongroup
    
    * chore: remove wrap options, keep wrap

---
+++ src/components/ButtonGroup/ButtonGroup.test.ts
@@ -0,0 +1,6 @@
+import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import * as ButtonGroupStoryFile from './ButtonGroup.stories';
+
+describe('<ButtonGroup />', () => {
+  generateSnapshots(ButtonGroupStoryFile);
+});
