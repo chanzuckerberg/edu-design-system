a824d815e75a37319e2593df472dd3b5639cf029
Diedra Rater <diedra.rater@gmail.com>
2022-04-11 17:23:01 -0700

    feat(button): polish button styles (#957)
    * feat(button): use new button tokens, update storybook
    
    * test(button): add snapshot tests
    
    * feat(button): polish button styles
    
    * chore(mixins): remove unnecessary empty line
    
    * chore(button): reduce side padding on medium buttons by half
    
    * test(button): port over tests from main
    
    * test(button): enable snapshots
    
    * chore(button): make textLink mixin inherit text size

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,24 +1,55 @@
+import { generateSnapshots } from '@chanzuckerberg/story-utils';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Button } from './Button';
+import * as ButtonStoryFile from './Button.stories';
 
-test('renders the text in the button', () => {
-  render(<Button>Click</Button>);
+describe('<Button />', () => {
+  generateSnapshots(ButtonStoryFile);
 
-  expect(screen.getByRole('button')).toHaveTextContent('Click');
-});
+  it('renders the text in the button', () => {
+    render(<Button>Click</Button>);
 
-test('fires callback on click', () => {
-  const onClick = jest.fn();
-  render(<Button onClick={onClick}>Click</Button>);
+    expect(screen.getByRole('button')).toHaveTextContent('Click');
+  });
 
-  userEvent.click(screen.getByRole('button'));
-  expect(onClick).toHaveBeenCalled();
-});
+  it('fires callback on click', () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Click</Button>);
 
-test('renders as a link when provided href', () => {
-  render(<Button href="/test-url">test link</Button>);
+    userEvent.click(screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalled();
+  });
 
-  expect(screen.getByRole('link')).toBeDefined();
+  it('renders as a link when provided href', () => {
+    render(<Button href="/test-url">test link</Button>);
+
+    expect(screen.getByRole('link')).toBeDefined();
+  });
+
+  it('passes test ids down properly', () => {
+    render(<Button data-testid="example-test-id">Click</Button>);
+    expect(screen.getByTestId('example-test-id')).toMatchSnapshot();
+  });
+
+  it('passes class names down properly', () => {
+    render(
+      <Button className="exampleClassName" data-testid="example-class-name">
+        Click
+      </Button>,
+    );
+    expect(screen.getByTestId('example-class-name')).toMatchSnapshot();
+  });
+
+  it('forwards refs', () => {
+    const ref = React.createRef<HTMLButtonElement>();
+    render(<Button ref={ref}>Click</Button>);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    ref.current!.focus();
+
+    const button = screen.getByRole('button');
+    expect(button).toHaveFocus();
+  });
 });
