31744eabc1019e96a241aae7fe709ab4a4f3df38
Diedra Rater <diedra.rater@gmail.com>
2022-04-18 05:07:24 -0700

    chore(banner-description, banner-title): remove redundant tests from Banner subcomponents

--- src/components/BannerDescription/BannerDescription.test.tsx
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as stories from './BannerDescription.stories';
-
-describe('<BannerDescription />', () => {
-  generateSnapshots(stories);
-});
--- src/components/BannerTitle/BannerTitle.test.tsx
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as stories from './BannerTitle.stories';
-
-describe('<BannerTitle />', () => {
-  generateSnapshots(stories);
-});
