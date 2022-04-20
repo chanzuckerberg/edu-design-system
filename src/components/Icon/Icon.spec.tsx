f7bc93be7fb4a3b08e054871191c2e61bab3b54b
Annie Hu <annie.hu@chanzuckerberg.com>
2022-04-20 12:36:51 -0400

    chore: merge in origin main

--- src/components/Icon/Icon.spec.tsx
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from "@chanzuckerberg/story-utils";
-import * as IconStoryFile from "./Icon.stories";
-
-describe("<Icon />", () => {
-  generateSnapshots(IconStoryFile);
-});
