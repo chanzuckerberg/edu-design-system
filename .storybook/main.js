11630bd75904d11452d52ce936f28e35c0478669
Diedra Rater <diedra.rater@gmail.com>
2022-04-20 11:39:34 -0700

    chore: convert double quotes to single in .storybook/main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const SVGSpritemapPlugin = require("svg-spritemap-webpack-plugin");
+const path = require('path');
+const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 
 module.exports = {
   stories: [
@@ -35,7 +35,7 @@ module.exports = {
   ],
   webpackFinal: async (config) => {
     const SVGSpritesPlugin = new SVGSpritemapPlugin(
-      path.resolve(__dirname, "../src/icons/**/*.svg"),
+      path.resolve(__dirname, '../src/icons/**/*.svg'),
       {
         sprite: {
           prefix: false,
