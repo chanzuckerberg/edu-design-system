190c7ed0fe884fd49503458bb0e57be32894f97e
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2022-04-21 15:13:26 -0700

    test(banner, tooltip): test deprecation console warn (#999)
    * test(banner, tooltip): test deprecation console warn
    
    prevents deprecation console warn showing up during testing
    
    * test(tooltip, banner): abstract console warn helper
    
    * test(helper): adds comments about helper consoleWarnMock

--- src/components/Banner/Banner.test.tsx
+++ src/components/Banner/Banner.test.tsx
@@ -1,11 +1,15 @@
 import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import { composeStories } from '@storybook/testing-react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Banner } from './Banner';
 import * as BannerStoryFile from './Banner.stories';
+import consoleWarnMockHelper from '../../../jest/helpers/consoleWarnMock';
 
 describe('<Banner />', () => {
+  const consoleWarnMock = consoleWarnMockHelper();
+
   generateSnapshots(BannerStoryFile);
 
   it('dismisses when dismissed if dismissable', () => {
@@ -21,5 +25,15 @@ describe('<Banner />', () => {
     expect(screen.getByTestId('banner-test')).toBeInTheDocument();
     userEvent.click(screen.getByRole('button'));
     expect(screen.queryByTestId('banner-test')).not.toBeInTheDocument();
+  });
+  it('should display warning message when attempting to use isFlat', () => {
+    const { Flat } = composeStories(BannerStoryFile);
+    render(<Flat />);
+    expect(consoleWarnMock).toHaveBeenCalledTimes(1);
+    expect(consoleWarnMock).toBeCalledWith(
+      'The isFlat style is deprecated and will be removed in an upcoming release.\n',
+      'Please remove this prop to use the default elevated style (with a border and drop shadow) instead.',
+    );
+    consoleWarnMock.mockRestore();
   });
 });
--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -4,10 +4,13 @@ import { composeStories } from '@storybook/testing-react';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import * as TooltipStoryFile from './Tooltip.stories';
+import consoleWarnMockHelper from '../../../jest/helpers/consoleWarnMock';
 
 const { Interactive } = composeStories(TooltipStoryFile);
 
 describe('<Tooltip />', () => {
+  const warnMock = consoleWarnMockHelper();
+
   generateSnapshots(TooltipStoryFile, {
     // Tippy renders tooltip as a child of <body> and hence is why baseElement needs to be targetted
     getElement: (wrapper) => {
@@ -24,5 +27,13 @@ describe('<Tooltip />', () => {
     expect(screen.getByTestId('tooltip-content')).toBeInTheDocument();
     await userEvent.keyboard('{esc}');
     expect(screen.queryByTestId('tooltip-content')).not.toBeInTheDocument();
+  });
+
+  it('should display warning message when attempting to use dark variant', () => {
+    render(<Interactive variant="dark" />);
+    expect(warnMock).toHaveBeenCalledTimes(1);
+    expect(warnMock).toBeCalledWith(
+      'The dark variant is deprecated and will be removed in an upcoming release. Please use the default light variant instead.',
+    );
   });
 });
