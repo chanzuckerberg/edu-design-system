239f4e8b1730ec64ea32cfbb4bfc2cab40dde2f9
csnizik <csnizik@tulane.edu>
2022-04-22 11:46:51 -0500

    chore(list det): minor rm unused import

--- src/components/ListDetailPanel/ListDetailPanel.tsx
+++ src/components/ListDetailPanel/ListDetailPanel.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode } from 'react';
 import styles from '../ListDetail/ListDetail.module.css';
 
 export interface Props {
