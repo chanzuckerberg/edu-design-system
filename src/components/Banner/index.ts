452b0baa32e3d266d2268f8dd2d834111d23921c
Diedra Rater <diedra.rater@gmail.com>
2022-04-25 16:37:42 -0700

    feat(banner): update API on main to match next

--- src/components/Banner/index.ts
+++ src/components/Banner/index.ts
@@ -1,2 +1,6 @@
 export { Banner as default } from "./Banner";
+<<<<<<< HEAD
 export type { BannerProps } from "./Banner";
+=======
+export type { Variant } from "./Banner";
+>>>>>>> 44be5591 (feat(banner): update API on main to match next)
