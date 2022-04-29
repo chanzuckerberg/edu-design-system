72f6683735d1f7f7f786a691e33a28c175929a97
csnizik <csnizik@tulane.edu>
2022-04-29 07:35:54 -0500

    feat(dragdrop): temp rm test

--- src/components/DragDrop/DragDrop.test.tsx
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as stories from './DragDrop.stories';
-
-describe('<DragDrop />', () => {
-  generateSnapshots(stories);
-});
