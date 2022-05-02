fda96340fd69806b6e0300b4bbbe4782a68783f2
Annie Hu <annie.hu@chanzuckerberg.com>
2022-05-02 16:47:06 -0400

    docs: add token values to typography docs

--- .storybook/components/DesignTokens/Tier2/TypographyUsage.js
+++ .storybook/components/DesignTokens/Tier2/TypographyUsage.js
@@ -1,156 +1,83 @@
 import React, { Component } from 'react';
 import { Grid, Section } from '../../../../src';
-import styles from '../../../../src/components/Utilities/TypographyUsage.module.css';
+import '../../../../src/components/Utilities/TypographyUsage.css';
 import { TokenSpecimen } from '../../TokenSpecimen/TokenSpecimen';
+import { PRESET_SIZE_MAP } from '../Tier1/TypographyPresets';
 
 export class Tier2TypographyUsage extends Component {
   render() {
+    const renderTypeToken = (size, preset) => {
+      // remove variant suffix
+      const normalizedPreset = preset.split('-')[0];
+      const { fontSize, lineHeight } = PRESET_SIZE_MAP[normalizedPreset];
+
+      return (
+        <TokenSpecimen
+          comment={`Ref: preset-${preset}`}
+          name={`eds-theme-typography-${size}`}
+          specimenClassName={`u-theme-typography-${size}`}
+          value={`${fontSize} / ${lineHeight}`}
+          variant="typography-body"
+        />
+      );
+    };
+
     return (
       <div>
         <Section title="Headline">
           <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-headline-lg"
-              specimenClassName={styles['u-theme-typography-headline-lg']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-headline-md"
-              specimenClassName={styles['u-theme-typography-headline-md']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-headline-sm"
-              specimenClassName={styles['u-theme-typography-headline-sm']}
-              variant="typography-body"
-            />
+            {renderTypeToken('headline-lg', '001-bold')}
+            {renderTypeToken('headline-md', '002-bold')}
+            {renderTypeToken('headline-sm', '003-bold')}
           </Grid>
         </Section>
 
         <Section title="Title">
           <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-title-md"
-              specimenClassName={styles['u-theme-typography-title-md']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-title-sm"
-              specimenClassName={styles['u-theme-typography-title-sm']}
-              variant="typography-body"
-            />
+            {renderTypeToken('title-md', '004-bold')}
+            {renderTypeToken('title-sm', '005-bold')}
           </Grid>
         </Section>
 
         <Section title="Label">
           <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-label-lg-subtle"
-              specimenClassName={styles['u-theme-typography-label-lg-subtle']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-label-md-subtle"
-              specimenClassName={styles['u-theme-typography-label-md-subtle']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-label-md"
-              specimenClassName={styles['u-theme-typography-label-md']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-label-sm"
-              specimenClassName={styles['u-theme-typography-label-sm']}
-              variant="typography-body"
-            />
+            {renderTypeToken('label-lg-subtle', '005')}
+            {renderTypeToken('label-md-subtle', '007')}
+            {renderTypeToken('label-md', '007-bold')}
+            {renderTypeToken('label-sm', '009-bold')}
           </Grid>
         </Section>
 
         <Section title="Body Text">
           <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-body-text-md"
-              specimenClassName={styles['u-theme-typography-body-text-md']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-body-text-sm"
-              specimenClassName={styles['u-theme-typography-body-text-sm']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-body-text-xs"
-              specimenClassName={styles['u-theme-typography-body-text-xs']}
-              variant="typography-body"
-            />
+            {renderTypeToken('body-text-lg', '004')}
+            {renderTypeToken('body-text-md', '005')}
+            {renderTypeToken('body-text-sm', '006')}
+            {renderTypeToken('body-text-xs', '008')}
           </Grid>
         </Section>
 
         <Section title="Caption">
           <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-caption-text-lg"
-              specimenClassName={styles['u-theme-typography-caption-text-lg']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-caption-text-md"
-              specimenClassName={styles['u-theme-typography-caption-text-md']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-caption-text-sm"
-              specimenClassName={styles['u-theme-typography-caption-text-sm']}
-              variant="typography-body"
-            />
+            {renderTypeToken('caption-text-lg', '006')}
+            {renderTypeToken('caption-text-md', '008')}
+            {renderTypeToken('caption-text-sm', '010')}
           </Grid>
         </Section>
 
         <Section title="Button">
-          <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-button-label"
-              specimenClassName={styles['u-theme-typography-button-label']}
-              variant="typography-body"
-            />
-          </Grid>
+          <Grid>{renderTypeToken('button-label', '006-bold')}</Grid>
         </Section>
 
         <Section title="Kicker">
           <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-kicker"
-              specimenClassName={styles['u-theme-typography-kicker']}
-              variant="typography-body"
-            />
-
-            <TokenSpecimen
-              name="eds-theme-typography-kicker-sm"
-              specimenClassName={styles['u-theme-typography-kicker-sm']}
-              variant="typography-body"
-            />
+            {renderTypeToken('kicker', '011-bold')}
+            {renderTypeToken('kicker-sm', '012-bold')}
           </Grid>
         </Section>
 
         <Section title="Tag">
-          <Grid>
-            <TokenSpecimen
-              name="eds-theme-typography-tag"
-              specimenClassName={styles['u-theme-typography-tag']}
-              variant="typography-body"
-            />
-          </Grid>
+          <Grid>{renderTypeToken('tag', '009-bold')}</Grid>
         </Section>
       </div>
     );
