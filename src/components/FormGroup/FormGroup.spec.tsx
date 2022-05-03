75f3179ae1d6951147281b7cc659db01aeeb24f1
Diedra Rater <diedra.rater@gmail.com>
2022-05-03 16:08:00 -0700

    feat: remove FormGroup

--- src/components/FormGroup/FormGroup.spec.tsx
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from "@chanzuckerberg/story-utils";
-import * as FormGroupStoryFile from "./FormGroup.stories";
-
-describe("<FormGroup />", () => {
-  generateSnapshots(FormGroupStoryFile);
-});
