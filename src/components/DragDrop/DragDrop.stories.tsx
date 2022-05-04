b37544a5cc307bad1e78a03462b7856e392ce161
csnizik <csnizik@tulane.edu>
2022-05-04 12:49:54 -0500

    feat(dragdrop): content in story

--- src/components/DragDrop/DragDrop.stories.tsx
+++ src/components/DragDrop/DragDrop.stories.tsx
@@ -6,6 +6,29 @@ import { DragDrop, Props } from './DragDrop';
 export default {
   title: 'Organisms/Interactive/Drag and Drop',
   component: DragDrop,
+  args: {
+    cards: {
+      'task-1': { id: 'task-1', content: 'Project #1' },
+      'task-2': { id: 'task-2', content: 'Project #2' },
+      'task-3': { id: 'task-3', content: 'Project #3' },
+      'task-4': { id: 'task-4', content: 'Project #4' },
+      'task-5': { id: 'task-5', content: 'Project #5' },
+    },
+    columns: {
+      'column-1': {
+        id: 'column-1',
+        title: 'Available projects',
+        taskIds: ['task-1', 'task-2', 'task-3', 'task-4', 'task-5'],
+      },
+      'column-2': {
+        id: 'column-2',
+        title: 'Planned projects',
+        taskIds: [],
+      },
+    },
+    // Facilitate reordering of columns
+    columnOrder: ['column-1', 'column-2'],
+  },
   decorators: [
     (Story) => (
       <div
