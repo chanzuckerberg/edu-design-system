937840dc632c22d30337364d5b6d4b1049b44a0e
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2022-05-05 11:23:11 -0700

    feat(databar): add databar and databarsegment (#1031)

---
+++ src/components/DataBar/index.ts
@@ -0,0 +1,1 @@
+export { DataBar as default } from './DataBar';
---
+++ src/components/DataBarSegment/index.ts
@@ -0,0 +1,2 @@
+export { DataBarSegment as default } from './DataBarSegment';
+export type { Props, Variants } from './DataBarSegment';
