88eb04cba24c96aafc969119666f998ea1835786
Diedra Rater <diedra.rater@gmail.com>
2022-05-09 08:51:53 -0700

    chore: remove Label component

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,6 @@ export { default as HorizontalStep } from './components/HorizontalStep';
 export { default as HorizontalStepper } from './components/HorizontalStepper';
 export { default as Hr } from './components/Hr';
 export { default as Icon } from './components/Icon';
-export { default as Label } from './components/Label';
 export { default as Layout } from './components/Layout';
 export { default as LayoutContainer } from './components/LayoutContainer';
 export { default as LayoutLinelengthContainer } from './components/LayoutLinelengthContainer';
