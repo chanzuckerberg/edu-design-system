31093d0d6c4fc2da214266a0cb56f4eb7e85b7fc
Diedra Rater <diedra.rater@gmail.com>
2022-05-09 14:28:16 -0700

    chore: move upcoming components to new directory, remove package exports

---
+++ src/upcoming-components/Feature/Feature.stories.tsx
@@ -0,0 +1,31 @@
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+
+import { Feature, Props } from './Feature';
+import Heading from '../../components/Heading';
+import TextPassage from '../../components/TextPassage';
+
+export default {
+  title: 'Molecules/Blocks/Feature',
+  component: Feature,
+} as Meta;
+
+const Template: Story<Props> = (args) => (
+  <Feature {...args}>
+    <Heading size="h2">Feature title</Heading>
+    <TextPassage className="u-margin-top-md">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+    </TextPassage>
+  </Feature>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  // eslint-disable-next-line @chanzuckerberg/stories/no-ext-resources-in-stories
+  imgSrc: 'https://via.placeholder.com/450x500.png',
+  imgAlt: 'Placeholder Text',
+  title: 'Feature Title',
+  text: 'This is a button',
+};
---
+++ src/upcoming-components/FieldNote/FieldNote.stories.tsx
@@ -0,0 +1,33 @@
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+
+import { FieldNote, Props } from './FieldNote';
+import Link from '../../components/Link';
+import TextPassage from '../../components/TextPassage';
+
+export default {
+  title: 'Atoms/Forms/FieldNote',
+  component: FieldNote,
+} as Meta;
+
+const Template: Story<Props> = (args) => (
+  <FieldNote {...args}>This is a fieldnote.</FieldNote>
+);
+
+export const Default = Template.bind({});
+Default.args = { id: 'field-1' };
+
+export const WithTextPassage = () => (
+  <FieldNote id="field-1">
+    <TextPassage size="sm">
+      Here is a field note that involves:
+      <ul>
+        <li>Multiple lines</li>
+        <li>Arbitrary HTML text</li>
+        <li>
+          Even <Link href="#">text links</Link>
+        </li>
+      </ul>
+    </TextPassage>
+  </FieldNote>
+);
