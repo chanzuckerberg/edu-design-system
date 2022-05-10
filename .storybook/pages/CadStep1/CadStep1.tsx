64edb01c031f5b156d4191e66ad87bc706520699
Jin Lee <jin.lee@chanzuckerberg.com>
2022-05-09 19:06:20 -0700

    feat(cadstep1): populate header

--- .storybook/pages/CadStep1/CadStep1.tsx
+++ .storybook/pages/CadStep1/CadStep1.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
 
-import { Layout, LayoutContainer, LayoutSection, Panel } from '../../../src';
+import {
+  Button,
+  ButtonGroup,
+  HorizontalStepper,
+  Icon,
+  Layout,
+  LayoutContainer,
+  LayoutSection,
+  PageHeader,
+  Panel,
+} from '../../../src';
 
 import utilityStyles from '../../../src/components/Utilities/Spacing.module.css';
 
 export const CadStep1 = () => (
   <>
     <Panel className={utilityStyles['u-margin-bottom-lg']} variant="squared">
-      <div className="fpo">Page Header</div>
+      <LayoutContainer>
+        <PageHeader
+          className={utilityStyles['u-margin-bottom-none']}
+          description={
+            <HorizontalStepper
+              activeIndex={0}
+              steps={[
+                'Add classroom details',
+                'Add projects',
+                'Create course plan',
+              ]}
+            />
+          }
+          headingSize="h5"
+          right={
+            <ButtonGroup>
+              <Button>Return to Course</Button>
+              <Button variant="primary">
+                Next <Icon name="arrow-narrow-right" purpose="decorative" />
+              </Button>
+            </ButtonGroup>
+          }
+          title="Course Planner: History 6"
+        />
+      </LayoutContainer>
     </Panel>
     <LayoutContainer>
       <div className="fpo">Page</div>
