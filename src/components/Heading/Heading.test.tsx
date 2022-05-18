c776b3836f772b2286ea986e82110afbbcdfe291
Diedra Rater <diedra.rater@gmail.com>
2022-05-18 08:37:59 -0700

    feat(heading): use new heading tokens in old heading size props

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -1,12 +1,16 @@
 import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import { composeStories } from '@storybook/testing-react';
 import { render, screen } from '@testing-library/react';
 
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Heading } from './Heading';
 import * as HeadingStoryFile from './Heading.stories';
+import consoleWarnMockHelper from '../../../jest/helpers/consoleWarnMock';
 
 describe('<Heading />', () => {
+  const consoleWarnMock = consoleWarnMockHelper();
+
   generateSnapshots(HeadingStoryFile);
 
   it('should add the passthrough className', () => {
@@ -17,6 +21,7 @@ describe('<Heading />', () => {
     );
     expect(screen.getByText('Some Heading').classList).toContain('passthrough');
   });
+
   it('should handle refs', async () => {
     const HelperComponent = () => {
       const refContainer = React.useRef(null);
@@ -37,5 +42,25 @@ describe('<Heading />', () => {
     render(<HelperComponent />);
     expect(screen.getByText('Ref container parent test')).toBeTruthy();
     userEvent.click(screen.getByRole('button'));
+  });
+
+  it('should display warning message when attempting to use h6 size', () => {
+    const { Heading6 } = composeStories(HeadingStoryFile);
+    render(<Heading6 />);
+    expect(consoleWarnMock).toHaveBeenCalledTimes(1);
+    expect(consoleWarnMock).toBeCalledWith(
+      `The h6 size is deprecated and will be removed in an upcoming release.\n`,
+      'Please bump this heading up to a larger size if possible.',
+    );
+  });
+
+  it('should display warning message when attempting to use h7 size', () => {
+    const { Heading7 } = composeStories(HeadingStoryFile);
+    render(<Heading7 />);
+    expect(consoleWarnMock).toHaveBeenCalledTimes(1);
+    expect(consoleWarnMock).toBeCalledWith(
+      `The h7 size is deprecated and will be removed in an upcoming release.\n`,
+      'Please bump this heading up to a larger size if possible.',
+    );
   });
 });
