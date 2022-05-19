05447dc812d95a41ffb3e24226b00c00df6696c7
Jin Lee <jin.lee@chanzuckerberg.com>
2022-05-18 21:56:46 -0700

    feat(inlinenotification): add color variants and shape notification

--- src/components/InlineNotification/InlineNotification.stories.tsx
+++ src/components/InlineNotification/InlineNotification.stories.tsx
@@ -1,12 +1,23 @@
 import { BADGE } from '@geometricpanda/storybook-addon-badges';
-import { StoryObj } from '@storybook/react';
+import type { StoryObj } from '@storybook/react';
 import React from 'react';
-
-import { InlineNotification } from './InlineNotification';
+import { InlineNotification, VARIANTS } from './InlineNotification';
 
 export default {
   title: 'Example/InlineNotification',
   component: InlineNotification,
+  args: {
+    text: 'Inline notifications lorem ipsum text',
+    variant: 'success' as const,
+  },
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: VARIANTS,
+      },
+    },
+  },
   parameters: {
     badges: [BADGE.BETA],
   },
@@ -14,4 +25,62 @@ export default {
 
 type Args = React.ComponentProps<typeof InlineNotification>;
 
-export const Default: StoryObj<Args> = {};
+export const Success: StoryObj<Args> = {};
+export const Score: StoryObj<Args> = {
+  args: {
+    score: '9/10',
+  },
+};
+
+export const LongText: StoryObj<Args> = {
+  args: {
+    text: 'Long text inline notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+};
+
+export const SubtleVariants: StoryObj<Args> = {
+  render: (args) => (
+    <div
+      style={{
+        display: 'inline-flex',
+        flexDirection: 'column',
+        gap: '0.25rem',
+      }}
+    >
+      {VARIANTS.map((variant) => {
+        return (
+          <InlineNotification
+            key={variant}
+            {...args}
+            text={`${variant} inline notification lorem ipsum`}
+            variant={variant}
+          />
+        );
+      })}
+    </div>
+  ),
+};
+
+export const StrongVariants: StoryObj<Args> = {
+  render: (args) => (
+    <div
+      style={{
+        display: 'inline-flex',
+        flexDirection: 'column',
+        gap: '0.25rem',
+      }}
+    >
+      {VARIANTS.map((variant) => {
+        return (
+          <InlineNotification
+            {...args}
+            isStrong
+            key={variant}
+            text={`${variant} inline notification lorem ipsum`}
+            variant={variant}
+          />
+        );
+      })}
+    </div>
+  ),
+};
--- src/components/InlineNotification/InlineNotification.tsx
+++ src/components/InlineNotification/InlineNotification.tsx
@@ -1,13 +1,64 @@
 import clsx from 'clsx';
 import React from 'react';
 import styles from './InlineNotification.module.css';
+import { Icon, Text } from '../../';
 
-export interface Props {
+export const VARIANTS = ['brand', 'success', 'warning', 'yield'] as const;
+
+const variantToIconAssetsMap: {
+  [key in 'brand' | 'success' | 'warning' | 'yield']: {
+    name: 'info' | 'check-circle' | 'warning' | 'error';
+    title: 'info' | 'success' | 'alert' | 'review';
+  };
+} = {
+  brand: {
+    name: 'info',
+    title: 'info',
+  },
+  success: {
+    name: 'check-circle',
+    title: 'success',
+  },
+  warning: {
+    name: 'warning',
+    title: 'alert',
+  },
+  yield: {
+    name: 'error',
+    title: 'review',
+  },
+};
+
+type Variant = typeof VARIANTS[number];
+
+type Props = {
   /**
-   * CSS class names that can be appended to the component.
+   * CSS class names that can be appended to the component for styling.
    */
   className?: string;
-}
+  /**
+   * Toggles notification that fills the full width of its container.
+   * Used for CardWithNotification recipe.
+   */
+  isFullWidth?: boolean;
+  /**
+   * Toggles the stronger background variants.
+   */
+  isStrong?: boolean;
+  /**
+   * The text contents of the tag, nested inside the component, in addition to the icon.
+   */
+  text?: string;
+  /**
+   * The score as a string to be appended after the text.
+   */
+  score?: string;
+  /**
+   * The color variant of the tag. New variants should be supported
+   * in the VARIANTS array and by its separate style in CSS file.
+   */
+  variant: Variant;
+};
 
 /**
  * BETA: This component is still a work in progress and is subject to change.
@@ -16,14 +67,52 @@ export interface Props {
  * import {InlineNotification} from "@chanzuckerberg/eds";
  * ```
  *
- * TODO: update this comment with a description of the component.
+ * This component provides an inline banner accompanied with an icon for messaging users.
  */
-export const InlineNotification = ({ className, ...other }: Props) => {
-  const componentClassName = clsx(styles['inline-notification'], className);
-
+export const InlineNotification = ({
+  className,
+  isFullWidth,
+  isStrong,
+  score,
+  text,
+  variant,
+  ...other
+}: Props) => {
+  const componentClassName = clsx(
+    styles['inline-notification'],
+    styles[`inline-notification--${variant}`],
+    isFullWidth && styles[`inline-notification--full-width`],
+    isStrong && styles[`inline-notification--${variant}-strong`],
+    className,
+  );
+  const iconClassName = clsx(
+    styles['inline-notification__icon'],
+    styles[`inline-notification__icon-${variant}`],
+  );
   return (
     <div className={componentClassName} {...other}>
-      Hello!
+      <Icon
+        className={iconClassName}
+        name={variantToIconAssetsMap[variant].name}
+        purpose="informative"
+        size="1.5rem"
+        title={variantToIconAssetsMap[variant].title}
+      />
+      <Text as="span" variant={variant === 'yield' ? 'base' : variant}>
+        {text}
+        {score && (
+          <Text as="span" variant="base">
+            {' '}
+            Score:
+          </Text>
+        )}
+        {score && (
+          <Text as="span" variant="base" weight="bold">
+            {' '}
+            {score}
+          </Text>
+        )}
+      </Text>
     </div>
   );
 };
