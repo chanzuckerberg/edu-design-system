1455734828ad8933fa7f4a5997280ce23c72e6ca
ifrost1 <ifrost@contractor.chanzuckerberg.com>
2022-05-23 16:10:36 -0400

    feat: add shadows within drag and drop

--- src/components/DragDrop/DragDrop.tsx
+++ src/components/DragDrop/DragDrop.tsx
@@ -1,5 +1,12 @@
 import clsx from 'clsx';
-import React, { ReactNode, useState } from 'react';
+import React, {
+  ReactNode,
+  useState,
+  useEffect,
+  useRef,
+  MutableRefObject,
+  UIEvent,
+} from 'react';
 import {
   DragDropContext,
   DropResult,
@@ -48,16 +55,92 @@ export const DragDrop = ({
   unstyledItems = false,
 }: Props) => {
   /**
-   * If either multipleContainers or unstyledItems is set, apply the corresponding class
+   * Set states and refs
+   * 1) Set isEnd state: set to true, right shadow gradient activates. Removed when false
+   * 2) Set isState state: set to true, left shadow gradient activates. Removed when false
+   * 3) Target drag drop and drag drop inner DOM elements
    */
-  const componentClassName = clsx(
-    styles['drag-drop'],
-    className,
-    unstyledItems && styles['drag-drop--unstyled'],
-    multipleContainers && styles['drag-drop--multiple'],
-  );
+  const [isEnd, setIsEnd] = useState(true); /* 1 */
+  const [isStart, setIsStart] = useState(false); /* 2 */
+  const dragDropInnerRef = useRef() as MutableRefObject<HTMLDivElement>; /* 3 */
+  const dragDropRef = useRef() as MutableRefObject<HTMLElement>;
+  /**
+   * Set right and left gradients on tables
+   * 1) Target the actual drag drop inside drag drop
+   * 2) Get the width of the drag drop offscreen when drag drop overflows
+   * 3) If drag drop width is less than or equal to drag drop width, remove all shadows
+   * 4) If drag drop inner scroll isn't all the way to the left or to the right, turn all shadows on
+   * 5) If drag drop inner scroll is >= to width of drag drop offscreen, turn off right shadow and turn left shadow on
+   * 6) Else, set the right shadow to true and the left shadow to false
+   */
+  const setShadows = () => {
+    const overflowListItems = Array.from(
+      dragDropInnerRef.current.children,
+    ); /* 1 */
+    let childrenWidth = 0;
 
-  console.log(Droppable);
+    overflowListItems.forEach(function (item) {
+      const itemMarginLeft = parseInt(
+        getComputedStyle(item).marginLeft.slice(0, -2),
+      );
+      childrenWidth += item.clientWidth + itemMarginLeft;
+    });
+    if (overflowListItems) {
+      const dragDropWidth = dragDropRef.current.clientWidth; /* 2 */
+      const totalItemsWidth = childrenWidth; /* 2 */
+      const dragDropOffScreen =
+        totalItemsWidth - dragDropRef.current.clientWidth; /* 2 */
+
+      if (totalItemsWidth <= dragDropWidth) {
+        /* 3 */
+        setIsEnd(false);
+        setIsStart(false);
+      } else if (
+        dragDropInnerRef.current.scrollLeft > 0 &&
+        dragDropInnerRef.current.scrollLeft < dragDropOffScreen
+      ) {
+        /* 4 */
+        setIsEnd(true);
+        setIsStart(true);
+      } else if (dragDropInnerRef.current.scrollLeft >= dragDropOffScreen) {
+        /* 5 */
+        setIsEnd(false);
+        setIsStart(true);
+      } else {
+        /* 6 */
+        setIsEnd(true);
+        setIsStart(false);
+      }
+    }
+  };
+
+  /**
+   * drag drop inner onscroll
+   * 1) Set shadows when user scrolls the drag drop right and left
+   */
+  const handleOnScroll = (e: UIEvent<HTMLElement>): void => {
+    setShadows(); /* 1 */
+  };
+
+  /**
+   * Use effect lifecycle
+   * 1) Set shadows when component is updated/loaded
+   * 2) Set window resize listener
+   * 3) Remove window resize listener
+   */
+  useEffect(() => {
+    dragDropRef.current = dragDropInnerRef.current
+      .parentNode as HTMLElement; /* 3 */
+    setShadows(); /* 1 */
+    window.addEventListener('resize', setShadows); /* 2 */
+    return () => {
+      window.removeEventListener('resize', setShadows); /* 3 */
+    };
+  }, []);
+
+  /**
+   * Update sticky wrapper height on mount
+   */
 
   const containerOrder: string[] = [];
   Object.entries(items).forEach(([key, item]) => {
@@ -167,6 +250,18 @@ export const DragDrop = ({
     setState(newState);
   };
 
+  /**
+   * If either multipleContainers or unstyledItems is set, apply the corresponding class
+   */
+  const componentClassName = clsx(
+    styles['drag-drop'],
+    className,
+    unstyledItems && styles['drag-drop--unstyled'],
+    multipleContainers && styles['drag-drop--multiple'],
+    isStart && styles['eds-is-overflow-left'],
+    isEnd && styles['eds-is-overflow-right'],
+  );
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable
@@ -181,23 +276,29 @@ export const DragDrop = ({
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
-              {state?.containerOrder &&
-                state.containerOrder.map((containerId: string) => {
-                  const container = state.containers[containerId];
-                  const items = container.itemIds
-                    ? container.itemIds.map((itemId) => state.items[itemId])
-                    : [];
+              <div
+                className={styles['drag-drop__inner']}
+                onScroll={handleOnScroll}
+                ref={dragDropInnerRef}
+              >
+                {state?.containerOrder &&
+                  state.containerOrder.map((containerId: string) => {
+                    const container = state.containers[containerId];
+                    const items = container.itemIds
+                      ? container.itemIds.map((itemId) => state.items[itemId])
+                      : [];
 
-                  return (
-                    <DragDropContainer
-                      container={container}
-                      emptyContent={container.emptyContent}
-                      items={items}
-                      key={container.id}
-                    />
-                  );
-                })}
-              {provided.placeholder}
+                    return (
+                      <DragDropContainer
+                        container={container}
+                        emptyContent={container.emptyContent}
+                        items={items}
+                        key={container.id}
+                      />
+                    );
+                  })}
+                {provided.placeholder}
+              </div>
             </section>
           );
         }}
