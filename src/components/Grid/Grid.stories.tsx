27a9a694ebecbbd2f6b25f9bd04cfc7afe9ac833
Jin Lee <jin.lee@chanzuckerberg.com>
2022-05-25 22:02:37 -0700

    chore(grid): add 1-2-1up variant and change to csf

--- src/components/Grid/Grid.stories.tsx
+++ src/components/Grid/Grid.stories.tsx
@@ -1,75 +1,91 @@
 import { BADGE } from '@geometricpanda/storybook-addon-badges';
-import { Story, Meta } from '@storybook/react';
+import { StoryObj } from '@storybook/react';
 import React from 'react';
 
-import { Grid, Props } from './Grid';
+import { Grid } from './Grid';
 import GridItem from '../GridItem';
 
 export default {
   title: 'Molecules/Layout and Containers/Grid',
+  children: (
+    <>
+      <Grid.Item>
+        <div className="fpo u-margin-top-none u-margin-bottom-none">
+          Grid Item
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div className="fpo u-margin-top-none u-margin-bottom-none">
+          Grid Item
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div className="fpo u-margin-top-none u-margin-bottom-none">
+          Grid Item
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div className="fpo u-margin-top-none u-margin-bottom-none">
+          Grid Item
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div className="fpo u-margin-top-none u-margin-bottom-none">
+          Grid Item
+        </div>
+      </Grid.Item>
+      <Grid.Item>
+        <div className="fpo u-margin-top-none u-margin-bottom-none">
+          Grid Item
+        </div>
+      </Grid.Item>
+    </>
+  ),
   component: Grid,
   subcomponents: { GridItem },
   parameters: {
-    axe: {
-      // TODO: re-enable when component is worked on
-      skip: true,
-    },
     badges: [BADGE.BETA],
   },
-} as Meta;
+};
 
-const Template: Story<Props> = (args) => (
-  <Grid {...args}>
-    <Grid.Item>
-      <div className="fpo u-margin-top-none u-margin-bottom-none">
-        Grid Item
-      </div>
-    </Grid.Item>
-    <Grid.Item>
-      <div className="fpo u-margin-top-none u-margin-bottom-none">
-        Grid Item
-      </div>
-    </Grid.Item>
-    <Grid.Item>
-      <div className="fpo u-margin-top-none u-margin-bottom-none">
-        Grid Item
-      </div>
-    </Grid.Item>
-    <Grid.Item>
-      <div className="fpo u-margin-top-none u-margin-bottom-none">
-        Grid Item
-      </div>
-    </Grid.Item>
-    <Grid.Item>
-      <div className="fpo u-margin-top-none u-margin-bottom-none">
-        Grid Item
-      </div>
-    </Grid.Item>
-    <Grid.Item>
-      <div className="fpo u-margin-top-none u-margin-bottom-none">
-        Grid Item
-      </div>
-    </Grid.Item>
-  </Grid>
-);
+type Args = React.ComponentProps<typeof Grid>;
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Default: StoryObj<Args> = {};
 
-export const SideBySide = Template.bind({});
-SideBySide.args = { variant: 'side-by-side' };
+export const SideBySide: StoryObj<Args> = {
+  args: {
+    variant: 'side-by-side',
+  },
+};
 
-export const TwoUpGrid = Template.bind({});
-TwoUpGrid.args = { variant: '2up' };
+export const TwoUpGrid: StoryObj<Args> = {
+  args: {
+    variant: '2up',
+  },
+};
 
-export const OneToThreeUpGrid = Template.bind({});
-OneToThreeUpGrid.args = { variant: '1-3up' };
+export const OneToThreeUpGrid: StoryObj<Args> = {
+  args: {
+    variant: '1-3up',
+  },
+};
 
-export const ThreeUpGrid = Template.bind({});
-ThreeUpGrid.args = { variant: '3up' };
+export const ThreeUpGrid: StoryObj<Args> = {
+  args: {
+    variant: '3up',
+  },
+};
 
-export const OneToTwoToFourUpGrid = Template.bind({});
-OneToTwoToFourUpGrid.args = { variant: '1-2-4up' };
+export const OneToTwoToFourUpGrid: StoryObj<Args> = {
+  args: { variant: '1-2-4up' },
+};
 
-export const FourUpGrid = Template.bind({});
-FourUpGrid.args = { variant: '4up' };
+export const FourUpGrid: StoryObj<Args> = {
+  args: {
+    variant: '4up',
+  },
+};
+
+export const OneToTwoToOneUpGrid: StoryObj<Args> = {
+  args: { variant: '1-2-1up' },
+};
