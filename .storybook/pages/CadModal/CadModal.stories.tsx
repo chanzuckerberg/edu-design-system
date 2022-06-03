e4737c71586d08dacb4ce5a74137f59eb281d2d8
Jin Lee <jin.lee@chanzuckerberg.com>
2022-06-03 00:44:03 -0700

    chore(cadmodal): add brand modal

--- .storybook/pages/CadModal/CadModal.stories.tsx
+++ .storybook/pages/CadModal/CadModal.stories.tsx
@@ -1,12 +1,17 @@
 import { StoryObj } from '@storybook/react';
 import React from 'react';
 
+import { ModalBrand } from './ModalBrand';
 import { ModalFirstStep } from './ModalFirstStep';
 
 export default {
-  title: 'Pages/CadModal',
+  title: 'Pages/CAD/CadModal',
 };
 
-export const Default: StoryObj = {
+export const FirstStep: StoryObj = {
   render: () => <ModalFirstStep />,
+};
+
+export const Brand: StoryObj = {
+  render: () => <ModalBrand />,
 };
--- .storybook/pages/CadModal/ModalBrand.tsx
+++ .storybook/pages/CadModal/ModalBrand.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-import { Modal } from '../../../src';
+import { Button, Modal, Heading, Text } from '../../../src';
 
-import { PageShell } from '../../recipes/PageShell/PageShell';
+import '../../../src/components/Utilities/Alignment.css';
+import '../../../src/components/Utilities/Display.css';
+import '../../../src/components/Utilities/Spacing.css';
+
+import CompassSolo from '../../static/compass-solo.svg';
 
 export const ModalBrand = () => (
-  // <Modal>
-  //   <PageHeader title="Page title" />
-  // </Modal>
+  <Modal
+    onClose={
+      () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
+    }
+    open
+    variant="brand"
+  >
+    <Modal.Header brandAsset={<img alt="compass solo" src={CompassSolo} />}>
+      <Modal.Title>Check out the new course planner</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Heading className="u-margin-bottom-md" size="title-sm">
+        Intro header copy
+      </Heading>
+      <Text>Content pieces go here</Text>
+    </Modal.Body>
+    <Modal.Footer className="u-display-flex u-justify-content-flex-end">
+      <Button
+        onClick={
+          () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
+        }
+        variant="primary"
+      >
+        Button
+      </Button>
+    </Modal.Footer>
+  </Modal>
 );
