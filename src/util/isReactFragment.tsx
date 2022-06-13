18140e90853e5157412d71da9d134e3b25fdb0d2
ifrost1 <ifrost@contractor.chanzuckerberg.com>
2022-06-13 14:48:56 -0400

    feat: util for if children is fragment logic

---
+++ src/util/isReactFragment.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const isReactFragment = (node: React.ReactNode) => {
+  if (!node) return false;
+
+  if ((node as React.ReactElement)?.type) {
+    return (node as React.ReactElement)?.type === React.Fragment;
+  }
+
+  return node === React.Fragment;
+};
