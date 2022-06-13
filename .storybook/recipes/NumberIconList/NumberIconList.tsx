8d6dc7ae70071eabd16bffb37748e09627488470
ifrost1 <ifrost@contractor.chanzuckerberg.com>
2022-06-13 19:26:36 -0400

    fix: numbericonlist classname cleanup

--- .storybook/recipes/NumberIconList/NumberIconList.tsx
+++ .storybook/recipes/NumberIconList/NumberIconList.tsx
@@ -17,7 +17,7 @@ export interface Props {
  * Primary UI component for user interaction
  */
 export const NumberIconList = ({ className, children, ...other }: Props) => {
-  const componentClassName = clsx(styles['number-icon-list'], className, {});
+  const componentClassName = clsx(styles['number-icon-list'], className);
   return (
     <div className={componentClassName} {...other}>
       {children}
