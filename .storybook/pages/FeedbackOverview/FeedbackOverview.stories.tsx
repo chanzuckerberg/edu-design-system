ac72a79553e30c9f6888c7e15a75cd6285607f3f
ifrost1 <ifrost@contractor.chanzuckerberg.com>
2022-06-16 11:38:07 -0400

    fix: remove axe. proj and feed overview CSF

--- .storybook/pages/FeedbackOverview/FeedbackOverview.stories.tsx
+++ .storybook/pages/FeedbackOverview/FeedbackOverview.stories.tsx
@@ -1,4 +1,4 @@
-import { Story, Meta } from '@storybook/react';
+import { StoryObj } from '@storybook/react';
 import React from 'react';
 
 import { FeedbackOverview } from './FeedbackOverview';
@@ -6,15 +6,7 @@ import { FeedbackOverview } from './FeedbackOverview';
 export default {
   title: 'Pages/Feedback/Overview',
   component: FeedbackOverview,
-  parameters: {
-    axe: {
-      // TODO: remove when "fpo" content is fully stubbed out
-      disabledRules: ['color-contrast'],
-    },
-  },
-} as Meta;
+};
 
-const Template: Story = (args) => <FeedbackOverview {...args} />;
-
-export const TeacherView = Template.bind({});
-TeacherView.args = {};
+type Args = React.ComponentProps<typeof FeedbackOverview>;
+export const TeacherView: StoryObj<Args> = {};
