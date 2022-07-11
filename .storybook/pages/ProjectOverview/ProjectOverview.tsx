cf63c430a8a04d907680e5a1c85be6dd7633fbdc
csnizik <csnizik@tulane.edu>
2022-07-11 15:07:54 -0500

    chore(section): update project overview

--- .storybook/pages/ProjectOverview/ProjectOverview.tsx
+++ .storybook/pages/ProjectOverview/ProjectOverview.tsx
@@ -38,130 +38,142 @@ export const ProjectOverview = () => {
       />
       <TimelineNav>
         <TimelineNavPanel title="Overview" variant="success">
-          <PageHeader
-            as="h2"
-            className="u-margin-bottom-xl"
+          <Section
             description="Estimated schedule: Days 4,5,6"
-            headingSize="headline-md"
             kicker="CHECKPOINT 1"
             title="What is this Project About?"
-          />
-          <Text size="lg">
-            Consectetur adipiscing elit. Ac id velit ut egestas arcu. At
-            maecenas urna, risus donec praesent eu consectetur. Nunc non eu
-            mattis sem turpis id dictum. Volutpat ornare turpis ultrices augue
-            bibendum pellentesque. Habitasse.
-          </Text>
-          <Button className="u-margin-bottom-xl" variant="link">
-            See More
-          </Button>
-          <Heading as="h3" className="u-margin-bottom-sm" size="title-md">
-            Essential Questions
-          </Heading>
-          <Text as="div" className="u-margin-bottom-xl">
-            <ul>
-              <li>How are structures in the body organized?</li>
-              <li>
-                How do these structures work together to support life? But
-                really need to see what it looks like if we have an extra long
-                question.
-              </li>
-              <li>How are structures in the body organized?</li>
-            </ul>
-          </Text>
-          <Heading as="h3" className="u-margin-bottom-sm" size="title-md">
-            Key Take Aways
-          </Heading>
-          <Text as="div" className="u-margin-bottom-xl">
-            <ol>
-              <li>
-                Groups of specialized cells make up organs and body systems,
-                which work together to support an organism’s survival.
-              </li>
-              <li>
-                How are structures in the body organized? More text blah blah
-                blah.
-              </li>
-              <li>How are structures in the body organized?</li>
-            </ol>
-          </Text>
-          <Hr className="u-margin-bottom-none" />
-          <Section
-            title="Power Focus Areas"
-            titleBefore={
-              <img
-                alt="placeholder"
-                src="https://via.placeholder.com/32x32"
-                style={{ display: 'block', borderRadius: '50%' }}
-              />
-            }
           >
-            <Card>
-              <CardBody>
-                <Heading as="h3" className="u-margin-bottom-sm" size="title-md">
-                  What Was Medieval Japan Like?
-                </Heading>
-                <Text>
-                  In this Focus Area, you explore the main question: How does
-                  the structures of organisms enable lifes functions?
-                </Text>
-              </CardBody>
-            </Card>
-          </Section>
-          <Section
-            title="Additional Focus Areas"
-            titleBefore={
-              <img
-                alt="placeholder"
-                src="https://via.placeholder.com/32x32"
-                style={{ display: 'block', borderRadius: '50%' }}
-              />
-            }
-          >
-            <Card className="u-margin-bottom-md">
-              <CardBody>
-                <Heading as="h3" className="u-margin-bottom-sm" size="title-md">
-                  What Was Medieval Japan Like?
-                </Heading>
-                <Text>
-                  In this Focus Area, you explore the main question: How does
-                  the structures of organisms enable life’s functions?
-                </Text>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardBody>
-                <Heading as="h3" className="u-margin-bottom-sm" size="title-md">
-                  What Was Medieval Japan Like?
-                </Heading>
-                <Text>
-                  In this Focus Area, you explore the main question: How does
-                  the structures of organisms enable life’s functions?
-                </Text>
-              </CardBody>
-            </Card>
-          </Section>
-          <Section
-            title="Cognitive Skills"
-            titleBefore={
-              <img
-                alt="placeholder"
-                src="https://via.placeholder.com/32x32"
-                style={{ display: 'block', borderRadius: '50%' }}
-              />
-            }
-          >
-            <Text as="div">
-              <a href="https://go.czi.team/eds">Selecting Relevant Sources</a>
-              <a href="https://go.czi.team/eds">
-                Identifying Patterns and Relationships
-              </a>
-              <a href="https://go.czi.team/eds">Argumentative Claim</a>
-              <a href="https://go.czi.team/eds">Selection of Evidence</a>
-              <a href="https://go.czi.team/eds">Explanation of Evidence</a>
-              <a href="https://go.czi.team/eds">Multimedia in Communication</a>
-              <a href="https://go.czi.team/eds">Integration of Evidence</a>
+            <Text size="lg">
+              Consectetur adipiscing elit. Ac id velit ut egestas arcu. At
+              maecenas urna, risus donec praesent eu consectetur. Nunc non eu
+              mattis sem turpis id dictum. Volutpat ornare turpis ultrices augue
+              bibendum pellentesque. Habitasse.
             </Text>
+            <Button className="u-margin-bottom-xl" variant="link">
+              See More
+            </Button>
+            <Heading as="h3" className="u-margin-bottom-sm" size="title-md">
+              Essential Questions
+            </Heading>
+            <Text as="div" className="u-margin-bottom-xl">
+              <ul>
+                <li>How are structures in the body organized?</li>
+                <li>
+                  How do these structures work together to support life? But
+                  really need to see what it looks like if we have an extra long
+                  question.
+                </li>
+                <li>How are structures in the body organized?</li>
+              </ul>
+            </Text>
+            <Heading as="h3" className="u-margin-bottom-sm" size="title-md">
+              Key Take Aways
+            </Heading>
+            <Text as="div" className="u-margin-bottom-xl">
+              <ol>
+                <li>
+                  Groups of specialized cells make up organs and body systems,
+                  which work together to support an organism’s survival.
+                </li>
+                <li>
+                  How are structures in the body organized? More text blah blah
+                  blah.
+                </li>
+                <li>How are structures in the body organized?</li>
+              </ol>
+            </Text>
+            <Hr className="u-margin-bottom-none" />
+            <Section
+              title="Power Focus Areas"
+              titleBefore={
+                <img
+                  alt="placeholder"
+                  src="https://via.placeholder.com/32x32"
+                  style={{ display: 'block', borderRadius: '50%' }}
+                />
+              }
+            >
+              <Card>
+                <CardBody>
+                  <Heading
+                    as="h3"
+                    className="u-margin-bottom-sm"
+                    size="title-md"
+                  >
+                    What Was Medieval Japan Like?
+                  </Heading>
+                  <Text>
+                    In this Focus Area, you explore the main question: How does
+                    the structures of organisms enable lifes functions?
+                  </Text>
+                </CardBody>
+              </Card>
+            </Section>
+            <Section
+              title="Additional Focus Areas"
+              titleBefore={
+                <img
+                  alt="placeholder"
+                  src="https://via.placeholder.com/32x32"
+                  style={{ display: 'block', borderRadius: '50%' }}
+                />
+              }
+            >
+              <Card className="u-margin-bottom-md">
+                <CardBody>
+                  <Heading
+                    as="h3"
+                    className="u-margin-bottom-sm"
+                    size="title-md"
+                  >
+                    What Was Medieval Japan Like?
+                  </Heading>
+                  <Text>
+                    In this Focus Area, you explore the main question: How does
+                    the structures of organisms enable life’s functions?
+                  </Text>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody>
+                  <Heading
+                    as="h3"
+                    className="u-margin-bottom-sm"
+                    size="title-md"
+                  >
+                    What Was Medieval Japan Like?
+                  </Heading>
+                  <Text>
+                    In this Focus Area, you explore the main question: How does
+                    the structures of organisms enable life’s functions?
+                  </Text>
+                </CardBody>
+              </Card>
+            </Section>
+            <Section
+              title="Cognitive Skills"
+              titleBefore={
+                <img
+                  alt="placeholder"
+                  src="https://via.placeholder.com/32x32"
+                  style={{ display: 'block', borderRadius: '50%' }}
+                />
+              }
+            >
+              <Text as="div">
+                <a href="https://go.czi.team/eds">Selecting Relevant Sources</a>
+                <a href="https://go.czi.team/eds">
+                  Identifying Patterns and Relationships
+                </a>
+                <a href="https://go.czi.team/eds">Argumentative Claim</a>
+                <a href="https://go.czi.team/eds">Selection of Evidence</a>
+                <a href="https://go.czi.team/eds">Explanation of Evidence</a>
+                <a href="https://go.czi.team/eds">
+                  Multimedia in Communication
+                </a>
+                <a href="https://go.czi.team/eds">Integration of Evidence</a>
+              </Text>
+            </Section>
           </Section>
         </TimelineNavPanel>
 
