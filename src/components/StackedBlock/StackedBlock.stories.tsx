2871f1d6ba5b6e8fd2d866f220ae7185b7935806
ifrost1 <ifrost@contractor.chanzuckerberg.com>
2022-07-21 18:08:02 -0400

    fix: stacked block location. plopfile change

--- src/components/StackedBlock/StackedBlock.stories.tsx
+++ src/components/StackedBlock/StackedBlock.stories.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { StackedBlock } from './StackedBlock';
 
 export default {
-  title: 'Example/StackedBlock',
+  title: 'Molecules/Blocks/StackedBlock',
   component: StackedBlock,
   parameters: {
     badges: [BADGE.BETA],
