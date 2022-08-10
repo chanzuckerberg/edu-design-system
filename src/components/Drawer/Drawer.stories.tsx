ec905c0aa0cc884dbcf51a574072b3fd6064d653
Jin Lee <jin.lee@chanzuckerberg.com>
2022-08-10 09:24:39 -0700

    test(drawer): add an open drawer story

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -1,18 +1,54 @@
 import { BADGE } from '@geometricpanda/storybook-addon-badges';
-import { Story, Meta } from '@storybook/react';
+import { StoryObj, Meta } from '@storybook/react';
 import React from 'react';
 
-import { DrawerExample, Props } from './DrawerExample';
+import { Drawer } from './Drawer';
+import { DrawerExample } from './DrawerExample';
+import { Button } from '../Button/Button';
+import { ButtonGroup } from '../ButtonGroup/ButtonGroup';
+import { Heading } from '../Heading/Heading';
 
 export default {
   title: 'Organisms/Interactive/Drawer',
-  component: DrawerExample,
+  component: Drawer,
   parameters: {
     badges: [BADGE.BETA],
   },
-} as Meta;
+  args: {
+    'aria-describedby': 'drawer-description-1',
+    'aria-labelledby': 'drawer-heading-1',
+    dismissible: true,
+    isActive: true,
+    children: (
+      <>
+        <Drawer.Header>
+          <Heading id="drawer-heading-1" size="h2">
+            Drawer title
+          </Heading>
+        </Drawer.Header>
+        <Drawer.Body>
+          <div className="fpo">Drawer body</div>
+        </Drawer.Body>
+        <Drawer.Footer>
+          <ButtonGroup>
+            <Button variant="primary">Button 1</Button>
+            <Button>Button 2</Button>
+          </ButtonGroup>
+        </Drawer.Footer>
+      </>
+    ),
+  },
+  argTypes: {
+    onClose: {
+      action: 'on close',
+    },
+  },
+} as Meta<Args>;
 
-const Template: Story<Props> = (args) => <DrawerExample {...args} />;
+type Args = React.ComponentProps<typeof Drawer>;
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Default: StoryObj<Args> = {};
+
+export const DefaultInteractive: StoryObj = {
+  render: () => <DrawerExample />,
+};
