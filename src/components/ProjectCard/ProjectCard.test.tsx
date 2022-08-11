7fb66944744b7001869fa784555d4f13269587fc
Jin Lee <jin.lee@chanzuckerberg.com>
2022-08-11 16:11:22 -0700

    test(projectcard): test for numberarialabel warning

--- src/components/ProjectCard/ProjectCard.test.tsx
+++ src/components/ProjectCard/ProjectCard.test.tsx
@@ -1,6 +1,20 @@
 import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import { render } from '@testing-library/react';
+import React from 'react';
+import { ProjectCard } from './ProjectCard';
 import * as stories from './ProjectCard.stories';
+import consoleWarnMockHelper from '../../../jest/helpers/consoleWarnMock';
 
 describe('<ProjectCard />', () => {
+  const consoleWarnMock = consoleWarnMockHelper();
+
   generateSnapshots(stories);
+
+  it('should warn if number is passed without numberAriaLabel', () => {
+    render(<ProjectCard number={1} />);
+    expect(consoleWarnMock).toHaveBeenCalledTimes(1);
+    expect(consoleWarnMock).toHaveBeenCalledWith(
+      'You must provide a "numberAriaLabel" for the number icon if a "number" has been passed',
+    );
+  });
 });
