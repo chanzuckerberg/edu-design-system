283a79a5165dc1a9d59d49bd43a6abe97f89453c
Jin Lee <jin.lee@chanzuckerberg.com>
2022-08-11 18:05:13 -0700

    test(checkbox): remove unused import and add tests

--- src/components/Checkbox/Checkbox.test.tsx
+++ src/components/Checkbox/Checkbox.test.tsx
@@ -1,11 +1,15 @@
 import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import { render } from '@testing-library/react';
+import { composeStories } from '@storybook/testing-react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Checkbox } from './Checkbox';
-import * as CheckboxStoryFile from './Checkbox.stories';
+import * as stories from './Checkbox.stories';
+
+const { Default } = composeStories(stories);
 
 describe('<Checkbox />', () => {
-  generateSnapshots(CheckboxStoryFile);
+  generateSnapshots(stories);
 
   test('throws an error if no label or aria-label', () => {
     expect(() => {
@@ -17,5 +21,13 @@ describe('<Checkbox />', () => {
     const { container } = render(<Checkbox disabled label="Disabled" />);
     // eslint-disable-next-line testing-library/no-node-access
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('should toggle the checkbox with space', () => {
+    render(<Default />);
+    const checkbox = screen.getByRole('checkbox');
+    checkbox.focus();
+    userEvent.keyboard('{space}');
+    expect(checkbox).toBeChecked();
   });
 });
