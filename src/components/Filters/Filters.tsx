799e904eba5444cf464b45bbc7d0426a93d4fd37
Jin Lee <jin.lee@chanzuckerberg.com>
2022-08-18 17:28:57 -0700

    feat(filters): make filters components controlled

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,22 +1,33 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import styles from './Filters.module.css';
 import Button from '../Button';
-import { CheckboxField, FiltersDrawer } from '../FiltersDrawer/FiltersDrawer';
+import { FiltersCheckboxField } from '../FiltersCheckboxField/FiltersCheckboxField';
+import { FiltersDrawer } from '../FiltersDrawer/FiltersDrawer';
 import Icon from '../Icon';
 
 export type Props = {
+  buttonText?: string;
+  buttonStatusVariant: 'neutral' | 'brand';
   /**
-   * Checkbox fields that will be displayed in the filters drawer.
+   * Checkboxes or Checkbox fields that will be displayed in the filters drawer.
    */
-  checkboxFields: CheckboxField[];
+  children: React.ReactNode;
   /**
    * CSS class names that can be appended to the component.
    */
   className?: string;
   /**
+   * Callback called when Clear All button is called.
+   */
+  onClear?: () => void;
+  /**
    * Callback called when filters drawer is closed.
    */
-  onClose: (checkedValues: { [key: string]: boolean }) => void;
+  onClose?: () => void;
+  /**
+   * Callback called when Apply button is called.
+   */
+  onApply?: () => void;
 };
 
 /**
@@ -28,71 +39,71 @@ export type Props = {
  *
  * A filter component with a button that triggers a drawer of checkbox filters to be selected.
  */
-export const Filters = ({ checkboxFields, className, onClose }: Props) => {
-  /**
-   * Maps the checkbox values to false initially to indicate that there are no filters selected.
-   * TODO: customize to allow initial filter values.
-   */
-  const checkedMap = {};
-  checkboxFields.forEach(({ checkboxes }) => {
-    checkboxes.forEach(({ value }) => {
-      checkedMap[value] = false;
-    });
-  });
-
-  /**
-   * Manages state of the applied checkboxes in the context of the filters component.
-   * It is only updated when the filters drawer is closed.
-   */
-  const [appliedCheckedBoxes, setAppliedCheckedBoxes] = useState({
-    ...checkedMap,
-  });
-
+export const Filters = ({
+  buttonText,
+  buttonStatusVariant,
+  children,
+  className,
+  onClear,
+  onClose,
+  onApply,
+}: Props) => {
   /**
    * Manages the active state of the filters drawer.
    */
   const [isActive, setIsActive] = useState(false);
 
-  function closeFiltersDrawer(checkedValues: { [key: string]: boolean }) {
-    onClose(checkedValues);
-    setAppliedCheckedBoxes({ ...checkedValues });
+  function closeFiltersDrawer() {
     setIsActive(false);
   }
 
-  const filtersButton = useRef<HTMLButtonElement>(null);
-  /**
-   * Counts how many filters have been applied.
-   */
-  const filterCount = Object.values(appliedCheckedBoxes).reduce(
-    (count: number, status) => {
-      if (status) return count + 1;
-      else return count;
-    },
-    0,
-  );
+  function closeFilters() {
+    if (onClose) {
+      onClose();
+    }
+    closeFiltersDrawer();
+  }
 
-  const hasFilters = filterCount > 0;
-  const statusVariant = hasFilters ? 'brand' : 'neutral';
+  function clearFilters() {
+    if (onClear) {
+      onClear();
+    }
+    closeFiltersDrawer();
+  }
+
+  function applyFilters() {
+    if (onApply) {
+      onApply();
+    }
+    closeFiltersDrawer();
+  }
+
+  const buttonVariant =
+    buttonStatusVariant === 'brand' ? 'primary' : 'secondary';
 
   return (
     <div>
       <Button
         className={styles['filters__button']}
         onClick={() => setIsActive(true)}
-        ref={filtersButton}
-        status={statusVariant}
-        variant={hasFilters ? 'primary' : 'secondary'}
+        status={buttonStatusVariant}
+        variant={buttonVariant}
       >
         <Icon name="filter-list" purpose="decorative" size="1.5rem" />
-        Filters {filterCount > 0 && `(${filterCount})`}
+        {buttonText}
       </Button>
       <FiltersDrawer
-        checkboxFields={checkboxFields}
-        checkedMap={appliedCheckedBoxes}
         className={className}
         isActive={isActive}
-        onClose={closeFiltersDrawer}
-      />
+        onApply={applyFilters}
+        onClear={clearFilters}
+        onClose={closeFilters}
+      >
+        {children}
+      </FiltersDrawer>
     </div>
   );
 };
+
+Filters.FiltersDrawer = FiltersDrawer;
+Filters.FiltersCheckboxField = FiltersCheckboxField;
