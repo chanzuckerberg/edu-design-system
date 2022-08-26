0efa6084cead95a8d333dc3b5162d5f7e0ad5c48
Jin Lee <jin.lee@chanzuckerberg.com>
2022-08-26 09:59:39 -0700

    chore(checkboxfield): add todo for disabled styling

--- src/upcoming-components/CheckboxField/CheckboxField.tsx
+++ src/upcoming-components/CheckboxField/CheckboxField.tsx
@@ -23,6 +23,7 @@ export interface Props {
   className?: string;
   /**
    * Disables the field and prevents editing the contents
+   * TODO: disabled variant needs to be styled. Checkbox components themselves already support disabled styling.
    */
   disabled?: boolean;
   /**
@@ -79,7 +80,6 @@ export const CheckboxField = ({
   label,
   isError,
   fieldNote,
-  disabled,
   'aria-describedby': ariaDescribedBy,
   children,
   optionalLabel,
@@ -91,6 +91,7 @@ export const CheckboxField = ({
     ? ariaDescribedBy || generatedId
     : undefined;
 
+  // TODO: disabled variant styling
   const componentClassName = clsx(
     styles['checkbox-field'],
     className,
