604ab589c0057df4e5f4e0ffcdd68c26e4850098
Jin Lee <jin.lee@chanzuckerberg.com>
2022-08-26 13:15:56 -0700

    chore(radio): remove radiofield, update comments

--- .storybook/pages/CoursePlannerModal/ModalFirstStep.tsx
+++ .storybook/pages/CoursePlannerModal/ModalFirstStep.tsx
@@ -1,71 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Button, ButtonGroup, Icon, Modal, Heading } from '../../../src';
-
-import { RadioField } from '../../../src/upcoming-components/RadioField/RadioField';
+import {
+  Button,
+  ButtonGroup,
+  Heading,
+  Icon,
+  Modal,
+  Radio,
+  Fieldset,
+} from '../../../src';
 
 import '../../../src/components/Utilities/Alignment.css';
 import '../../../src/components/Utilities/Display.css';
 import '../../../src/components/Utilities/Spacing.css';
 
-export const ModalFirstStep = () => (
-  <Modal
-    isScrollable
-    onClose={
-      () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
-    }
-    open
-  >
-    <Modal.Header>
-      <Modal.Title className="u-margin-bottom-md">
-        Before we start, tell us your planning needs
-      </Modal.Title>
-      <Heading as="h3" size="title-md">
-        We’ll need to know how to set up your plan. Can you tell us how you’ll
-        use the planner?
-      </Heading>
-    </Modal.Header>
-    <Modal.Body>
-      <RadioField label="Content pieces go here">
-        <RadioField.Item
-          name="course-planner"
-          text="I need my own planner. No additional teachers will need to create a plan for this course."
-          value="radio1"
-        />
-        <RadioField.Item
-          name="course-planner"
-          text="I need my own plans, but other teachers may need their own plans for this course too."
-          value="radio2"
-        />
-        <RadioField.Item
-          name="course-planner"
-          text="I’ll review and use a plan set by another teacher."
-          value="radio3"
-        />
-      </RadioField>
-    </Modal.Body>
-    <Modal.Footer className="u-display-flex u-justify-content-space-between u-align-items-center">
-      <Modal.Stepper activeStep={1} totalSteps={3} />
-      <ButtonGroup className="u-justify-content-flex-end">
-        <Button
-          onClick={
-            () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
-          }
-          status="neutral"
-        >
-          Not right now
-        </Button>
-        <Button
-          disabled
-          onClick={
-            () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
-          }
-          variant="primary"
-        >
-          Next
-          <Icon name="arrow-forward" purpose="decorative" />
-        </Button>
-      </ButtonGroup>
-    </Modal.Footer>
-  </Modal>
-);
+export const ModalFirstStep = () => {
+  const [checked, setChecked] = useState([false, false, false]);
+  const handleChange = (index: number) => {
+    const newChecked = [false, false, false];
+    newChecked[index] = true;
+    setChecked(newChecked);
+  };
+  return (
+    <Modal
+      isScrollable
+      onClose={
+        () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
+      }
+      open
+    >
+      <Modal.Header>
+        <Modal.Title className="u-margin-bottom-md">
+          Before we start, tell us your planning needs
+        </Modal.Title>
+        <Heading as="h3" size="title-md">
+          We’ll need to know how to set up your plan. Can you tell us how you’ll
+          use the planner?
+        </Heading>
+      </Modal.Header>
+      <Modal.Body>
+        <Fieldset className="u-margin-none">
+          <Fieldset.Legend text="Content pieces go here" />
+          <Fieldset.Items>
+            <Radio
+              checked={checked[0]}
+              label="I need my own planner. No additional teachers will need to create a plan for this course."
+              name="course-planner"
+              onChange={() => handleChange(0)}
+              value="radio1"
+            />
+            <Radio
+              checked={checked[1]}
+              label="I need my own plans, but other teachers may need their own plans for this course too."
+              name="course-planner"
+              onChange={() => handleChange(1)}
+              value="radio2"
+            />
+            <Radio
+              checked={checked[2]}
+              label="I’ll review and use a plan set by another teacher."
+              name="course-planner"
+              onChange={() => handleChange(2)}
+              value="radio3"
+            />
+          </Fieldset.Items>
+        </Fieldset>
+      </Modal.Body>
+      <Modal.Footer className="u-display-flex u-justify-content-space-between u-align-items-center">
+        <Modal.Stepper activeStep={1} totalSteps={3} />
+        <ButtonGroup className="u-justify-content-flex-end">
+          <Button
+            onClick={
+              () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
+            }
+            status="neutral"
+          >
+            Not right now
+          </Button>
+          <Button
+            disabled
+            onClick={
+              () => {} /* eslint-disable-line @typescript-eslint/no-empty-function */
+            }
+            variant="primary"
+          >
+            Next
+            <Icon name="arrow-forward" purpose="decorative" />
+          </Button>
+        </ButtonGroup>
+      </Modal.Footer>
+    </Modal>
+  );
+};
--- src/components/CheckboxLabel/CheckboxLabel.tsx
+++ src/components/CheckboxLabel/CheckboxLabel.tsx
@@ -8,9 +8,14 @@ export type CheckboxLabelProps = InputLabelProps;
 /**
  * Checkbox label element, styles and relies on the InputLabel component.
  */
-export const CheckboxLabel = ({ size, ...other }: CheckboxLabelProps) => {
+export const CheckboxLabel = ({
+  className,
+  size,
+  ...other
+}: CheckboxLabelProps) => {
   const componentClassName = clsx(
     size === 'md' && styles['checkbox__label--md'],
+    className,
   );
 
   return <InputLabel className={componentClassName} size={size} {...other} />;
