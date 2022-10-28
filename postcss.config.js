72b9f0ba9b7cceb2c24517f9d9684e19dabcac16
Soma Chakraborty <schakraborty@CZIMACOS4229.fios-router.home>
2022-10-28 09:41:26 -0400

    chore: removed comment line to replace tailwindcss nesting

--- postcss.config.js
+++ postcss.config.js
@@ -8,8 +8,6 @@ module.exports = {
     'tailwindcss/nesting': {},
     'postcss-mixins': {},
     'postcss-simple-vars': { variables: breakpoints },
-    // TODO: replace with tailwindcss/nesting (https://tailwindcss.com/docs/using-with-preprocessors#nesting)
-    // to silence warnings; for some reason this is not working currently.
     'postcss-nested': {},
     tailwindcss: {},
   },
