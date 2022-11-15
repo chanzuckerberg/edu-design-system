3745bb1654544efcc0c07a717c75a36e902e2a60
Jin Lee <jin.lee@chanzuckerberg.com>
2022-11-14 18:24:17 -0800

    chore(filtersrecipe): move props and give checkboxes state

--- .storybook/recipes/Filters/Filters.tsx
+++ .storybook/recipes/Filters/Filters.tsx
@@ -1,21 +1,16 @@
 import debounce from 'lodash.debounce';
 import React, { useEffect, useState } from 'react';
+import Checkbox from '../../../src/components/Checkbox';
+import FiltersCheckboxField from '../../../src/components/FiltersCheckboxField';
 import FiltersDrawer from '../../../src/components/FiltersDrawer';
-import type { FiltersPopoverProps } from '../../../src/components/FiltersPopover/FiltersPopover';
 import { FiltersPopover } from '../../../src/components/FiltersPopover/FiltersPopover';
 
 import breakpoint from '../../../src/design-tokens/tier-1-definitions/breakpoints';
-type Props = {
-  /**
-   * Popover placement options relative to the filters trigger button.
-   */
-  placement?: FiltersPopoverProps['placement'];
-} & FiltersPopoverProps;
 
 /**
  * Demonstrates usage of both FiltersPopover and FiltersDrawer depending on screensize.
  */
-export const Filters = ({ placement, ...other }: Props) => {
+export const Filters = ({ ...other }) => {
   const [isLarge, setIsLarge] = useState(false);
   const popoverBreakpoint = parseInt(breakpoint['eds-bp-md'], 10) * 16;
 
@@ -37,10 +32,61 @@ export const Filters = ({ placement, ...other }: Props) => {
       setIsLarge(false);
     }
   }, 200);
+
+  const initialCheckedState = [false, false, false];
+  const [checked, setChecked] = useState(initialCheckedState);
+
+  const onCheckboxChange = (index: number) => {
+    const newChecked = [...checked];
+    newChecked[index] = !checked[index];
+    setChecked(newChecked);
+  };
+
+  /**
+   * Counts how many filters have been applied.
+   */
+  const filterCount = Object.values(checked).reduce((count: number, status) => {
+    if (status) return count + 1;
+    else return count;
+  }, 0);
+
+  const hasSelectedFilters = filterCount > 0;
+  const triggerText = hasSelectedFilters
+    ? `Filters (${filterCount})`
+    : 'Filters';
+
+  const checkboxes = (
+    <FiltersCheckboxField legend="Filters Segment 1">
+      <Checkbox
+        checked={checked[0]}
+        label="Filters label 1"
+        onChange={() => onCheckboxChange(0)}
+      />
+      <Checkbox
+        checked={checked[1]}
+        label="Filters label 2"
+        onChange={() => onCheckboxChange(1)}
+      />
+      <Checkbox
+        checked={checked[2]}
+        label="Filters label 3"
+        onChange={() => onCheckboxChange(2)}
+      />
+    </FiltersCheckboxField>
+  );
+
+  const sharedProps = {
+    hasSelectedFilters,
+    triggerText,
+    children: checkboxes,
+  };
+
   return (
     <div>
-      {!isLarge && <FiltersDrawer {...other} />}
-      {isLarge && <FiltersPopover placement={placement} {...other} />}
+      {!isLarge && <FiltersDrawer {...sharedProps} {...other} />}
+      {isLarge && (
+        <FiltersPopover placement="bottom-start" {...sharedProps} {...other} />
+      )}
     </div>
   );
 };
