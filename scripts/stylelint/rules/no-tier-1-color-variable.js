c88d2647f746a4b9cf30997a5a3d5fd1b05ab7e9
Diedra Rater <diedra.rater@gmail.com>
2022-12-07 18:54:28 -0800

    chore: remove dedent

--- scripts/stylelint/rules/no-tier-1-color-variable.js
+++ scripts/stylelint/rules/no-tier-1-color-variable.js
@@ -1,14 +1,12 @@
 /* eslint-env node */
 
-const dedent = require('dedent');
 const stylelint = require('stylelint');
 
 const ruleName = 'eds/no-tier-1-color-variable';
 
 const messages = stylelint.utils.ruleMessages(ruleName, {
-  rejected: dedent`
-    Don't use tier 1 color CSS variables directly. Choose a tier 2 or tier 3 variable that has the same color value with a name relevant to the style you're adding. (Example: a neutral border might use --eds-theme-color-border-neutral-default.) If such a variable does not exist, talk to design about adding a token for this use case.
-  `,
+  rejected:
+    "Don't use tier 1 color CSS variables directly. Choose a tier 2 or tier 3 variable that has the same color value with a name relevant to the style you're adding. (Example: a neutral border might use --eds-theme-color-border-neutral-default.) If such a variable does not exist, talk to design about adding a token for this use case.",
 });
 
 /**
