6540c84123663f3d8804d77009aff012a83ab0c0
Jin Lee <jin.lee@chanzuckerberg.com>
2022-12-12 21:14:53 -0800

    refactor(dropdown): remove accidental addition of null

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -73,7 +73,7 @@ function InteractiveExampleUsingSeparateProps(props: Props) {
         options={exampleOptions}
         optionsAlign={optionsAlign}
         optionsClassName={optionsClassName}
-        value={selectedOption?.label || null}
+        value={selectedOption}
         variant={variant}
         {...props}
       />
