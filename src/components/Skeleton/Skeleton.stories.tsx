4516e7e12173601fdbf440c326d862fb9b8a416e
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2022-12-16 18:07:44 -0600

    feat(Skeleton): add skeleton components
    - add skeleton component with sub-components supporting different sizes
    - use `ease-out` for consistency
    - add in a "white" background color, and set to default on these stories
    - use width and height consistently
    - update colors to neutral-medium and neutral-subtle
    - add fallback for users with reduced motion set in OS
    - show how to use aria-label with skeleton states on the wrapper

---
+++ src/components/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,61 @@
+import { BADGE } from '@geometricpanda/storybook-addon-badges';
+import type { StoryObj, Meta } from '@storybook/react';
+import React from 'react';
+
+import { Skeleton } from './Skeleton';
+import PopoverContainer from '../PopoverContainer';
+
+export default {
+  title: 'Atoms/Skeleton',
+  component: Skeleton,
+  subcomponents: {
+    'Skeleton.Circle': Skeleton.Circle,
+    'Skeleton.Rect': Skeleton.Rect,
+    'Skeleton.Text': Skeleton.Text,
+  },
+  parameters: {
+    badges: [BADGE.BETA],
+    layout: 'centered',
+    backgrounds: {
+      default: 'white',
+    },
+  },
+} as Meta<Args>;
+
+type Args = React.ComponentProps<typeof Skeleton>;
+
+export const Default: StoryObj<Args> = {
+  args: {
+    width: 100,
+    height: 100,
+  },
+};
+
+export const Circle: StoryObj<Args> = {
+  render: (args) => {
+    return <Skeleton.Circle width={100} />;
+  },
+};
+
+export const Text: StoryObj<Args> = {
+  render: (args) => {
+    return <Skeleton.Text height="1.5rem" width="30ch" />;
+  },
+};
+
+export const LayoutExample: StoryObj<Args> = {
+  render: (args) => {
+    return (
+      <div aria-label="Loading example profile card" role="status">
+        <PopoverContainer className="p-3 m-2">
+          <Skeleton.Circle className="mb-3" width="50px" />
+          <Skeleton.Text className="mt-2 mb-2" height="4rem" width="15ch" />
+          <Skeleton.Text className="mt-2 mb-2" height="1.5rem" width="30ch" />
+          <Skeleton.Text className="mt-2 mb-2" height="1.5rem" width="25ch" />
+          <Skeleton.Text className="mt-2 mb-8" height="1.5rem" width="10ch" />
+          <Skeleton height={50} width="100%" />
+        </PopoverContainer>
+      </div>
+    );
+  },
+};
