edb2a02def2e6434a28137adea34759f9f0986c7
Jin Lee <jin.lee@chanzuckerberg.com>
2023-02-03 13:15:26 -0800

    test(input): remove low value tests

--- src/components/Input/Input.test.ts
+++
@@ -1,6 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as InputStoryFile from './Input.stories';
-
-describe('<Input />', () => {
-  generateSnapshots(InputStoryFile);
-});
