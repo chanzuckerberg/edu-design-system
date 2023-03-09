758d34a656cc6c361b37d4ee3b10e93389a550be
Jin Lee <jin.lee@chanzuckerberg.com>
2023-03-08 23:31:08 -0800

    refactor(tailwind): refer to css-variables-nested file and dry

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-const variableTokens = require('./src/tokens-dist/json/css-variables-nested.json');
-const staticTokens = require('./src/tokens-dist/json/variables-nested.json');
+const edsTokens = require('./lib/tokens/json/css-variables-nested.json').eds;
 
 const {
   background: backgroundColorTokens,
   border: borderColorTokens,
   text: textColorTokens,
   ...colorTokens
-} = variableTokens.eds.theme.color;
+} = edsTokens.theme.color;
 
 module.exports = {
   /**
@@ -35,22 +34,22 @@ module.exports = {
     fontSize: {
       // provide values for both font-size and line-height
       // sync with src/design-tokens/tier-1-definitions/typography-presets.css
-      body: [staticTokens.eds['font-size']['16'], '1.5'],
-      h1: [staticTokens.eds['font-size']['40'], '1.2'],
-      h2: [staticTokens.eds['font-size']['32'], '1.25'],
-      h3: [staticTokens.eds['font-size']['24'], '1.333'],
-      h4: [staticTokens.eds['font-size']['18'], '1.333'],
-      h5: [staticTokens.eds['font-size']['16'], '1.5'],
-      h6: [staticTokens.eds['font-size']['14'], '1.57'],
-      h7: [staticTokens.eds['font-size']['12'], '1.666'],
-      sm: [staticTokens.eds['font-size']['14'], '1.57'],
-      xs: [staticTokens.eds['font-size']['12'], '1.666'],
-      caption: [staticTokens.eds['font-size']['12'], '1.666'],
+      body: [edsTokens['font-size']['16'], '1.5'],
+      h1: [edsTokens['font-size']['40'], '1.2'],
+      h2: [edsTokens['font-size']['32'], '1.25'],
+      h3: [edsTokens['font-size']['24'], '1.333'],
+      h4: [edsTokens['font-size']['18'], '1.333'],
+      h5: [edsTokens['font-size']['16'], '1.5'],
+      h6: [edsTokens['font-size']['14'], '1.57'],
+      h7: [edsTokens['font-size']['12'], '1.666'],
+      sm: [edsTokens['font-size']['14'], '1.57'],
+      xs: [edsTokens['font-size']['12'], '1.666'],
+      caption: [edsTokens['font-size']['12'], '1.666'],
     },
     fontWeight: {
-      normal: staticTokens.eds['font-weight'].light,
-      medium: staticTokens.eds['font-weight'].medium,
-      bold: staticTokens.eds['font-weight'].bold,
+      normal: edsTokens['font-weight'].light,
+      medium: edsTokens['font-weight'].medium,
+      bold: edsTokens['font-weight'].bold,
     },
     // sync with src/design-tokens/tier-1-definitions/breakpoints.js
     screens: {
