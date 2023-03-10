6e315ea1402938682169d4b060f2e0039e3e8e63
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2023-03-10 16:30:33 -0600

    feat(LoadingIndicator): extract and use SVG animation directly (#1540)
    - we only have one particular loading indicator we want to use at this time
    - apply styles and parameters to match previous implementation's output

--- src/components/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 import React from 'react';
 
-import { Oval } from 'react-loader-spinner';
 import styles from './LoadingIndicator.module.css';
 
 export type Props = {
@@ -18,17 +17,11 @@ export type Props = {
 
   /**
    * Aria label of the oval. Default is "loading". Will be overridden if ariaLabel is passed in props
-   * (Shadowed from OvalProps)
-   *
-   * See: https://mhnpd.github.io/react-loader-spinner/docs/components/oval
    */
   ariaLabel?: string;
 
   /**
    * Whether the oval is visible. Default is true.
-   * (Shadowed from OvalProps)
-   *
-   * See: https://mhnpd.github.io/react-loader-spinner/docs/components/oval
    */
   visible?: boolean;
 };
@@ -47,6 +40,13 @@ const loaderStrokeSize = {
   lg: 4,
 };
 
+// The viewport changes based on adjustments to handle the stroke width
+const loaderViewportSize = {
+  sm: '-20 -20 42 42',
+  md: '-20.5 -20.5 43 43',
+  lg: '-21 -21 44 44',
+};
+
 /**
  * BETA: This component is still a work in progress and is subject to change.
  *
@@ -54,7 +54,7 @@ const loaderStrokeSize = {
  *
  * Loading indicators inform users about the wait time, reason, and status of ongoing processes when the layout is unknown
  *
- * For screen readers, add an `aria-label` to describe what is loading.
+ * For screen readers, add a custom `aria-label` to describe what is loading.
  *
  */
 export const LoadingIndicator = ({
@@ -64,22 +64,58 @@ export const LoadingIndicator = ({
   visible = true,
   ...other
 }: Props) => {
-  const componentClassName = clsx(styles['loading-indicator'], className);
+  const componentClassName = clsx(
+    styles['loading-indicator'],
+    !visible && styles['loading-indicator--invisible'],
+    className,
+  );
 
   // setting the colors to be transparent since we override in CSS
   // (and have a lint rule prevent token variable use in components)
   return (
-    <Oval
-      ariaLabel={ariaLabel}
-      color="transparent"
-      height={loaderSize[size]}
-      secondaryColor="transparent"
-      strokeWidth={loaderStrokeSize[size]}
-      strokeWidthSecondary={loaderStrokeSize[size]}
-      visible={visible}
-      width={loaderSize[size]}
-      wrapperClass={componentClassName}
+    <div
+      aria-busy="true"
+      aria-label={ariaLabel}
+      className={componentClassName}
+      data-testid="oval-loading"
+      role="status"
       {...other}
-    />
+    >
+      <svg
+        data-testid="oval-svg"
+        height={loaderSize[size]}
+        stroke="transparent"
+        viewBox={loaderViewportSize[size]}
+        width={loaderSize[size]}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g fill="none" fillRule="evenodd">
+          <g
+            data-testid="oval-secondary-group"
+            strokeWidth={loaderStrokeSize[size]}
+            transform="translate(1 1)"
+          >
+            <circle
+              cx="0"
+              cy="0"
+              r="20"
+              stroke="transparent"
+              strokeOpacity=".5"
+              strokeWidth={loaderStrokeSize[size]}
+            ></circle>
+            <path d="M20 0c0-9.94-8.06-20-20-20">
+              <animateTransform
+                attributeName="transform"
+                dur="1s"
+                from="0 0 0"
+                repeatCount="indefinite"
+                to="360 0 0"
+                type="rotate"
+              ></animateTransform>
+            </path>
+          </g>
+        </g>
+      </svg>
+    </div>
   );
 };
