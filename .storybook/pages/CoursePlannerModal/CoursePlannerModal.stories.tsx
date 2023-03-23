48b8d0383ffd693207359b918c01a5a56d46d99d
Jin Lee <jin.lee@chanzuckerberg.com>
2023-03-23 15:47:37 -0700

    test(courseplannermodal): disable flaky rule for all steps

--- .storybook/pages/CoursePlannerModal/CoursePlannerModal.stories.tsx
+++ .storybook/pages/CoursePlannerModal/CoursePlannerModal.stories.tsx
@@ -7,6 +7,14 @@ import { ModalFirstStep } from './ModalFirstStep';
 
 export default {
   title: 'Pages/Course Planner/Modal',
+  parameters: {
+    axe: {
+      /**
+       * Axe testing is flaky here, but is tested across the component modal stories and would be testing Headless Dialog functionality anyways.
+       */
+      disabledRules: ['aria-dialog-name'],
+    },
+  },
 };
 
 export const FirstStep: StoryObj = {
@@ -19,12 +27,4 @@ export const Brand: StoryObj = {
 
 export const FinalStep: StoryObj = {
   render: () => <ModalFinalStep />,
-  parameters: {
-    axe: {
-      /**
-       * Axe testing is flaky here, but is tested across the other modal stories and would be testing Headless Dialog functionality anyways.
-       */
-      disabledRules: ['aria-dialog-name'],
-    },
-  },
 };
