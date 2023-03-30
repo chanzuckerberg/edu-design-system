97145e5c7b5727ada362835981d0fa5f455a0db3
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2023-03-30 14:16:33 -0700

    build(rollup): mark dependencies as external (#1558)
    * build(rollup): mark dependencies as external
    
    * build(rollup): use node resolve plugin
    
    * docs(build): explain rollup external option

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,4 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 
@@ -9,8 +10,14 @@ export default {
     preserveModules: true,
     preserveModulesRoot: 'src',
   },
-  external: ['react-children-by-type'],
+  /**
+   * With the nodeResolve plugin, this marks all EDS node_modules as external, aka provided by the consumer.
+   * Since EDS is not imported directly into a web <script>, package managers (such as npm or yarn)
+   * will install the deps listed in EDS package.json and hence won't need to be included in the bundle.
+   */
+  external: [/node_modules/],
   plugins: [
+    nodeResolve(),
     postcss({
       extract: true,
       modules: true,
