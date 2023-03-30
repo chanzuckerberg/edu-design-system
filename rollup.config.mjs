58e63ba1f9459e5257c75f0eeec9740ae4eb7386
Jin Lee <jin.lee@chanzuckerberg.com>
2023-03-30 14:09:21 -0700

    docs(build): explain rollup external option

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,6 +10,11 @@ export default {
     preserveModules: true,
     preserveModulesRoot: 'src',
   },
+  /**
+   * With the nodeResolve plugin, this marks all EDS node_modules as external, aka provided by the consumer.
+   * Since EDS is not imported directly into a web <script>, package managers (such as npm or yarn)
+   * will install the deps listed in EDS package.json and hence won't need to be included in the bundle.
+   */
   external: [/node_modules/],
   plugins: [
     nodeResolve(),
