e4c15c4a849904f69fb7a8c79580755f52d37f66
Jin Lee <jin.lee@chanzuckerberg.com>
2023-03-31 00:12:19 -0700

    build: generate cjs and esm, separate types build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,14 +2,26 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 
+// rollup.config.js
+/**
+ * @type {import('rollup').RollupOptions}
+ */
 export default {
   input: 'src/index.ts',
-  output: {
-    dir: 'lib',
-    format: 'es',
-    preserveModules: true,
-    preserveModulesRoot: 'src',
-  },
+  output: [
+    {
+      dir: 'lib/es',
+      format: 'es',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+    {
+      dir: 'lib/cjs',
+      format: 'cjs',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+  ],
   /**
    * With the nodeResolve plugin, this marks all EDS node_modules as external, aka provided by the consumer.
    * Since EDS is not imported directly into a web <script>, package managers (such as npm or yarn)
