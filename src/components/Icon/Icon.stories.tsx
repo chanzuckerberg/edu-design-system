c6162efe4e73c0348eaf417b8f86a0f8772d633c
Jin Lee <jin.lee@chanzuckerberg.com>
2023-04-13 13:26:16 -0700

    fix: no need for ts expect error

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -34,10 +34,8 @@ type Args = React.ComponentProps<typeof Icon>;
 export const Default: StoryObj<Args> = {
   render: ({ name, color, ...rest }) => {
     // ESlint can't tell if ColorTokens[color] is valid or not, since it's computed at runtime.
-    /* eslint-disable import/namespace */
-    // @ts-expect-error cannot check type compliance on runtime imports
+    /* eslint-disable-next-line import/namespace */
     const computedColor = color && ColorTokens[color];
-    /* eslint-enable import/namespace */
     return <Icon {...rest} color={computedColor} name={name} />;
   },
   args: {
