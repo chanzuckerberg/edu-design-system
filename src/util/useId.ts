6c3f2d06116ece48393ce833318b20fc3712e95b
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2023-04-28 12:00:11 -0500

    fix: include node env. check for warning

--- src/util/useId.ts
+++ src/util/useId.ts
@@ -5,9 +5,12 @@ function genId() {
   return ++id;
 }
 
-console.warn(
-  'EDS useId is not ssr friendly if using React <18 as it uses the native React.useId hook if available https://react.dev/reference/react/useId.',
-);
+if (process.env.NODE_ENV !== 'production') {
+  console.warn(
+    'EDS useId is not ssr friendly if using React <18 as it uses the native React.useId hook if available https://react.dev/reference/react/useId.',
+  );
+}
+
 export const useId =
   React.useId ??
   function useId() {
