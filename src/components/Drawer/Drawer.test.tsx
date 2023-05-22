c61508b8675016c33551e8ccd868dddd803fb49a
Jin Lee <86632227+jinlee93@users.noreply.github.com>
2023-05-22 09:15:40 -0700

    fix(drawer): handle clickoutside on slower active boolean (#1621)
    * fix(drawer): handle clickoutside on slower active boolean
    
    * test: drawer opens on click

---
+++ src/components/Drawer/Drawer.test.tsx
@@ -0,0 +1,27 @@
+import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import { composeStories } from '@storybook/testing-react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import * as stories from './Drawer.stories';
+const { DefaultInteractive } = composeStories(stories);
+
+describe('<Drawer />', () => {
+  generateSnapshots(stories, {
+    getElement: async () => {
+      const filters = await screen.findByRole('dialog');
+      return filters.parentElement; // eslint-disable-line testing-library/no-node-access
+    },
+  });
+
+  it('shows the dialog when the open dialog button is clicked', async () => {
+    const user = userEvent.setup();
+    render(<DefaultInteractive />);
+    const openDrawerButton = await screen.findByRole('button', {
+      name: 'Open Drawer',
+    });
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+    await user.click(openDrawerButton);
+    expect(screen.getByRole('dialog')).toBeTruthy();
+  });
+});
