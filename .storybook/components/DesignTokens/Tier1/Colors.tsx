c41254217535ba4a7a03030db700a82483ff3f7e
Jin Lee <jin.lee@chanzuckerberg.com>
2023-07-25 17:42:19 -0700

    docs(colors): convert to input colors and show figma token

--- .storybook/components/DesignTokens/Tier1/Colors.tsx
+++ .storybook/components/DesignTokens/Tier1/Colors.tsx
@@ -4,26 +4,35 @@ import { Section } from '../../../../src';
 import filterTokens from '../../../util/filterTokens';
 import { ColorList } from '../../ColorList/ColorList';
 
-export const Tier1Colors = () => (
-  <div>
-    <Section title="Brand Colors">
-      <ColorList listItems={filterTokens(`eds-color-brand`)} />
-    </Section>
-
-    <Section title="Neutral Colors">
-      <ColorList listItems={filterTokens(`eds-color-neutral`)} />
-    </Section>
-
-    <Section title="Other Colors">
-      <ColorList listItems={filterTokens(`eds-color-other-mint`)} />
-      <div className="p-3"></div>
-      <ColorList listItems={filterTokens(`eds-color-other-yellow`)} />
-      <div className="p-3"></div>
-      <ColorList listItems={filterTokens(`eds-color-other-lemon`)} />
-      <div className="p-3"></div>
-      <ColorList listItems={filterTokens(`eds-color-other-orange`)} />
-      <div className="p-3"></div>
-      <ColorList listItems={filterTokens(`eds-color-other-ruby`)} />
-    </Section>
-  </div>
-);
+export const Tier1Colors = () => {
+  const getListItems = (filterTerm: string, figmaTokenHeader: string) =>
+    filterTokens(filterTerm).map(({ name, value }) => ({
+      name,
+      value,
+      figmaToken:
+        figmaTokenHeader + '/' + name.slice(name.lastIndexOf('-') + 1),
+    }));
+  return (
+    <div>
+      <Section title="Neutral Colors">
+        <ColorList listItems={getListItems('eds-color-neutral', 'neutral')} />
+      </Section>
+      <Section title="Brand Colors">
+        <ColorList listItems={getListItems('eds-color-brand', 'brand-grape')} />
+      </Section>
+      <Section className="flex flex-col gap-3" title="Other Colors">
+        <ColorList
+          listItems={getListItems('eds-color-other-orange', 'orange')}
+        />
+        <ColorList listItems={getListItems('eds-color-other-mint', 'mint')} />
+        <ColorList
+          listItems={getListItems('eds-color-other-yellow', 'yellow')}
+        />
+        <ColorList listItems={getListItems('eds-color-other-ruby', 'ruby')} />
+        <ColorList
+          listItems={getListItems('eds-color-other-lemon', 'supporting')}
+        />
+      </Section>
+    </div>
+  );
+};
--- .storybook/components/TokenSpecimen/TokenSpecimen.tsx
+++ .storybook/components/TokenSpecimen/TokenSpecimen.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import React, { Component } from 'react';
+import React from 'react';
 import './TokenSpecimen.css';
 
 type Props = {
@@ -14,86 +14,84 @@ type Props = {
   behavior?: 'stacked';
 };
 
-export class TokenSpecimen extends Component<Props> {
-  render() {
-    const componentClassName = clsx('token-specimen', this.props.className, {
-      'token-specimen--stacked': this.props.behavior === 'stacked',
-      'token-specimen--inverted': this.props.name.includes('inverse'),
-    });
+export const TokenSpecimen = (props: Props) => {
+  const componentClassName = clsx('token-specimen', props.className, {
+    'token-specimen--stacked': props.behavior === 'stacked',
+    'token-specimen--inverted': props.name.includes('inverse'),
+  });
 
-    let sample;
+  let sample;
 
-    if (this.props.variant === 'typography-title') {
-      sample = (
-        <div
-          className={
-            'token-specimen__sample token-specimen__sample--typography-title ' +
-            this.props.specimenClassName
-          }
-          contentEditable
-          style={this.props.inlineStyles}
-          suppressContentEditableWarning
-        >
-          AaBbCcDdEeFfGg
-        </div>
-      );
-    } else if (this.props.variant === 'typography-body') {
-      sample = (
-        <div
-          className={
-            'token-specimen__sample token-specimen__sample--typography-body ' +
-            this.props.specimenClassName
-          }
-          contentEditable
-          style={this.props.inlineStyles}
-          suppressContentEditableWarning
-        >
-          Almost before we knew it, we had left the ground.
-        </div>
-      );
-    } else if (this.props.variant === 'animation-fade') {
-      sample = (
-        <div
-          className={
-            'token-specimen__sample token-specimen__sample--box token-specimen__sample--animation-fade ' +
-            this.props.specimenClassName
-          }
-          style={this.props.inlineStyles}
-        ></div>
-      );
-    } else if (this.props.variant === 'animation-move') {
-      sample = (
-        <div
-          className={
-            'token-specimen__sample token-specimen__sample--box token-specimen__sample--animation-move ' +
-            this.props.specimenClassName
-          }
-          style={this.props.inlineStyles}
-        ></div>
-      );
-    } else {
-      sample = (
-        <div
-          className={
-            'token-specimen__sample token-specimen__sample--box ' +
-            this.props.specimenClassName
-          }
-          style={this.props.inlineStyles}
-        >
-          {this.props.children}
-        </div>
-      );
-    }
-
-    return (
-      <div className={componentClassName}>
-        <div className="token-specimen__info">
-          <code className="token-specimen__name">{this.props.name}</code>
-          <code className="token-specimen__value">{this.props.value}</code>
-          <p className="token-specimen__comment">{this.props.comment}</p>
-        </div>
-        <div className="token-specimen__body">{sample}</div>
+  if (props.variant === 'typography-title') {
+    sample = (
+      <div
+        className={
+          'token-specimen__sample token-specimen__sample--typography-title ' +
+          props.specimenClassName
+        }
+        contentEditable
+        style={props.inlineStyles}
+        suppressContentEditableWarning
+      >
+        AaBbCcDdEeFfGg
+      </div>
+    );
+  } else if (props.variant === 'typography-body') {
+    sample = (
+      <div
+        className={
+          'token-specimen__sample token-specimen__sample--typography-body ' +
+          props.specimenClassName
+        }
+        contentEditable
+        style={props.inlineStyles}
+        suppressContentEditableWarning
+      >
+        Almost before we knew it, we had left the ground.
+      </div>
+    );
+  } else if (props.variant === 'animation-fade') {
+    sample = (
+      <div
+        className={
+          'token-specimen__sample token-specimen__sample--box token-specimen__sample--animation-fade ' +
+          props.specimenClassName
+        }
+        style={props.inlineStyles}
+      ></div>
+    );
+  } else if (props.variant === 'animation-move') {
+    sample = (
+      <div
+        className={
+          'token-specimen__sample token-specimen__sample--box token-specimen__sample--animation-move ' +
+          props.specimenClassName
+        }
+        style={props.inlineStyles}
+      ></div>
+    );
+  } else {
+    sample = (
+      <div
+        className={
+          'token-specimen__sample token-specimen__sample--box ' +
+          props.specimenClassName
+        }
+        style={props.inlineStyles}
+      >
+        {props.children}
       </div>
     );
   }
-}
+
+  return (
+    <div className={componentClassName}>
+      <div className="token-specimen__info">
+        <code className="token-specimen__name">{props.name}</code>
+        <code className="token-specimen__value">{props.value}</code>
+        <p className="token-specimen__comment">{props.comment}</p>
+      </div>
+      <div className="token-specimen__body">{sample}</div>
+    </div>
+  );
+};
