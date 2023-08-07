3f80272e9be39846e86955d74b04a4c01a0e9a1a
Jin Lee <jin.lee@chanzuckerberg.com>
2023-08-07 10:48:19 -0700

    docs: split t2 colors stories and remove t2 stories aggregator

---
+++ .storybook/components/DesignTokens/Tier2/Colors.stories.tsx
@@ -0,0 +1,100 @@
+import type { StoryObj } from '@storybook/react';
+import React from 'react';
+import { Section } from '../../../../src';
+import filterTokens from '../../../util/filterTokens';
+import { ColorList } from '../../ColorList/ColorList';
+
+export default {
+  title: 'Design Tokens/Tier 2: Usage/Colors',
+  parameters: {
+    axe: {
+      // For documentation purposes only
+      skip: true,
+    },
+  },
+};
+
+const getListItems = ({
+  filterTerm,
+  figmaTokenHeader,
+  tailwindClassHeader,
+}: {
+  filterTerm: string;
+  figmaTokenHeader: string;
+  tailwindClassHeader: string;
+}) =>
+  filterTokens(filterTerm).map(({ name, value }) => {
+    const specifier = name.slice(
+      name.indexOf(filterTerm) + filterTerm.length + 1,
+    );
+    return {
+      name,
+      value,
+      figmaToken: figmaTokenHeader + '/' + specifier,
+      tailwindClass: tailwindClassHeader + '-' + specifier,
+    };
+  });
+
+export const Text: StoryObj = {
+  render: () => (
+    <div className="text-neutral-default">
+      <Section title="Text Colors">
+        <ColorList
+          listItems={getListItems({
+            filterTerm: 'eds-theme-color-text',
+            figmaTokenHeader: 'text',
+            tailwindClassHeader: 'text',
+          })}
+        />
+      </Section>
+    </div>
+  ),
+};
+
+export const Icon: StoryObj = {
+  render: () => (
+    <div>
+      <Section title="Icon Colors">
+        <ColorList
+          listItems={getListItems({
+            filterTerm: 'eds-theme-color-icon',
+            figmaTokenHeader: 'icon',
+            tailwindClassHeader: 'text-icon',
+          })}
+        />
+      </Section>
+    </div>
+  ),
+};
+
+export const Background: StoryObj = {
+  render: () => (
+    <div>
+      <Section title="Background Colors">
+        <ColorList
+          listItems={getListItems({
+            filterTerm: 'eds-theme-color-background',
+            figmaTokenHeader: 'background',
+            tailwindClassHeader: 'bg',
+          })}
+        />
+      </Section>
+    </div>
+  ),
+};
+
+export const Border: StoryObj = {
+  render: () => (
+    <div>
+      <Section title="Border Colors">
+        <ColorList
+          listItems={getListItems({
+            filterTerm: 'eds-theme-color-border',
+            figmaTokenHeader: 'border',
+            tailwindClassHeader: 'border',
+          })}
+        />
+      </Section>
+    </div>
+  ),
+};
