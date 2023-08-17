efbde351ec8e053cff1805965ca37595ca7bd4d3
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2023-08-17 18:32:10 -0500

    fix(Avatar): improve support for generating initials
    - add handling for unicode surrogate pairs using graphemer
    - handling for extended BMP (Basic Multilingual Plane) characters
    - handling for leading and trailing spaces

--- src/components/Avatar/Avatar.test.ts
+++ src/components/Avatar/Avatar.test.ts
@@ -1,6 +1,37 @@
 import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import { getInitials } from './Avatar';
 import * as stories from './Avatar.stories';
 
 describe('<Avatar />', () => {
   generateSnapshots(stories);
+
+  // Testing handling of surrogate pairs, et al
+  // https://javascript.info/unicode#surrogate-pairs
+  describe('.getInitials', () => {
+    it.each`
+      givenName                     | expectedInitials
+      ${'John Smith'}               | ${'JS'}
+      ${'Jenny C. Sallow'}          | ${'JS'}
+      ${'Thomas Stroughton-Felton'} | ${'TS'}
+      ${'Young Yarn Lad'}           | ${'YL'}
+      ${'Tim 肇廷'}                 | ${'T肇'}
+      ${'Ãndrew 🙂'}                | ${'Ã🙂'}
+      ${'你好世界'}                 | ${'你'}
+      ${'🧶👦🏽'}                     | ${'🧶'}
+      ${'𩷶'}                       | ${'𩷶'}
+      ${'𝒳'}                        | ${'𝒳'}
+      ${'☹️ 🙂'}                    | ${'☹️🙂'}
+      ${'👦🏽🧶'}                     | ${'👦🏽'}
+      ${'🧙‍♂️'}                       | ${'🧙‍♂️'}
+      ${''}                         | ${''}
+      ${' Leading Space'}           | ${'LS'}
+      ${'Trailing Space '}          | ${'TS'}
+      ${'Z͑ͫ̓ͪ̂ͫ̽͏̴̙̤̞͉͚̯̞̠͍A̴̵̜̰͔ͫ͗͢L̠ͨͧͩ͘G̴̻͈͍͔̹̑͗̎̅͛́Ǫ̵̹̻̝̳͂̌̌͘'}                    | ${'Z͑ͫ̓ͪ̂ͫ̽͏̴̙̤̞͉͚̯̞̠͍'}
+    `(
+      'generates initials $expectedInitials when given $givenName',
+      ({ givenName, expectedInitials }) => {
+        expect(getInitials(givenName)).toEqual(expectedInitials);
+      },
+    );
+  });
 });
