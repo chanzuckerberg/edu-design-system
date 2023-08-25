334ad8fb9193925c83b73887dc0dea5b418519a3
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2023-08-25 17:58:19 -0500

    refactor: apply updates to prepare for introduction to EDS
    - update naming for consistency
    - add in better ordering of commands and exception handling
    - sync changes to style-dictionary
    - add documentation for using EDS commands
    - update README to link to new documentation

--- bin/eds-apply-theme.js
+++ bin/eds-apply-theme.js
@@ -1,43 +1,21 @@
 #!/usr/bin/env node
 (async function () {
-  /**
-   * Documentation:
-   * - Token change log published when EDS changed, and errors link to changelog
-   *
-   * Possible Usages:
-   * - Checks for reference to the token to link to change log entry(ies)
-   * - Integrate with husky so that PRs always include updated theme values (pre-push)
-   *
-   * TODO:
-   * - remove the config, and generated files from this PR before merging
-   * - update the local app-theme.json with any new values from the base theme
-   *
-   * Questions:
-   * - should this preserve the tier 1 tokens used by tier 2 and 3 files?
-   * - (in wireframe kit, the only updates happen to colors/fonts)
-   * - (in along, colors/fonts/border-* & drop shadows need overriding)
-   * - should we use token references in the output instead of the hex codes?
-   * - should we remove the listing of the tier-1 values in the theme base file?
-   * - should we use the wireframe theme as the values used in the theme base?
-   * - should all the grade-based tokens be filtered out (they might not apply to point solutions)?
-   */
   const StyleDictionary = require('style-dictionary');
   const path = require('path');
   const fs = require('fs');
   const { getConfig } = require('./_util');
 
-  // Compare local to base theme file for differences. flag and quit when theme has
-  // undefined tokens being overridden
   let packageRootPath;
   try {
     packageRootPath =
-      path.dirname(require.resolve('@chanzuckerberg/eds')) + '/tokens/';
+      path.dirname(require.resolve('@chanzuckerberg/eds')) + '/tokens/json/';
   } catch (e) {
     console.error('EDS package not installed. Using local path...');
     packageRootPath =
       path.dirname(require.main.path) + '/src/tokens-dist/json/';
   }
 
+  // Read the config to sort out where to read JSON from and where to write the CSS file
   const config = await getConfig();
 
   // read and parse JSON files on disk
@@ -48,20 +26,12 @@
     fs.readFileSync(`${packageRootPath}theme-base.json`, 'utf8'),
   );
 
-  // Keys in theme must be a strict subset of those in base
-  try {
-    isStrictSubset(baseTheme, localTheme);
-  } catch (error) {
-    // TODO: if theme has things not in base, error showing where the conflict
-    console.error('Theme error:', error.message);
-    return;
-  }
-
+  // define the header to use in the resulting CSS file so people know not to edit it directly
   StyleDictionary.registerFileHeader({
     name: 'cssOverrideHeader',
     fileHeader: (defaultMessage) => [
       ...defaultMessage,
-      'To update, edit app-theme.json, then run `npx eds-update-theme`',
+      'To update, edit app-theme.json, then run `npx eds-apply-theme`',
     ],
   });
 
@@ -87,20 +57,39 @@
       },
     },
   });
-  EDSStyleDictionary.buildAllPlatforms();
 
+  /**
+   * Determine if the given theme file is a subset of what's in the base theme file.
+   * If it isnt, throw an error:
+   * - If keys are in base that are missing in the theme file, that's OK (no need to override everything)
+   * - If keys are in theme that aren't in base, throw (you can't theme tokens that don't exist in EDS)
+   * @param {object} base The tokens theme file stored in the EDS project
+   * @param {object} theme The project theme file stored in the app code (same format as bas)
+   * @param {Array} path The base path, stored as an array of object key names (default [])
+   * @throws Error when there are tokens in theme that aren't in base
+   */
   function isStrictSubset(base, theme, path = []) {
     for (const name in theme) {
       if (typeof theme[name] === 'object') {
         if (base[name] === undefined) {
           throw new Error(
-            `Local themeable value does not exist in base theme: ${path.join(
-              '.',
+            `Local themeable value does not exist in base theme: --${path.join(
+              '-',
             )}.${name}"`,
           );
         }
         isStrictSubset(base[name], theme[name], path.concat(name));
       }
     }
+  }
+
+  try {
+    // Keys in the theme file must be a strict subset of those in the base file
+    isStrictSubset(baseTheme, localTheme);
+    EDSStyleDictionary.buildAllPlatforms();
+  } catch (error) {
+    // TODO: if theme has things not in base, error showing where the conflict
+    console.error('EDS theming error:', error.message);
+    return;
   }
 })();
--- bin/eds-init.js
+++ bin/eds-init.js
@@ -7,30 +7,31 @@
   let packageRootPath;
   try {
     packageRootPath =
-      path.dirname(require.resolve('@chanzuckerberg/eds')) + '/tokens/';
+      path.dirname(require.resolve('@chanzuckerberg/eds')) + '/tokens/json/';
   } catch (e) {
     console.error('EDS package not installed. Using local path...');
-    packageRootPath = path.dirname(require.main.path) + '/src/tokens-dist/';
+    packageRootPath =
+      path.dirname(require.main.path) + '/src/tokens-dist/json/';
   }
 
   // read in the config from config file, package json "eds", etc.
   const config = await getConfig();
 
-  // take the packaged var file and place a copy in the project's 'json' directory
+  // take the packaged token file and place a copy in the project's 'json' specified path
   if (config) {
     try {
       fs.copyFileSync(
-        packageRootPath + 'json/theme-base.json',
+        packageRootPath + 'theme-base.json',
         `${config.json}app-theme.json`,
         fs.constants.COPYFILE_EXCL,
       );
     } catch (error) {
       console.error('The local theme file already exists. Exiting.');
-      return;
+      return 1;
     }
 
     console.log(
-      'File copy completed! Please use `npx eds-update-theme` to generate theme override CSS.',
+      'File copy completed! Please use `npx eds-apply-theme` to generate theme tokens (CSS Variables).',
     );
   }
 })();
