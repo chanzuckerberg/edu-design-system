f7968f2055c6f7ff0c69ed942061332ffce9db52
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2023-09-06 13:15:26 -0500

    chore: fix typo

--- src/util/variant-types.ts
+++ src/util/variant-types.ts
@@ -1,5 +1,7 @@
 /**
- * This contains the broad types of the commonprogrammatic variants: e.g.,
+ * This contains the broad types of the common programmatic variants: e.g.,
  * - size
+ *
+ * use Omit/Pick to trim any unsupported variants
  */
 export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
