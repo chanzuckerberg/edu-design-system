c4312d532a8a99e867292dc146fbed3fb18833ca
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2024-04-22 17:55:47 -0500

    test: add snapshot tests to 2.0 components (#1920)

---
+++ src/components/Modal/Modal-v2.test.tsx
@@ -0,0 +1,134 @@
+import { generateSnapshots, wait } from '@chanzuckerberg/story-utils';
+import type { StoryFile } from '@storybook/testing-react';
+import { composeStories } from '@storybook/testing-react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { Modal } from './Modal-v2';
+import * as stories from './Modal-v2.stories';
+import '../../../jest/helpers/removeModalTransitionStylesJestSerializer';
+
+const { Default } = composeStories(stories);
+
+window.ResizeObserver = class FakeResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+describe('Modal', () => {
+  generateSnapshots(stories as StoryFile, {
+    getElement: async () => {
+      const user = userEvent.setup();
+      const nonInteractiveModal = screen.queryByTestId('non-interactive');
+      if (nonInteractiveModal) return nonInteractiveModal;
+
+      const openModalButton = await screen.findByRole('button', {
+        name: 'Open the modal',
+      });
+      await user.click(openModalButton);
+      const modal = await screen.findByRole('dialog');
+
+      // Give Headless UI's transition/style classes time to settle.
+      await wait(50);
+
+      return modal;
+    },
+  });
+
+  it('is initially closed', () => {
+    render(<Default />);
+    expect(screen.queryByRole('dialog')).toBeFalsy();
+  });
+
+  it('shows the modal when the open modal button is clicked', async () => {
+    const user = userEvent.setup();
+    render(<Default />);
+    const openModalButton = await screen.findByRole('button', {
+      name: 'Open the modal',
+    });
+    await user.click(openModalButton);
+    const modal = await screen.findByRole('dialog');
+    expect(modal).toBeTruthy();
+  });
+
+  it('closes the modal on close button click', async () => {
+    const user = userEvent.setup();
+    render(<Default />);
+    const openModalButton = await screen.findByRole('button', {
+      name: 'Open the modal',
+    });
+    await user.click(openModalButton);
+    const closeButton = await screen.findByRole('button', {
+      name: 'close modal',
+    });
+    await user.click(closeButton);
+    await waitFor(() => {
+      expect(screen.queryByRole('dialog')).toBeFalsy();
+    });
+  });
+
+  it('closes the modal on ESC key press', async () => {
+    const user = userEvent.setup();
+    render(<Default />);
+    const openModalButton = await screen.findByRole('button', {
+      name: 'Open the modal',
+    });
+    await user.click(openModalButton);
+    await user.keyboard('{Escape}');
+    await waitFor(() => {
+      expect(screen.queryByRole('dialog')).toBeFalsy();
+    });
+  });
+
+  it('does not throw an error if modal uses <Modal.Title>', () => {
+    const modalWithTitle = (
+      <Modal onClose={() => {}} open>
+        <Modal.Header>
+          <Modal.Title>Modal Title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Modal body content.</Modal.Body>
+        <Modal.Footer>Modal footer content.</Modal.Footer>
+      </Modal>
+    );
+    const renderMethod = () => {
+      render(modalWithTitle);
+    };
+
+    expect(renderMethod).not.toThrow(Error);
+  });
+
+  it('does not throw an error if modal uses aria-label', () => {
+    const modalWithAriaLabel = (
+      <Modal aria-label="aria label" onClose={() => {}} open>
+        <Modal.Header>Modal Title</Modal.Header>
+        <Modal.Body>Modal body content.</Modal.Body>
+        <Modal.Footer>Modal footer content.</Modal.Footer>
+      </Modal>
+    );
+    const renderMethod = () => {
+      render(modalWithAriaLabel);
+    };
+
+    expect(renderMethod).not.toThrow(Error);
+  });
+
+  it('does throw an error if modal does not use <Modal.Title> or aria-label', () => {
+    const modalWithoutTitleOrAriaLabel = (
+      <Modal onClose={() => {}} open>
+        <Modal.Header>Modal Title</Modal.Header>
+        <Modal.Body>Modal body content.</Modal.Body>
+        <Modal.Footer>Modal footer content.</Modal.Footer>
+      </Modal>
+    );
+    const renderMethod = () => {
+      render(modalWithoutTitleOrAriaLabel);
+    };
+
+    // expect console error from react, suppressed.
+    const consoleErrorMock = jest.spyOn(console, 'error');
+    consoleErrorMock.mockImplementation();
+    expect(renderMethod).toThrow(Error);
+    consoleErrorMock.mockRestore();
+  });
+});
---
+++ src/components/Select/Select-v2.test.tsx
@@ -0,0 +1,286 @@
+import { generateSnapshots } from '@chanzuckerberg/story-utils';
+import { composeStory } from '@storybook/testing-react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { Select } from './Select-v2';
+import * as stories from './Select-v2.stories';
+
+const {
+  OpenByDefault,
+  LongOptionList,
+  Disabled,
+  DisabledRequired,
+  OptionsRightAligned,
+  SeparateButtonAndMenuWidth,
+  EventHandlingOnRenderProp,
+  EventHandlingOnStandardButton,
+  Multiple,
+  ...closedStories
+} = stories;
+
+const DisabledComponent = composeStory(Disabled, stories.default);
+
+const exampleOptions = [
+  {
+    key: '1',
+    label: 'Option 1',
+  },
+  {
+    key: '2',
+    label: 'Option 2',
+  },
+  {
+    key: '3',
+    label: 'Option 3',
+  },
+];
+
+describe('<Select />', () => {
+  describe('Generated Snapshots', () => {
+    generateSnapshots(closedStories, {
+      getElement: async () => {
+        const user = userEvent.setup();
+        const openButton = await screen.findByRole('button');
+        await user.click(openButton);
+        await screen.findAllByRole('option');
+        return screen.getByTestId('dropdown');
+      },
+    });
+  });
+
+  it('does not open a list when clicked and disabled', async () => {
+    const user = userEvent.setup();
+    render(<DisabledComponent />);
+
+    const openTrigger = await screen.findByRole('button');
+
+    await user.click(openTrigger);
+
+    // see if there are any options, which there should not be
+    expect(screen.queryByRole('option')).not.toBeInTheDocument();
+  });
+
+  it('throws an error if children is used without labeling', () => {
+    const dropdownWithChildrenAndLabelText = (
+      <Select
+        name="throwing-select"
+        onChange={() => undefined}
+        value={exampleOptions[0]}
+      >
+        <Select.Button>Select</Select.Button>
+
+        <Select.Options>
+          {exampleOptions.map((option) => (
+            <Select.Option key={option.key} value={option}>
+              {option.label}
+            </Select.Option>
+          ))}
+        </Select.Options>
+      </Select>
+    );
+    const renderMethod = () => {
+      render(dropdownWithChildrenAndLabelText);
+    };
+
+    // expect console error from react, suppressed.
+    const consoleErrorMock = jest.spyOn(console, 'error');
+    consoleErrorMock.mockImplementation();
+    expect(renderMethod).toThrow(Error);
+    consoleErrorMock.mockRestore();
+  });
+
+  it('does not throw an error if select uses <Select.Label>', () => {
+    const dropdownWithDropdownLabel = (
+      <Select
+        name="non-throwing-select"
+        onChange={() => undefined}
+        value={exampleOptions[0]}
+      >
+        <Select.Label htmlFor="non-throwing-select">Options:</Select.Label>
+        <Select.Button>Select</Select.Button>
+
+        <Select.Options>
+          {exampleOptions.map((option) => (
+            <Select.Option key={option.key} value={option}>
+              {option.label}
+            </Select.Option>
+          ))}
+        </Select.Options>
+      </Select>
+    );
+    const renderMethod = () => {
+      render(dropdownWithDropdownLabel);
+    };
+
+    expect(renderMethod).not.toThrow(Error);
+  });
+
+  it('[GROUP-112] generates a hidden field when used in uncontrolled mode', () => {
+    const dropdownWithChildrenAndLabelText = (
+      <Select
+        aria-label="test"
+        data-testid="dropdown"
+        name="uncontrolled-select"
+        onChange={() => undefined}
+        value={exampleOptions[0]}
+      >
+        <Select.Button>Select</Select.Button>
+
+        <Select.Options>
+          {exampleOptions.map((option) => (
+            <Select.Option key={option.key} value={option}>
+              {option.label}
+            </Select.Option>
+          ))}
+        </Select.Options>
+      </Select>
+    );
+
+    const { container } = render(dropdownWithChildrenAndLabelText);
+
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    expect(container.querySelector(`input`)).toBeInTheDocument();
+  });
+
+  describe('event handling', () => {
+    it('handles click on .Button', async () => {
+      const clickHandler = jest.fn();
+      const user = userEvent.setup();
+
+      render(
+        <Select
+          aria-label="test"
+          data-testid="dropdown"
+          name="uncontrolled-select"
+          onChange={() => undefined}
+          value={exampleOptions[0]}
+        >
+          <Select.Button onClick={clickHandler}>Select</Select.Button>
+
+          <Select.Options>
+            {exampleOptions.map((option) => (
+              <Select.Option key={option.key} value={option}>
+                {option.label}
+              </Select.Option>
+            ))}
+          </Select.Options>
+        </Select>,
+      );
+
+      const openTrigger = await screen.findByRole('button');
+
+      await user.click(openTrigger);
+      expect(clickHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it('handles click on .ButtonWrapper when using render prop', async () => {
+      const clickHandler = jest.fn();
+      const user = userEvent.setup();
+
+      render(
+        <Select
+          aria-label="test"
+          data-testid="dropdown"
+          name="uncontrolled-select"
+          onChange={() => undefined}
+          value={exampleOptions[0]}
+        >
+          <Select.Button>
+            {({ value, open }) => (
+              <Select.ButtonWrapper isOpen={open} onClick={clickHandler}>
+                {value.label}
+              </Select.ButtonWrapper>
+            )}
+          </Select.Button>
+
+          <Select.Options>
+            {exampleOptions.map((option) => (
+              <Select.Option key={option.key} value={option}>
+                {option.label}
+              </Select.Option>
+            ))}
+          </Select.Options>
+        </Select>,
+      );
+
+      const openTrigger = await screen.findByRole('button');
+
+      await user.click(openTrigger);
+      expect(clickHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it('handles change on <Select/>', async () => {
+      const changeHandler = jest.fn();
+      const user = userEvent.setup();
+
+      render(
+        <Select
+          aria-label="test"
+          data-testid="dropdown"
+          name="uncontrolled-select"
+          onChange={changeHandler}
+          value={exampleOptions[0]}
+        >
+          <Select.Button>Select</Select.Button>
+
+          <Select.Options>
+            {exampleOptions.map((option) => (
+              <Select.Option key={option.key} value={option}>
+                {option.label}
+              </Select.Option>
+            ))}
+          </Select.Options>
+        </Select>,
+      );
+
+      const openTrigger = await screen.findByRole('button');
+
+      // It should only fire change once, after the value is actually modified
+      await user.click(openTrigger);
+      expect(changeHandler).toHaveBeenCalledTimes(0);
+
+      // pick the second item
+      await user.keyboard('{arrowdown}');
+      await user.keyboard('{enter}');
+
+      expect(changeHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not call change when <Select/> is picking the same item', async () => {
+      const changeHandler = jest.fn();
+      const user = userEvent.setup();
+
+      render(
+        <Select
+          aria-label="test"
+          data-testid="dropdown"
+          name="uncontrolled-select"
+          onChange={changeHandler}
+          value={exampleOptions[0]}
+        >
+          <Select.Button>Select</Select.Button>
+
+          <Select.Options>
+            {exampleOptions.map((option) => (
+              <Select.Option key={option.key} value={option}>
+                {option.label}
+              </Select.Option>
+            ))}
+          </Select.Options>
+        </Select>,
+      );
+
+      const openTrigger = await screen.findByRole('button');
+
+      // It should only fire change once, after the value is actually modified
+      await user.click(openTrigger);
+      expect(changeHandler).toHaveBeenCalledTimes(0);
+
+      // pick the same item
+      await user.keyboard('{enter}');
+
+      expect(changeHandler).toHaveBeenCalledTimes(0);
+    });
+  });
+});
