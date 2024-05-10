e7fdfd41f99f27d248ab0a437ddbf3d1b576298c
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2024-05-10 17:55:56 -0500

    feat!: adjust responsive and spacing tokens/config
    - sync token names and values with design
    - adjust usage in relevant components
    - add in spacing tokens for EDS spacing units
    - update snapshots

--- src/design-tokens/tier-1-definitions/breakpoints.js
+++ src/design-tokens/tier-1-definitions/breakpoints.js
@@ -6,12 +6,12 @@
 //    variables since presently CSS custom
 //    properties can't be used within media
 //    query declarations
+// sync with tailwind.config.ts
 
 module.exports = {
-  'eds-bp-sm': '23.4375rem', // 375px
-  'eds-bp-sm-2': '35rem', // 560px
-  'eds-bp-md': '48rem', // 768px
-  'eds-bp-lg': '60rem', // 960px
-  'eds-bp-xl': '75rem', // 1200px
-  'eds-bp-xxl': '87.5rem', // 1400px
+  'eds-bp-xs': '0px',
+  'eds-bp-sm': '600px',
+  'eds-bp-md': '768px',
+  'eds-bp-lg': '1040px',
+  'eds-bp-xl': '1440px',
 };
