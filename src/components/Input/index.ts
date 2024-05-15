2f25f6ba12dc976cd12dfd9c5eaa907aba7f7d35
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2024-05-15 11:51:07 -0500

    chore: export types for InputPropsV2

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,3 +1,3 @@
 export { Input as default } from './Input';
 export type { InputProps } from './Input';
-export { Input as InputV2, InputProps as InputPropsV2 } from './Input-v2';
+export { Input as InputV2, type InputProps as InputPropsV2 } from './Input-v2';
