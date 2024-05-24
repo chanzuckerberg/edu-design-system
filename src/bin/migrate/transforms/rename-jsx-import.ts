109a0e583198e79a4ae819e2be5ba67b83b2264a
Jeremiah Clothier <94476616+jeremiah-clothier@users.noreply.github.com>
2024-05-24 10:17:59 -0700

    feat: add `eds-migrate` script for running codemods on major version upgrades (#1951)
    Adding a new script which can be executed using `npx eds-migrate --help` to help
    automatically migrate components with new major version releases.

---
+++ src/bin/migrate/transforms/rename-jsx-import.ts
@@ -0,0 +1,92 @@
+import { SyntaxKind, type SourceFile } from 'ts-morph';
+import { isDesignSystemImport } from '../helpers';
+
+export type Change = {
+  alias?: string;
+  oldImportName: string;
+  newImportName: string;
+  removeAlias?: boolean;
+};
+
+type TransformOptions = {
+  file: SourceFile;
+  changes: Change[];
+};
+
+/**
+ * Transforms import declarations in a source file based on the provided conversions.
+ * @param {TransformOptions} options - The transformation options.
+ */
+export default function transform({ file, changes }: TransformOptions) {
+  const importDeclarations = file.getImportDeclarations();
+  const importsToTransform = importDeclarations.filter(
+    (importDeclaration) =>
+      !importDeclaration.isTypeOnly() &&
+      isDesignSystemImport(importDeclaration),
+  );
+
+  /**
+   * Used to keep track of which JSXElements need to be renamed
+   */
+  const jsxElementsToRename: Array<{
+    oldName: string;
+    newName: string;
+  }> = [];
+
+  importsToTransform.forEach((importDeclaration) => {
+    const namedImports = importDeclaration.getNamedImports();
+
+    namedImports.forEach((namedImport) => {
+      const name = namedImport.getName();
+      const changeToApply = changes.find(
+        (change) => change.oldImportName === name,
+      );
+
+      if (changeToApply) {
+        const jsxElementName =
+          namedImport.getAliasNode()?.getText() || namedImport.getName();
+
+        jsxElementsToRename.push({
+          oldName: jsxElementName,
+          newName: changeToApply.alias || changeToApply.newImportName,
+        });
+
+        namedImport.setName(changeToApply.newImportName);
+        namedImport.getNameNode().rename(changeToApply.newImportName);
+        if (changeToApply.removeAlias) {
+          namedImport.removeAliasWithRename();
+        } else if (changeToApply.alias) {
+          namedImport.renameAlias(changeToApply.alias);
+        }
+      }
+    });
+  });
+
+  file.getDescendantsOfKind(SyntaxKind.JsxElement).forEach((element) => {
+    const openingElement = element.getOpeningElement();
+    const closingElement = element.getClosingElement();
+    const openingTagNameNode = openingElement.getTagNameNode();
+    const closingTagNameNode = closingElement.getTagNameNode();
+
+    const renameInfo = jsxElementsToRename.find(
+      ({ oldName }) => oldName === openingTagNameNode.getText(),
+    );
+
+    if (renameInfo) {
+      openingTagNameNode.replaceWithText(renameInfo.newName);
+      closingTagNameNode.replaceWithText(renameInfo.newName);
+    }
+  });
+
+  file
+    .getDescendantsOfKind(SyntaxKind.JsxSelfClosingElement)
+    .forEach((element) => {
+      const tagNameNode = element.getTagNameNode();
+      const renameInfo = jsxElementsToRename.find(
+        ({ oldName }) => oldName === tagNameNode.getText(),
+      );
+      if (renameInfo) {
+        tagNameNode.replaceWithText(renameInfo.newName);
+      }
+    });
+}
