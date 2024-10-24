fcd144aed481b0506b6214ed60b334e98d630717
Jeremiah Clothier <jclothier@chanzuckerberg.com>
2024-10-24 10:05:28 -0700

    fix: broken tests

--- jest/jest.setup.ts
+++ jest/jest.setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+beforeAll(() => {
+  // Mock out ResizeObserver, otherwise there are a bunch of failing tests
+  // "ReferenceError: ResizeObserver is not defined"
+  global.ResizeObserver = class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+});
