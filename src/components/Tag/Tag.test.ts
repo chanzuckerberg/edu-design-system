e0b9d6c7161d3884716734441cb653be0f15c4a9
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2024-11-07 17:14:51 -0600

    feat(Tag): update to 2.0 styles
    - use new render color tokens
    - update layout
    - update tests and snapshots

--- src/components/Tag/Tag.test.ts
+++
@@ -1,7 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as stories from './Tag.stories';
-import type { StoryFile } from '../../util/utility-types';
-
-describe('<Tag />', () => {
-  generateSnapshots(stories as StoryFile);
-});
