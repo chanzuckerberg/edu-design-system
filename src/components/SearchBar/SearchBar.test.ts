bc1331fac0a9201f375367f61c63bc3e91856b92
Andrés Moreno <amoreno@chanzuckerberg.com>
2025-03-21 16:13:32 -0600

    feat!: remove searchbar component [EDS-1537]
    Summary:
    Removes the SearchBar component and all associated files/references

--- src/components/SearchBar/SearchBar.test.ts
+++
@@ -1,7 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as stories from './SearchBar.stories';
-import type { StoryFile } from '../../util/utility-types';
-
-describe('<SearchField />', () => {
-  generateSnapshots(stories as StoryFile);
-});
