ac72bbc44c73777e69ccb6b56abaa8405591e0d9
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2025-08-06 11:59:47 -0500

    chore: remove comment

--- .storybook/utility-types.ts
+++ .storybook/utility-types.ts
@@ -1,5 +1,4 @@
 import { type ReactRenderer } from '@storybook/react';
 import type { Store_CSFExports } from '@storybook/types';
 
-// TODO-AH: don't make storybook an external dependency!
 export type StoryFile = Store_CSFExports<ReactRenderer>;
