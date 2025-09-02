b3a01fdb517954ad2cad541780f090a49e2f921c
EDU Dev Tooling Automations <109541719+czi-developer-foundations@users.noreply.github.com>
2025-09-02 10:36:25 -0500

    chore(deps): update dependency stylelint-config-recommended to v17 (#2252)
    * chore(deps): update dependency stylelint-config-recommended to v17
    
    * chore: address lint errors
    
    - fix linting issues
    - remove unused variant prop and styles from internal Grid component
    - disable stylelint in mixins.css file
    
    ---------
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>
    Co-authored-by: Andrew Holloway <booc0mtaco@users.noreply.github.com>

--- .storybook/components/Grid/Grid.tsx
+++ .storybook/components/Grid/Grid.tsx
@@ -17,24 +17,6 @@ export type GridProps = {
    * - **sm** renders a smaller gap between items than the default
    */
   gap?: 'sm';
-  /**
-   * Stylistic variations of the GridL
-   * - **1up** yields a Grid whose Grid items span the entire width of the container
-   * - **1-3up** yields a Grid whose Grid items are stacked on small screens and expand to fit 3 across when enough screen real estate becomes available to display them comfortably side-by-side
-   * - **2up** yields a Grid whose Grid items are stacked on small screens but display side-by-side when enough screen real estate is available to do so
-   * - **3up** yields a Grid whose Grid items are stacked on small screens, transforms to a 2-across pattern and then transforms again to a 3-across pattern
-   * - **4up** yields a Grid whose Grid items are stacked on small screens, transforms to a 2-across pattern, transforms again to a 3-across pattern, and ultimately transforms to a 4-across pattern
-   * - **1-2-4up** yields a Grid whose Grid items are stacked on small screens, transforms to a 2-across pattern, and ultimately transforms to a 4-across pattern
-   * - **side-by-side** yields a Grid whose Grid items are always displayed side-by-side
-   */
-  variant?:
-    | '2up'
-    | 'side-by-side'
-    | '1-3up'
-    | '3up'
-    | '4up'
-    | '1-2-4up'
-    | '1-2-1up';
 };
 
 export type GridItemProps = {
@@ -58,16 +40,9 @@ export type GridItemProps = {
  * Grid component used to layout Grid item components into a grid pattern. This is flexible component
  * allowing for a variety of responsive layout components.
  */
-export const Grid = ({
-  className,
-  variant,
-  children,
-  gap,
-  ...other
-}: GridProps) => {
+export const Grid = ({ className, children, gap, ...other }: GridProps) => {
   const componentClassName = clsx(
     styles['grid'],
-    variant && styles[`grid--${variant}`],
     gap === 'sm' && styles['grid--gap-sm'],
     className,
   );
