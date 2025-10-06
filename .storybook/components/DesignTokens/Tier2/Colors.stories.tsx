3b0ad455b471900c490afcf0dc8276d91388503c
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2025-10-06 13:42:30 -0500

    docs(tokens): add proper formatting to the figma/tailwind token refs  (#2285)
    * docs(tokens): add proper formatting to the figma/tailwind token refs
    
    - update the token documentation pages to format token names properly
    - update code documentation explaining how the token refs are generated
    - update comments and snapshots
    
    * fix(docs): show correct Tailwind label in doc.s pages
    
    * fix: choose hyphenated name when camelCase was not applied

--- .storybook/components/DesignTokens/Tier2/Colors.stories.tsx
+++ .storybook/components/DesignTokens/Tier2/Colors.stories.tsx
@@ -4,6 +4,8 @@ import filterTokens from '../../../util/filterTokens';
 import { ColorList } from '../../ColorList/ColorList';
 import Section from '../../Section';
 
+// TODO: remove the generation of tokens.json entirely (only used for these internal pages)?
+
 export default {
   title: 'Design Tokens/Tier 2: Usage/Colors',
   parameters: {
@@ -14,9 +16,25 @@ export default {
   },
 };
 
-const camelCaseWarning =
-  'NOTE: emphasis tokens have a camelCase suffix for the emphasis (e.g., lowEmphasis)';
-
+/**
+ * Generate data for each list item row based on the contents of the tokens.json file.
+ * 
+ * Given:
+ * - row in tokens.json like "eds-theme-color-background-utility-overlay-low-emphasis": "rgba(39, 38, 37, 0.50)",
+ * - param like {
+            filterTerm: 'eds-theme-color-background-utility',
+            figmaTokenHeader: 'background',
+            tailwindClassHeader: 'bg-utility',
+          }
+ * - generate content like {
+            name: "eds-theme-color-background-utility-overlay-low-emphasis",
+            value: "rgba(39, 38, 37, 0.50)",
+            figmaToken: 'background/overlay-low-emphasis',
+            tailwindClass: "bg-utility-overlay-low-emphasis"
+          } 
+ * @param object describing the: filter to apply to tokens.json, and prefixes for figma/tailwind
+ * @returns object with information for displaying the various token formats and value
+ */
 const getListItems = ({
   filterTerm,
   figmaTokenHeader,
@@ -27,14 +45,27 @@ const getListItems = ({
   tailwindClassHeader: string;
 }) =>
   filterTokens(filterTerm).map(({ name, value }) => {
+    // trim the filter term from the full token name, along with the hyphen separator
     const specifier = name.slice(
       name.indexOf(filterTerm) + filterTerm.length + 1,
     );
+
+    // tokens.json hyphen-separates camel case token name parts using the built-in "json/flat".
+    // Fxamples:
+    // - "*-emphasis" => "*Emphasis"
+    // Note that this is not a problem in the emitted tailwind config since it doesn't use tokens.json
+    const convertEmphasesToCamelCase = /([a-z]+)-emphasis*/g;
+    const updatedSpecifier = specifier.replace(
+      convertEmphasesToCamelCase,
+      '$1Emphasis',
+    );
+
     return {
       name,
       value,
-      figmaToken: figmaTokenHeader + '/' + specifier,
-      tailwindClass: tailwindClassHeader + '-' + specifier,
+      // apply the passed in format prefix, and the separator
+      figmaToken: figmaTokenHeader + '/' + updatedSpecifier,
+      tailwindClass: tailwindClassHeader + '-' + updatedSpecifier,
     };
   });
 
@@ -57,10 +88,7 @@ export const TextUtility: StoryObj = {
 export const BackgroundUtility: StoryObj = {
   render: () => (
     <div>
-      <Section
-        description={camelCaseWarning}
-        title="Background Colors (utility)"
-      >
+      <Section title="Background Colors (utility)">
         <ColorList
           listItems={getListItems({
             filterTerm: 'eds-theme-color-background-utility',
@@ -76,15 +104,12 @@ export const BackgroundUtility: StoryObj = {
 export const BackgroundBrand: StoryObj = {
   render: () => (
     <div>
-      <Section description={camelCaseWarning} title="Background Colors (brand)">
+      <Section title="Background Colors (brand)">
         <ColorList
           listItems={getListItems({
             filterTerm: 'eds-theme-color-background-brand',
             figmaTokenHeader: 'background',
             tailwindClassHeader: 'bg-brand',
-          }).filter((item) => {
-            // remove legacy primary tokens
-            return item.name.indexOf('primary') === -1;
           })}
         />
       </Section>
@@ -95,7 +120,7 @@ export const BackgroundBrand: StoryObj = {
 export const BorderUtility: StoryObj = {
   render: () => (
     <div>
-      <Section description={camelCaseWarning} title="Border Colors (utility)">
+      <Section title="Border Colors (utility)">
         <ColorList
           listItems={getListItems({
             filterTerm: 'eds-theme-color-border-utility',
@@ -111,15 +136,12 @@ export const BorderUtility: StoryObj = {
 export const BorderBrand: StoryObj = {
   render: () => (
     <div>
-      <Section description={camelCaseWarning} title="Border Colors (brand)">
+      <Section title="Border Colors (brand)">
         <ColorList
           listItems={getListItems({
             filterTerm: 'eds-theme-color-border-brand',
             figmaTokenHeader: 'border',
             tailwindClassHeader: 'border-brand',
-          }).filter((item) => {
-            // remove legacy primary tokens
-            return item.name.indexOf('primary') === -1;
           })}
         />
       </Section>
