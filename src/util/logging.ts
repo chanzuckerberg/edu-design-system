7859104e07a6673f8a930decb273fc14a5f3af05
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2026-03-04 10:52:46 -0600

    fix(AppHeader): adjust layout of mobile drawer position/behavior (#2404)
    * fix(AppHeader): adjust layout of mobile drawer position/behavior
    
    - reorganize stories to better show testing
    - add layout and structure changes to draw popover
    - add in support for nav menus in mobile
    - change handling of viewport in storybook
    - adjust padding/margins
    
    * fix: address PR comments
    
    * chore(AppHeader): add usage assertion to menu nav item fallback
    
    * chore: remove commented-out story config
    
    * fix(AppHeader): handle close on interact with nav items
    
    * docs(AppHeader): add clarifying doc.s to the mobile floating vert story

--- src/util/logging.ts
+++ src/util/logging.ts
@@ -7,8 +7,8 @@ type LogLevel = 'warn' | 'error';
  * Logging function used to check whether a usage of EDS is proper and advised. When using, it defaults
  * to warning, where it will print a message to the console for developers to see. LogLevel supported.
  *
- * @param checks set of boolean checks to assert whether the component usages are compatible
- * @param message Message to print when the component is not being used as advised
+ * @param list of truthy checks to assert whether the component usages are appropriate
+ * @param message Message to print when ANY of the checks are true
  * @param [loglevel] Severity of the tracked issue
  */
 export function assertEdsUsage(
