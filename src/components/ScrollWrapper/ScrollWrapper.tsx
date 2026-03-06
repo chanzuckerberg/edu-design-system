2aacbd11af4ea6017476c7af26ed017595135926
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2026-03-06 13:18:43 -0600

    feat(ScrollWrapper): add support for orientations

--- src/components/ScrollWrapper/ScrollWrapper.tsx
+++ src/components/ScrollWrapper/ScrollWrapper.tsx
@@ -20,6 +20,10 @@ export type ScrollWrapperProps = HTMLAttributes<HTMLDivElement> & {
    * CSS class names that can be appended to the component.
    */
   className?: string;
+  /**
+   * Determines the direction that the shadows apply
+   */
+  orientation?: 'horizontal' | 'vertical';
   // Design API
   /**
    * Type of shadow treatment for the wrapper:
@@ -40,6 +44,7 @@ export type ScrollWrapperProps = HTMLAttributes<HTMLDivElement> & {
 export const ScrollWrapper = ({
   children,
   className,
+  orientation = 'vertical',
   shadowType = 'cover',
   ...other
 }: ScrollWrapperProps) => {
@@ -47,23 +52,35 @@ export const ScrollWrapper = ({
   const [shadowState, setShadowState] = useState({
     top: false,
     bottom: false,
+    start: false,
+    end: false,
   });
 
   const outterClassName = clsx(
     styles['scroll-wrapper'],
+    orientation && styles[`scroll-wrapper--orientation-${orientation}`],
     shadowState.top && styles['scroll-wrapper--has-top-shadow'],
     shadowState.bottom && styles['scroll-wrapper--has-bottom-shadow'],
+    shadowState.start && styles['scroll-wrapper--has-start-shadow'],
+    shadowState.end && styles['scroll-wrapper--has-end-shadow'],
     shadowType && styles[`scroll-wrapper--shadow-type-${shadowType}`],
     className,
   );
 
   // This handler fires upon every scroll event. changes are "debounced" by the set state calls
   const handler = (ev: Event) => {
-    const showShadows = { top: false, bottom: false };
+    const showShadows = { top: false, bottom: false, start: false, end: false };
     if (ev.target) {
-      const { scrollTop, scrollHeight, clientHeight } =
-        ev.target as HTMLDivElement;
+      const {
+        scrollTop,
+        scrollLeft,
+        scrollHeight,
+        clientHeight,
+        scrollWidth,
+        clientWidth,
+      } = ev.target as HTMLDivElement;
 
+      // handle verticals
       if (scrollTop === 0) {
         showShadows.top = false;
       } else {
@@ -76,13 +93,26 @@ export const ScrollWrapper = ({
         showShadows.bottom = false;
       }
 
+      // handle horizontals
+      if (scrollLeft === 0) {
+        showShadows.start = false;
+      } else {
+        showShadows.start = true;
+      }
+
+      if (scrollLeft < scrollWidth - clientWidth) {
+        showShadows.end = true;
+      } else {
+        showShadows.end = false;
+      }
+
       setShadowState(showShadows);
     }
   };
 
   // remove the shadows when resizing occurs, so they aren't in the previous state
   const debouncedHandler = debounce(() => {
-    setShadowState({ top: false, bottom: false });
+    setShadowState({ top: false, bottom: false, start: false, end: false });
   }, 250);
 
   // Hook up the event handlers, monitoring resizes (reset the shadows) and scroll (add based on position)
