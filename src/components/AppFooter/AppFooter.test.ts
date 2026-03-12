24a1e6b9b5e85ad810a83fd9d188de3c3514a820
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2026-03-12 13:07:16 -0500

    test(AppFooter): add test coverage for event handling

--- src/components/AppFooter/AppFooter.test.ts
+++
@@ -1,7 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as stories from './AppFooter.stories';
-import type { StoryFile } from '../../../.storybook/utility-types';
-
-describe('<AppFooter />', () => {
-  generateSnapshots(stories as StoryFile);
-});
