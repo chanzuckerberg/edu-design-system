73a31204188cce83e1384d8d5416b97f27c77ee2
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2026-04-07 15:07:49 -0500

    feat(Table)!: remove deprecated Table component
    * remove component from src/index
    * remove component, tests, & snapshots
    * note to add manual migration instructions

--- src/components/Table/Table.test.ts
+++
@@ -1,7 +0,0 @@
-import { generateSnapshots } from '@chanzuckerberg/story-utils';
-import * as stories from './Table.stories';
-import type { StoryFile } from '../../../.storybook/utility-types';
-
-describe('<Table />', () => {
-  generateSnapshots(stories as StoryFile);
-});
