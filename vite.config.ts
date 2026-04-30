b25f166706fedfeb851c8738c85c49a8a51d2c29
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2026-04-29 20:06:27 -0500

    chore: add rollup-plugin-webpack-stats

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
+
+import webpackStats from 'rollup-plugin-webpack-stats';
 import { defineConfig } from 'vite';
+
 const dirname =
   typeof __dirname !== 'undefined'
     ? __dirname
@@ -11,6 +14,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  plugins: [webpackStats()],
   test: {
     projects: [
       {
